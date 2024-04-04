7bd18922e4571400dccda07421e66fc65304e5e8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-04 02:24:44 -0400

    Array

--- src/exception_P43/Test1.java
+++ src/exception_P43/Test1.java
@@ -6,19 +6,54 @@ import java.util.Arrays;
 public class Test1 {
 	public static void main(String[] args) {
 		
-		int abc[] = {32,232,43,23,423};
+	
 		//print using toString
+		int abc[] = {32,232,43,23,423};
 		String str = Arrays.toString(abc);
-		System.out.println(str);
+		System.out.print(str);
 		
 		
 		System.out.println("");
+		
+		
+		//printing normal array using for loop
+		int ab[] = {3,2,23,232,332,432,999};
+		for (int i = 0; i < ab.length; i++) {
+		System.out.print(ab[i] + ",");
+		}
+		
+		System.out.println("");
+		
+		
+		//This will print in reverse
+		int cba[] = {23,234,423,432,423};		
+		for (int i1 = cba.length - 1; i1 >= 0; i1--) {
+			  System.out.print(cba[i1] + ",");
+		}
+		
+		
+		System.out.println("");
+		
+		
+		//This will print in reverse
+		int cv[] = {99,88,77,66,55};
+		for (int j = cv.length - 1; j > 0; j--) {
+				System.out.print(cv[j] + ",");
+			}
+			
+		
+		System.out.println("");
+		
+		
+		//This will print in reverse
+		int cvv[] = {23,34,45,56,67};
+			for (int j11 = cvv.length -1; j11>0; j11--){
+			System.out.print(cvv[j11] + ",");
+					}
+					
+				}
+		}
 
 		
-		//This will print this array in reverse
-		int cba[] = {23,234,423,432,423};		
-		for (int i = cba.length - 1; i >= 0; i--) {
-			  System.out.print(cba[i] + ",");
-		}
-	}
-}
+
+
