869e990510a8b3833de1922615f51c49d723c088
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-17 01:38:37 -0400

    Printing a array

---
+++ src/arrays_p27/Test5.java
@@ -0,0 +1,17 @@
+package arrays_p27;
+
+import java.util.Arrays;
+
+public class Test5 {
+	public static void main(String[] args) {
+		
+		int i[] = {324,3,432,4,324};
+		
+		String str = Arrays.toString(i);
+		
+		System.out.print(str);
+		
+		//output
+		//[324, 3, 432, 4, 324]
+	}
+}
