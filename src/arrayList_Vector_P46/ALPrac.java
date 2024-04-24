d0c30c39c869985a0829103a5627cb75180c76ae
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-23 21:56:49 -0400

    Vector list

--- src/arrayList_Vector_P46/ALPrac.java
+++ src/arrayList_Vector_P46/ALPrac.java
@@ -13,9 +13,9 @@ public class ALPrac {
 		v1.add("nihar");
 		
 		//index(0 = first index), name 
-		v1.add(0,"raj");
+		v1.add("raj");
 		v1.add("raj1");
-		v1.add("raj2");
+		v1.add(1,"raj2");
 		v1.add("raj3");
 		v1.add("raj4");
 		v1.add("raj5");
@@ -23,10 +23,25 @@ public class ALPrac {
 		v1.add("raj7");
 		v1.add("raj8");
 		
-		//easy to print in collections 
-		System.out.println(v1);
 		
-		System.out.println("size: "+v1.size());
+		//easy to print in collections 
+		System.out.println(v1 + "\n");
+				
+			
+		Vector v2 = new Vector();
+
+		v2.add("v2");
+		v2.add("v22");
+		v2.add("v222");
+		v2.add("v2222");
+		v2.add("v22222");
+		
+		//this will print it 5 times using the for loop
+		for (int i = 0; i < 5; i++) {
+			System.out.println(v2);
+		}
+		
+		System.out.println("\n"+"size: "+v1.size());
 		System.out.println("capacity: "+v1.capacity());
 	}
 
