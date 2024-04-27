c4d79c2d260f45be98140102a1fc0a94b9d7b0e3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-27 01:19:10 -0400

    ArrayList in java

---
+++ src/arrayList_Vector_P46/Arraylist.java
@@ -0,0 +1,17 @@
+package arrayList_Vector_P46;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+	public static void main(String[] args) {
+		ArrayList<Integer> al1 = new ArrayList<>();
+		
+		al1.add(0,null);
+		al1.add(1);
+		al1.add(2);
+		al1.add(3);
+		
+		
+		System.out.println(al1);
+	}
+}
