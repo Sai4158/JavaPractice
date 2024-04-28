1a25cf43c95b8e35b7e7948c768c3f488f297bf1
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-28 18:13:10 -0400

    Printing Arraylist and vector using Integer and string arrays

--- src/arrayList_Vector_P46/Arraylist.java
+++ src/arrayList_Vector_P46/Arraylist.java
@@ -1,6 +1,10 @@
 package arrayList_Vector_P46;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
+
+import arrayCreation.Array;
 
 public class Arraylist {
 	public static void main(String[] args) {
@@ -13,5 +17,15 @@ public class Arraylist {
 		
 		
 		System.out.println(al1);
+		
+		Integer arr[] = {23,23,3,23,324,34343,43};
+		Vector<Integer> v1 =  new Vector<Integer>(Arrays.asList(arr));
+		System.out.println(v1);
+		System.out.println(v1.lastIndexOf(324));
+		
+		String[] str= {"3242","32423","4535"};
+		Vector<String> v2 = new Vector<String>(Arrays.asList(str));
+		System.out.println(v2);
+			
 	}
 }
