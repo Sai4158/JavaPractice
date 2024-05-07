0faa3128327758102f71797e9e33f389390b1d75
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-07 14:16:27 -0400

    This will add all even number only

---
+++ src/collections/Work2.java
@@ -0,0 +1,32 @@
+package collections;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Vector;
+
+import arrayCreation.Array;
+
+//this will print only sum of the even numbers
+public class Work2 {
+	public static void main(String[] args) {
+		Integer arr[] = {234,3,23,23,30,34,22,10, 10};
+		Vector<Integer> v1 = new Vector<Integer>(Arrays.asList(arr));
+		
+		int sum = 0;
+		for (int num : arr) {
+			if(num % 2 == 0) {
+				sum += num;
+			}	
+		}
+		System.out.print(sum);
+		
+//		//this will add the even numbers
+//		int sum = 0;
+//		for (int num : arr) {
+//			if(num % 2 == 0) {
+//				 sum += num;
+//			}
+//		}
+//		System.out.println(sum);
+	}
+}
