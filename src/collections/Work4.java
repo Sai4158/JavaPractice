2eb0e8159357568229cd77b903e2362d3622dc05
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-09 01:10:33 -0400

    Using simple Sublist to split the LinkedList

--- src/collections/Work4.java
+++ src/collections/Work4.java
@@ -2,19 +2,21 @@ package collections;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 //this will split into lists 
+//using the sublist 
+
 public class Work4 {
 	public static void main(String[] args) {
 		Integer arr[] = {11,2,22,11,30,11,2,2, 1};
 		LinkedList<Integer> s1 = new LinkedList<Integer>(Arrays.asList(arr));
-
-		//First the split array based on the size
-		int split = s1.size()/2;
 		
-		//then split into 2 
-		LinkedList<Integer> Split1 =  new LinkedList<Integer>(s1.subList(0,split));
-		LinkedList<Integer> Split2 =  new LinkedList<Integer>(s1.subList(split, s1.size()));
+		//split using the sublist
+		//from index to index
+		List<Integer> Split1 =  s1.subList(0,4);
+		List<Integer> Split2 =  s1.subList(4,8);
+
 		
 		System.out.println(Split1);
 		System.out.println(Split2);
