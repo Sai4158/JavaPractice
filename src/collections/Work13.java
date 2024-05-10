326d0a90a8289e385a4b14427da0f0afc7f95a10
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-10 13:56:35 -0400

    Printing and in reverse using vector

--- src/collections/Work13.java
+++ src/collections/Work13.java
@@ -9,10 +9,15 @@ public class Work13 {
 		Integer arr[] = {234,3,23,23,30,34,22,10, 10};
 		Vector<Integer> v1 = new Vector<Integer>(Arrays.asList(arr));
 		
-		//print this in reverse
-		for (int i = v1.size() -1; i>0; i--) {
-            System.out.print(v1.get(i) + ",");
+		//will print it 
+		for (int i = 0; i < v1.size(); i++) {
+			System.out.print(v1.get(i) + ",");
 		}
-		
+		System.out.println();
+		//will print in reverse
+		for (int i = v1.size() - 1; i>0; i--) {
+			System.out.print(v1.get(i)+ ",");
+		}
+	
 	}
 }
