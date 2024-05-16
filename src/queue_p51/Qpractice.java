45ea738e709d935c8c9c735703ae203e50f731b3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-16 11:34:15 -0400

    Comparator in PriorityQueue

--- src/queue_p51/Qpractice.java
+++ src/queue_p51/Qpractice.java
@@ -1,15 +1,21 @@
 package queue_p51;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
+
+import javax.sound.sampled.ReverbType;
 
 public class Qpractice {
 	public static void main(String[] args) {
 		//priority queue
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 		
 		//to add - add, offer
 		//will be priority only for lowest int head 
 		pq.offer(932);
+		pq.offer(34);
+		pq.add(2);
+		pq.offer(43);
 		pq.add(1);
 		pq.offer(34);
 		
@@ -18,8 +24,22 @@ public class Qpractice {
 		//peek will always give you the head 
 		System.out.println(pq.peek());
 		
-		//poll - will remove current head 
+		//poll - will remove current head and then print rest
 		System.out.println(pq.poll());
 		System.out.println(pq);
+		
+		//contains 
+		System.out.println(pq.contains(1));
+		
+		System.out.println("----------------------");
+		
+		//if you for each loop to print
+		//it will not give you head or anything
+		//it will print in the order I gave
+		
+		//but you can use WHILE loop to print 
+		while (!pq.isEmpty()) {
+			System.out.println(pq.poll());
+		}
 	}
 }
