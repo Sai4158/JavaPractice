67ea384b45a47406e3a150af7cd4d175e75f1fec
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-23 14:23:45 -0400

    Java project using list to add students data.

---
+++ src/javaProject_1/StudentTest.java
@@ -0,0 +1,86 @@
+package javaProject_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import switch_p20.Switch;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Student> StudentList = new ArrayList<Student>();
+		
+		while (true) {
+			System.out.println("Student project management");
+			System.out.println("==============");
+
+			// \n = new line
+			System.out.println("1.Add Student\n2.Update Student\n3.Display all strings\n4.Delete Student\n5.Exit\n");
+
+			System.out.println("please select operations: ");
+			int option = sc.nextInt();
+
+
+			//switch for particular operation 
+			switch (option) {
+			case 1: {
+
+				//add values
+				Student  student = StudentUtility.addStudent();
+				StudentList.add(student);
+				System.out.println("Student added Successfully");
+				break;
+			}
+			case 2: {
+
+				//update Students 
+				public static Student updateStudent(List<Student> students) {
+					System.out.println("Please enter the updated deatails");
+					
+					System.out.println("Enter Sid: ");
+					int id = sc.nextInt();
+					System.out.println("Enter Sname: ");
+					int name = sc.nextInt();
+					System.out.println("Enter sAge: ");
+					int age = sc.nextInt();
+					
+					//done with the changes 
+					//for each 
+					int count = 0;
+					for(String s: studets) {
+						if(id == s.getId()) {
+							student.get(s);
+							break;
+						}
+						count++;
+					}
+					student.get(count).setSname(name);
+					student.get(count).setAge(age);
+					return null;
+				}
+			}
+			case 3: {
+
+				//display students 
+				StudentUtility.DispayAllStudent(StudentList);
+				break;
+			}
+			case 4: {
+
+				//Delete Students
+			}
+			case 5: {
+
+				//Stop the program Students
+				System.exit(0);
+				break;
+			}
+
+			default:
+				System.err.println("please enter correct options");
+				
+			}
+		}
+	}
+}
--- src/javaProject_1/StudentUtility.java
+++ src/javaProject_1/StudentUtility.java
@@ -1,21 +1,44 @@
 package javaProject_1;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentUtility {
-	
+	Scanner sc = new Scanner(System.in);
+
 	//1 Add students 
 	
 	//obj from the student class 
 	public static Student addStudent() {
+		
 		Scanner sc = new Scanner(System.in);
 		Student s  = new Student();
 		
-		return null;
+		System.out.print("please enter student id: ");
+		//will store the id from the scanner 
+		s.setsID(sc.nextInt());
+		
+		System.out.print("please enter Student name: ");
+		s.setSname(sc.next());
+		
+		System.out.print("please enter age: ");
+		s.setAge(sc.nextInt());
+		
+		//return the obj (s)
+		return s;
 	}
+	
 	//2 Update the student 
 	
 	//3 Display the student
+	public static void DispayAllStudent(List<Student> student) {
+		Iterator<Student> iterator = student.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next().toString());
+			
+		}
+	}
 	
 	//4 Delete the student
 	
