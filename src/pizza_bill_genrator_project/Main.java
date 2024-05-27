15d508030f1f67fb33f6d2796661036635860ac3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-27 00:38:42 -0400

    Testing the app with boolean values

--- src/pizza_bill_genrator_project/Main.java
+++ src/pizza_bill_genrator_project/Main.java
@@ -1,5 +1,13 @@
 package pizza_bill_genrator_project;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+		//obj printing
+		Pizza basePizza = new Pizza (false);
+		
+		//now the get the value
+		basePizza.getPizzaPrice();
+		
+	}
 }
--- src/pizza_bill_genrator_project/Pizza.java
+++ src/pizza_bill_genrator_project/Pizza.java
@@ -1,14 +1,28 @@
 package pizza_bill_genrator_project;
 
 public class Pizza {
-	private String price;
+	private int price;
 	private Boolean veg;
 
 
 	public Pizza(Boolean veg) {
-		
-		this.veg = veg;
-	}
 
+		this.veg = veg;
+
+		//if the price of the pizza is a 300 then set it to 400
+		//if veg it is 300
+		//if its not veg then it is 400
+		if(this.veg) {
+			this.price = 300;
+		}else {
+			this.price = 400;
+		}
+
+
+	}
+	//printing using a constructor
+	public void getPizzaPrice() {
+		System.out.println(this.price);
+	}
 
 }
