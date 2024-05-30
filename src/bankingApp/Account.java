1829b0db1758ca70d0b48a4d0f8957546c7872e3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-30 12:06:30 -0400

    Made a simple banking app with oop

---
+++ src/bankingApp/Account.java
@@ -0,0 +1,60 @@
+package bankingApp;
+
+public class Account {
+	private String Number;
+	private double Balance;
+	private String  Name;
+	private String Email;
+	private	String Phone;
+	
+	//constructor
+	public  Account(String Number, double Balance, String  Name, String Email, String Phone) {
+		this.Number = Number;
+		this.Balance = Balance;
+		this.Name = Name;
+		this.Email = Email;
+		this.Phone = Phone;
+
+	}
+	
+	//deposit method 
+	public void depositemoney(double depositedmoney) {
+		this.Balance+=depositedmoney;
+		System.out.println("deposite done, new balance is " + this.Balance);
+	}
+	
+	
+	
+	//get and set methods
+	public String getNumber() {
+		return Number;
+	}
+	public void setNumber(String number) {
+		Number = number;
+	}
+	public double getBalance() {
+		return Balance;
+	}
+	public void setBalance(double balance) {
+		Balance = balance;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	
+}
---
+++ src/bankingApp/Main.java
@@ -0,0 +1,13 @@
+package bankingApp;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Account sai = new Account("12345", 0.0, "sai", "Saai@gmail.com", "99999");
+		
+	
+		sai.depositemoney(100);
+		
+		
+	}
+}
