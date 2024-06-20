5a501e3e2ef12aaa5c5ee839e27910918588e5af
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-20 12:59:34 +0530

    Properties files practice

--- src/properties_files_p53/PropPractice.java
+++ src/properties_files_p53/PropPractice.java
@@ -31,6 +31,7 @@ public class PropPractice {
 		prop.setProperty("username","sai41");
 		prop.setProperty("password","hyr123");
 
+		
 		//this is how you update a value 
 		prop.setProperty("password","saiiiiii");
 		prop.remove("password");
---
+++ src/properties_files_p53/PropPractice2.java
@@ -0,0 +1,18 @@
+package properties_files_p53;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PropPractice2 {
+	public static void main(String[] args) throws IOException {
+		
+		File abc  = new File("db.config2.properties");
+		
+		if(!abc.exists()) {
+			abc.createNewFile();
+		}
+		
+		System.out.print(abc.exists());
+
+	}
+}
