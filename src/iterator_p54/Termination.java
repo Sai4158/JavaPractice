9fa7cc6b7e3afa3cc0d999cad0b21189cc094cef
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-22 19:16:00 +0530

    Termination in java

--- src/iterator_p54/Termination.java
+++ src/iterator_p54/Termination.java
@@ -11,20 +11,25 @@ public class Termination {
 		//vector 
 		Vector<String> abc =  new Vector<String>();
 		
+		//add data to the vector
 		abc.add("Sai");
 		abc.add("String");
 		abc.add("abhi");
 		abc.add("pranav");
 		
-		
+		//load the data to Enumeration from vector
 		Enumeration<String> e = abc.elements();
+		
+		//if while has more element which will print true
+		//then it will print the next elements until there is no next element
 		
 		//this will return a boolean		
 		while(e.hasMoreElements()) {
 			//will print the next element
 			System.out.println(e.nextElement());
-
 		}
+		
+		
 		
 	}
 }
