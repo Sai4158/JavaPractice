42a5354d7189ee18256da0ec693896d6c711cf8e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-24 20:12:33 +0530

    Iterator using hash map

--- src/iterator_p54/IteratorPractice.java
+++ src/iterator_p54/IteratorPractice.java
@@ -1,6 +1,7 @@
 package iterator_p54;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import arrayList_Vector_P46.Arraylist;
@@ -27,6 +28,19 @@ public class IteratorPractice {
 		while(al.hasNext()) {
 			System.out.println(al.next());
 			al.remove();
+			
 		}
+		
+		//you cannot use iterator when using a map function 
+		HashMap<String, Integer> s1  = new HashMap<String, Integer>();
+		
+		s1.put("India", 500);
+		s1.put("efew", 700);
+		s1.put("rgr", 300);
+		
+		//this is how you apply iterator since it doesn't not apply directly 
+		s1.keySet().iterator();
+		s1.values().iterator();
+
 	}
 }
