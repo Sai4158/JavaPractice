1818985b2b07936674ff8785c50a833809ef3ccf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-26 17:52:12 +0530

    Project using a click listener

--- src/clickListenerProcject/Main.java
+++ src/clickListenerProcject/Main.java
@@ -3,5 +3,29 @@ package clickListenerProcject;
 public class Main {
 	public static void main(String[] args) {
 		
+		Button btnLogin = new Button();
+		
+		btnLogin.setOnClickListeners(new OnClickListener() {
+			
+			@Override
+			public void onclick() {
+				// TODO Auto-generated method stub
+				System.out.println("User is logged in");
+			}
+		});
+		
+		
+		
+		//this is for the button logout 
+		Button btnLogout = new Button();
+		btnLogout.setOnClickListeners(new OnClickListener() {
+			
+			@Override
+			public void onclick() {
+				// TODO Auto-generated method stub
+				System.out.println("User is logged  out");
+
+			}
+		});
 	}
 }
