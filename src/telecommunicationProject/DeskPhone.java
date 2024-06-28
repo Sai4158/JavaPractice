319702fb1c8ccfdf1601cd61bacbaaea138d2325
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-28 14:57:51 +0530

    Implemented methods from the interface

---
+++ src/telecommunicationProject/DeskPhone.java
@@ -0,0 +1,45 @@
+package telecommunicationProject;
+
+public class DeskPhone implements Telephone{
+
+	//Variables 
+	private String myNumber;
+	private boolean isRinging;
+	
+	//constructor 
+	public DeskPhone(String myNumber) {
+		this.myNumber = myNumber;
+	}
+
+	@Override
+	public void powerOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void number() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void phoneCall() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void anotherPhgone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ringing() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
