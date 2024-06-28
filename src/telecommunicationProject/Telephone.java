0e285d117ea2fc851695045819f6dedd27f9b0fe
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-28 14:51:24 +0530

    Telecommunication app using oops

---
+++ src/telecommunicationProject/Telephone.java
@@ -0,0 +1,18 @@
+package telecommunicationProject;
+
+public interface Telephone {
+	//power on
+	void powerOn();
+	
+	//dial a number 
+	void number();
+	
+	//answer phone call 
+	void phoneCall();
+	
+	//call another phone
+	void anotherPhgone();
+	
+	//is ringing 
+	void ringing();
+}
