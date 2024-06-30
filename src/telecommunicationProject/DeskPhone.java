685b6e9d5ea73a043ad7321f6ad73bd5f91f0323
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-01 01:25:21 +0530

    Updated code for Telecommunication App using oops

--- src/telecommunicationProject/DeskPhone.java
+++ src/telecommunicationProject/DeskPhone.java
@@ -5,56 +5,61 @@ public class DeskPhone implements Telephone{
 	//Variables 
 	private String myNumber;
 	private boolean isRinging;
-	
-	//constructor 
-	public DeskPhone(String myNumber) {
-		this.myNumber = myNumber;
-	}
-
 	@Override
 	public void powerOn() {
 		// TODO Auto-generated method stub
 		System.out.println("Desk phone always has power");
-	}
 
+	}
 	@Override
-	public void number( ) {
+	public void dail(String PhoneNumber) {
 		// TODO Auto-generated method stub
 		if(isRinging) {
-			System.out.println("Now ringing " + myNumber + " on deskphone ");
+			System.out.println("Now ringing " + PhoneNumber + " on deskphone ");
 			isRinging = false;
 		}		
 		else{
 			System.err.println("Phone is not ringing");
 		}
-	}
-
-	@Override
-	public void phoneCall() {
-		// TODO Auto-generated method stub
-		if(isRinging) {
-			System.out.println("Now ringing " + myNumber + " on deskphone ");
-			isRinging = false;
-		}		
-		else{
-			System.err.println("Phone is not ringing");
-		}
-	}
-
-	@Override
-	public void anotherPhgone() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void ringing() {
-		// TODO Auto-generated method stub
-		
 	}
 	
 	@Override
-    public boolean isRinging() {
-        return false;
-    }
+	public void answer() {
+		// TODO Auto-generated method stub
+		if(isRinging) {
+			System.out.println("Now ringing " + myNumber + " on deskphone ");
+			isRinging = false;
+		}		
+		else{
+			System.err.println("Phone is not ringing");
+		}
+	}
+	
+	
+	
+//	since this is boolean it must have return type of false or true
+	
+	@Override
+	public boolean CallPhone(String PhoneNumber) {
+		// TODO Auto-generated method stub
+		if(PhoneNumber == myNumber) {
+			System.out.println("Phone is ringing");
+		}else {
+			System.out.println("");
+			isRinging = false;
+		}
+		return false;
+	}
+	
+	
+	
+	@Override
+	public boolean isRinging() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+	
 }
+		
