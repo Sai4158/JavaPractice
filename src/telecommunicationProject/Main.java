6d87bd9490a14058fefd78428a9b1c9345794b47
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-01 23:39:07 +0530

    Working Telecommunication app using OOPS

--- src/telecommunicationProject/Main.java
+++ src/telecommunicationProject/Main.java
@@ -3,7 +3,7 @@ package telecommunicationProject;
 public class Main {
 	public static void main(String[] args) {
 		
-		DeskPhone phone  = new DeskPhone("23432434");
+		DeskPhone phone  = new DeskPhone("123456789");
 		
 		//Print out the methods using the objects
 		phone.powerOn();
@@ -12,5 +12,19 @@ public class Main {
 		phone.CallPhone("123456789");
 
 		phone.answer();
+		
+		
+//		This is for the mobile phone 
+		
+		System.out.println("----------------------------");
+		System.out.println("This is for the phone class");
+		
+		
+		//whatever is in the mobile class it needs to have the same num so it can call
+		MobileClass mobile = new MobileClass("1255234");
+		
+		mobile.powerOn();
+		mobile.callPhone("1255234");
+		mobile.answer();
 	}
 }	
