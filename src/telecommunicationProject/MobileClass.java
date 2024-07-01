b710da26c4c91d9568cdaa06507de63ab960f995
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-01 21:59:15 +0530

    Added methods to the mobile class

--- src/telecommunicationProject/MobileClass.java
+++ src/telecommunicationProject/MobileClass.java
@@ -1,7 +1,50 @@
 package telecommunicationProject;
 
 public class MobileClass {
-	public static void main(String[] args) {
-		
-	}
+
+    // variables 
+    private String myNumber;
+    private boolean isRinging;
+    private boolean isPowerOn;
+
+    // constructor
+    public MobileClass(String myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    public void powerOn() {
+        isPowerOn = true;
+        System.out.println("Phone Powered On");
+    }
+
+    public void dial(String phoneNumber) {
+        if(isPowerOn) {
+            System.out.println("Now ringing " + phoneNumber + " on mobile phone");
+        } else {
+            System.err.println("Phone is not powered on");
+        }
+    }
+
+    public void answer() {
+        if(isRinging) {
+            System.out.println("Answering the phone");
+            isRinging = false;
+        } else {
+            System.err.println("Phone is not ringing");
+        }
+    }
+
+    public boolean callPhone(String phoneNumber) {
+        if(phoneNumber.equals(myNumber) && isPowerOn) {
+            isRinging = true;
+            System.out.println("Phone is ringing");
+        } else {
+            isRinging = false;
+        }
+        return isRinging;
+    }
+
+    public boolean isRinging() {
+        return isRinging;
+    }
 }
