4bb4f0adaa40d5032cc32e7d91cc6d31e4ac4ff7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-19 19:44:05 +0530

    Inheritance practice (extends)

---
+++ src/aa_prac1/Numbers.java
@@ -0,0 +1,12 @@
+package aa_prac1;
+
+public class Numbers extends Nums {
+	public static void main(String[] args) {
+		
+//		Inheritance practice (extends)
+		Numbers numbb =  new Numbers();
+		
+		numbb.a();
+		numbb.b();
+	}
+}
---
+++ src/aa_prac1/Nums.java
@@ -0,0 +1,20 @@
+package aa_prac1;
+
+public class Nums {
+
+	public void  a() {
+		System.out.println("this is for a");
+	}
+	public void  b() {
+		System.out.println("this is for b");
+
+	}
+	public void  c() {
+		System.out.println("this is for c");
+
+	}
+	public void  d() {
+		System.out.println("this is for d");
+
+	}
+}
