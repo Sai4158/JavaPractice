b95e4fbe37d8775c547579c8b18f53aaad9c5b28
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-20 21:52:45 +0530

    Printing a square pattern using the for loop

---
+++ src/aa_prac1/Pattern1.java
@@ -0,0 +1,24 @@
+package aa_prac1;
+
+import java.util.Iterator;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		
+//		this is for the columns 
+		for (int i = 0; i <5; i++) {
+			
+//			this is for the rows
+			for (int j = 0; j <15; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+//***************
+//***************
+//***************
+//***************
+//***************
