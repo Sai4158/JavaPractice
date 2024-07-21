e538f8b4adf940fe677fe0dfccfcdc5de275ca20
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-21 23:35:47 +0530

    Reverse pattern using letters

---
+++ src/a_patterns/Pattern4.java
@@ -0,0 +1,16 @@
+package a_patterns;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		
+		int n = 10;
+		
+		for (int i = 1; i <= n; i++) {
+			
+			for (int j = n; j >= i; j--) {
+			System.out.print("a ");
+			}	
+		System.out.println("");
+		}
+	}
+}
