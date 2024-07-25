e5c3bcc66a414d13645239cde6287d1d9aa5a878
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-25 19:59:56 +0530

    Practicing with MVC

---
+++ src/mvc_project1/Student.java
@@ -0,0 +1,24 @@
+package mvc_project1;
+//this is a model class 
+
+public class Student {
+//	These are some variables 
+	private String  rollno;
+	private String name;
+	
+	
+	
+//	generate gets and sets 
+	public String getRollno() {
+		return rollno;
+	}
+	public void setRollno(String rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/mvc_project1/StudentView.java
@@ -0,0 +1,11 @@
+package mvc_project1;
+//This is student view class
+
+public class StudentView {
+	public void printStudentDetails(String studName, String studentRollNum) {
+//		we will print out the details 
+		System.out.println("Student Deatails: ");
+		System.out.println("Name : " + studName );
+		System.out.println("Roll Number : " + studentRollNum );
+	}
+}
