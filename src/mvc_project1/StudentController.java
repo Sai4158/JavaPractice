629a2af08837df722a368729972bc33cd4af8aff
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-25 23:57:50 +0530

    Fixes to MVC

--- src/mvc_project1/StudentController.java
+++ src/mvc_project1/StudentController.java
@@ -1,26 +1,38 @@
 package mvc_project1;
-// this is controller class 
 
 public class StudentController {
-// so the whole point of the controller is to control view and model objects 
-	
-//	model object 
-//	view object
-	
-	private  Student model;
-	private  StudentView view;
-	
-//	this is a student controller class
-	public StudentController(Student model,StudentView view ) {
-		this.model = model;
-		this.view =  view;
-	}
-	
-//	these are sets and gets 
-	public void setStudentName(String name) {
-		model.setName("sai");
-	}
-	public void setstudentRollNum(String rollno) {
-		model.setRollno("123");
-	}
+    // Model object
+    private Student model;
+    // View object
+    private StudentView view;
+
+    // Constructor
+    public StudentController(Student model, StudentView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    // Setter methods -  this is to control model objects
+    public void setStudentName(String name) {
+        model.setName(name);
+    }
+
+    public void setStudentRollNum(String rollno) {
+        model.setRollno(rollno);
+    }
+
+    // Getter methods
+    public String getStudentName() {
+        return model.getName();
+    }
+    public String getStudentRollNum() {
+        return model.getRollno();
+    }
+
+   
+
+    // Control object view types
+    public void updateView() {
+        view.printStudentDetails(model.getName(), model.getRollno());
+    }
 }
