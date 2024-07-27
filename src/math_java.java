52a7facb19867a2843ba08cc91811deb60ab7dcd
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-27 20:31:49 +0530

    Max and min using a for loop in java

--- src/math_java.java
+++ src/math_java.java
@@ -6,6 +6,11 @@ public class math_java {
 		
 		double a = 5.434;
 		
+		int[] numbers = {5, 2, 9, 1, 7};
+
+        int min = numbers[0];
+        int max = numbers[0];
+	        
 //		this rounded will print 5
 		System.out.println("Round: "+Math.round(a));
 		
@@ -14,5 +19,15 @@ public class math_java {
 		
 //		this is will change to next decimal if there is decimal point
 		System.out.println("Ceil: "+Math.ceil(a));
+		
+		
+//		this loop print max and min in java
+		for (int i = 1; i < numbers.length; i++) {
+            min = Math.min(min, numbers[i]);
+            max = Math.max(max, numbers[i]);
+        }
+
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
 	}
 }
