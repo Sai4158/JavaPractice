4fc6aa837bc08bc5e2cec2a165cbc369297ca9ad
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-27 21:19:38 +0530

    Upside down right triangle

--- src/math_java.java
+++ src/math_java.java
@@ -1,33 +1,71 @@
+import java.util.Iterator;
 
 public class math_java {
 	public static void main(String[] args) {
-		
+
 		//this is where I will write in the math code 
-		
+
 		double a = 5.434;
-		
+
 		int[] numbers = {5, 2, 9, 1, 7};
 
-        int min = numbers[0];
-        int max = numbers[0];
-	        
-//		this rounded will print 5
-		System.out.println("Round: "+Math.round(a));
-		
-//		this is will remove the decimals points 
-		System.out.println("Floor: "+Math.floor(a));
-		
-//		this is will change to next decimal if there is decimal point
-		System.out.println("Ceil: "+Math.ceil(a));
-		
-		
-//		this loop print max and min in java
-		for (int i = 1; i < numbers.length; i++) {
-            min = Math.min(min, numbers[i]);
-            max = Math.max(max, numbers[i]);
-        }
+		int min = numbers[0];
+		int max = numbers[0];
 
-        System.out.println("Minimum value: " + min);
-        System.out.println("Maximum value: " + max);
+		//		this rounded will print 5
+		System.out.println("Round: "+Math.round(a));
+
+		//		this is will remove the decimals points 
+		System.out.println("Floor: "+Math.floor(a));
+
+		//		this is will change to next decimal if there is decimal point
+		System.out.println("Ceil: "+Math.ceil(a));
+
+
+		//		this loop print max and min in java
+		for (int i = 1; i < numbers.length; i++) {
+			min = Math.min(min, numbers[i]);
+			max = Math.max(max, numbers[i]);
+		}
+
+		System.out.println("Minimum value: " + min);
+		System.out.println("Maximum value: " + max);
+
+
+		//        simple triangle 
+		int t = 5;
+
+		for (int i = 0; i < t; i++) {
+			
+			for (int j = i; j < t; j++) {
+				System.out.print(" ");
+
+			}
+			
+			for (int j = 0; j < j; j++) {
+				System.out.print("% ");
+
+			}
+			
+			System.out.println();
+		}
+
+
+
+
+		//        this is right upside down rigth triangle 
+		int n = 5;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(" ");
+
+			}
+			for (int j = i; j < n; j++) {
+				System.out.print("^ ");
+
+			}
+			System.out.println();
+		}
 	}
 }
