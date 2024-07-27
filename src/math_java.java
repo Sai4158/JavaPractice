b11bedf86944433e9a050a44cd78c2454a4a9c0b
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-27 20:20:28 +0530

    Math in java, floor, round and ceil

---
+++ src/math_java.java
@@ -0,0 +1,18 @@
+
+public class math_java {
+	public static void main(String[] args) {
+		
+		//this is where I will write in the math code 
+		
+		double a = 5.434;
+		
+//		this rounded will print 5
+		System.out.println("Round: "+Math.round(a));
+		
+//		this is will remove the decimals points 
+		System.out.println("Floor: "+Math.floor(a));
+		
+//		this is will change to next decimal if there is decimal point
+		System.out.println("Ceil: "+Math.ceil(a));
+	}
+}
