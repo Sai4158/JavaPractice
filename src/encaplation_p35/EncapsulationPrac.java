4e17d0daa6f871d76776b2e6654516446807a2a0
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-29 01:31:45 +0530

    encapsulation practice add variables and get and setters.

---
+++ src/encaplation_p35/EncapsulationPrac.java
@@ -0,0 +1,40 @@
+package encaplation_p35;
+
+import java.util.Scanner;
+
+public class EncapsulationPrac {
+
+//	these are the variables 
+	String car;
+	int carNumber;
+	String carColor;
+	
+	
+//	setters and getters here  
+	public String getCar() {
+		return car;
+	}
+	public void setCar(String car) {
+		this.car = car;
+	}
+	public int getCarNumber() {
+		return carNumber;
+	}
+	public void setCarNumber(int carNumber) {
+		this.carNumber = carNumber;
+	}
+	public String getCarColor() {
+		return carColor;
+	}
+	public void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+	
+	
+	
+//	this is the main method 
+	
+
+	
+	
+}
