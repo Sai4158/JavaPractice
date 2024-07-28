535b15db2ed9c0a8387a3214c087caecbd07486b
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-29 01:39:05 +0530

    printing the values using encapsulation

--- src/encaplation_p35/EncapsulationPrac.java
+++ src/encaplation_p35/EncapsulationPrac.java
@@ -34,7 +34,18 @@ public class EncapsulationPrac {
 	
 //	this is the main method 
 	
-
+	public static void main(String[] args) {
+		EncapsulationPrac h1 = new EncapsulationPrac();
+		
+		h1.setCar("BMW");
+		h1.setCarColor("Gray");
+		h1.setCarNumber(9999);
+		
+		System.out.println("this is the car model: " + 
+		h1.car + ", this is the car color: " + 
+				h1.carColor +" and this is car number "+ 
+		h1.carNumber);
+	}
 	
 	
 }
