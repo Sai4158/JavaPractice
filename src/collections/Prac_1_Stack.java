a5fb305ebceb842d905a5b3cd220fb0fb76cffc6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-30 00:14:34 +0530

    Added more methods for stack

--- src/collections/Prac_1_Stack.java
+++ src/collections/Prac_1_Stack.java
@@ -11,15 +11,25 @@ public class Prac_1_Stack {
 		s1.add("2");
 		s1.add("3");
 
+//		print all the values 
+		System.out.println(s1);
+		
+//		this will set it at index  0 
+		s1.set(0, "0");
 		
 //		meaning this is LIFO - last in first out 
 		System.out.println(s1.peek());
 		
-
+//		this will remove LIFO
+		System.out.println(s1.pop());
 		
 //		this will print out the array of the string, in order
 		System.out.println(s1);
 		
+//		this will clear the list 
+		s1.clear();
+		
+		System.out.println(s1);
 		
 	}
 }
