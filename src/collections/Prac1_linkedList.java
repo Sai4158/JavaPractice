9b989d5f385dc2e47da9174e8e5b8c2fb7974b27
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-30 01:32:55 +0530

    LinkedList practice

--- src/collections/Prac1_linkedList.java
+++ src/collections/Prac1_linkedList.java
@@ -8,14 +8,24 @@ public class Prac1_linkedList {
 //		this is the linked list  -  Can be FIFO or LIFO
 		
 		LinkedList<Integer> L1 = new LinkedList<Integer>();
+		LinkedList<Integer> L2 = new LinkedList<Integer>();
 		
+		L2.add(124);
+
 		L1.add(1);
 		L1.add(2);
 		L1.add(3);
 		
-		System.out.println(L1);
 		
-//		this first in first out
+		
+		System.out.println(L1);
+				
+//		this first in first out - FIFO
 		System.out.println(L1.peek());
+		
+//		Using add all method to add all the 
+		L1.addAll(L2);
+		System.out.println(L1);
+
 	}
 }
