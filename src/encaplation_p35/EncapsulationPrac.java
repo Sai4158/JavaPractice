63541123ffface0133a204b8cc61cff8f78378cd
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-29 14:16:59 +0530

    Used static class and sperate class to run the program

--- src/encaplation_p35/EncapsulationPrac.java
+++ src/encaplation_p35/EncapsulationPrac.java
@@ -1,66 +1,65 @@
-package encaplation_p35;
+package encapsulation_p35;
 
 import java.util.Scanner;
 
 public class EncapsulationPrac {
 
-//	these are the variables 
-	String car;
-	int carNumber;
-	String carColor;
-	
-	
-//	setters and getters here  
-	public String getCar() {
-		return car;
-	}
-	public void setCar(String car) {
-		this.car = car;
-	}
-	public int getCarNumber() {
-		return carNumber;
-	}
-	public void setCarNumber(int carNumber) {
-		this.carNumber = carNumber;
-	}
-	public String getCarColor() {
-		return carColor;
-	}
-	public void setCarColor(String carColor) {
-		this.carColor = carColor;
-	}
-	
+    // These are the variables 
+    private String car;
+    private int carNumber;
+    private String carColor;
+
+    // Setters and getters here  
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public int getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(int carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    // This is the main method 
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.run();
+    }
+
+    // Printer class to handle input and output
+    static class Printer {
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            EncapsulationPrac h1 = new EncapsulationPrac();
+
+            System.out.print("Enter car model: ");
+            h1.setCar(scanner.nextLine());
+
+            System.out.print("Enter car color: ");
+            h1.setCarColor(scanner.nextLine());
+
+            System.out.print("Enter car number: ");
+            h1.setCarNumber(scanner.nextInt());
+
+            System.out.println("This is the car model: " + h1.getCar() + 
+                ", this is the car color: " + h1.getCarColor() + 
+                " and this is the car number: " + h1.getCarNumber());
+            
+            scanner.close();
+        }
+    }
 }
-	
-//	this is the main method 
-	
-	 class printer {
-		Scanner scanner = new Scanner(System.in);
-        EncapsulationPrac h1 = new EncapsulationPrac();
-
-        System.out.print("Enter car model: ");
-        h1.setCar(scanner.nextLine());
-
-        System.out.print("Enter car color: ");
-        h1.setCarColor(scanner.nextLine());
-
-        System.out.print("Enter car number: ");
-        h1.setCarNumber(scanner.nextInt());
-
-        System.out.println("This is the car model: " + h1.getCar() + 
-            ", this is the car color: " + h1.getCarColor() + 
-            " and this is the car number: " + h1.getCarNumber());
-        
-        scanner.close();
-
-	}
-	
-		
-	public static void main(String[] args) {
-		printer main = new printer();
-		
-		
-	}
-	
-	
-
