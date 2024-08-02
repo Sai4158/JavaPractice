06a060d18532b5d70cbc71f5ac672477251f0222
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-02 10:56:26 +0530

    Even nums using object creation

--- src/aa_prac1/EvenNums.java
+++ src/aa_prac1/EvenNums.java
@@ -2,18 +2,30 @@ package aa_prac1;
 
 import java.util.Iterator;
 
-//this program will only print even numbers
+//this program will only print even numbd
 
 public class EvenNums {
 	
-	
 	public static void main(String[] args) {
 		
-		int maxnums = 100;
-		for (int i = 0; i < maxnums; i++) {
-			if(i % 2 == 0) {
-				System.out.println(i);
-			}
+//		object creation
+		EvenNums even =  new EvenNums();
+		
+		even.evennums();
+		
+	}
+	
+
+//	this will print the even nums 
+public void evennums() {
+	int maxnums = 100;
+	System.out.println();
+	for (int i = 0; i < maxnums; i++) {
+		if(i % 2 == 0) {
+			System.out.println(i);
 		}
 	}
-}	
+}
+
+
+}
