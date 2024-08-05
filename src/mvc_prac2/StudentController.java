ee38e206e9d9121d99ef39e076640ff5f01c0ec5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-05 13:37:23 +0530

    Added controller code

--- src/mvc_prac2/StudentController.java
+++ src/mvc_prac2/StudentController.java
@@ -2,4 +2,38 @@ package mvc_prac2;
 
 public class StudentController {
 
+	//	get the class names 
+	private StudentModel model;
+	private StudentView view;
+
+
+	//	then add a constructor 
+	public StudentController(StudentModel model, StudentView view) {
+		this.model = model;
+		this.view =  view;
+		
+	}
+
+	
+//	set and get
+	public StudentModel getModel() {
+		return model;
+	}
+
+
+	public void setModel(StudentModel model) {
+		this.model = model;
+	}
+
+
+	public StudentView getView() {
+		return view;
+	}
+
+
+	public void setView(StudentView view) {
+		this.view = view;
+	}
+
+	
 }
