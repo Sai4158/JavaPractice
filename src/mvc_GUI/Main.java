2bd17b4bc74c5365192b51d70931cc9ac6ddf007
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-08 18:58:48 +0530

    added main method to print out the code

--- src/mvc_GUI/Main.java
+++ src/mvc_GUI/Main.java
@@ -3,5 +3,10 @@ package mvc_GUI;
 public class Main {
 	public static void main(String[] args) {
 		
+		 StudentModel model = new StudentModel();
+		 StudentView view = new StudentView();
+		 StudentController controller = new StudentController(model, view);
+		 
+		
 	}
 }
--- src/mvc_GUI/StudentController.java
+++ src/mvc_GUI/StudentController.java
@@ -1,5 +1,20 @@
 package mvc_GUI;
 
-public class StudentController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class StudentController {
+	
+//	variables
+	private StudentModel model;
+	private StudentView view;
+	
+//	make a constructor then bring in the variables 
+	public StudentController(StudentModel model, StudentView view) {
+		this.model = model;
+		this.view = view;
+	
+	}
+	
+//	this is class for the to update the button and show it on the screen
 }
