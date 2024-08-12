1ed1e7c05904ee7266ed3dc3fa498cbdbf70681d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-12 22:38:54 +0530

    Setting a GUI

---
+++ src/a_GUI_Prac/Prac1.java
@@ -0,0 +1,18 @@
+package a_GUI_Prac;
+
+import javax.swing.*;
+
+public class Prac1 extends JFrame {
+	
+	public void build() {
+//		setting up the window 
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(700, 500);
+		
+//		make a JPanel
+		 JPanel test = new JPanel();
+		 
+		 
+	}
+}
