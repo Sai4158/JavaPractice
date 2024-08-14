14ee0789db1f9706caf47627b1b83803db1fa79f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-14 13:25:54 +0530

    View class done

--- src/gui_mvc_tut/CalculatorView.java
+++ src/gui_mvc_tut/CalculatorView.java
@@ -1,5 +1,7 @@
 package gui_mvc_tut;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 
@@ -25,7 +27,71 @@ public class CalculatorView extends JFrame{
 		this.setVisible(true);
 		this.setLayout(null);
 		
+//		now add the stuff to the pannels
 		
+		calcPannel.add(firstNum);
+		calcPannel.add(additionLabel);
+		calcPannel.add(secondNum);
+		calcPannel.add(sum);
+		calcPannel.add(calcution);
+		
+		this.add(calcPannel); 
+	}
+
+
+	public JTextField getFirstNum() {
+		return firstNum;
+	}
+
+
+	public void setFirstNum(JTextField firstNum) {
+		this.firstNum = firstNum;
+	}
+
+
+	public JLabel getAdditionLabel() {
+		return additionLabel;
+	}
+
+
+	public void setAdditionLabel(JLabel additionLabel) {
+		this.additionLabel = additionLabel;
+	}
+
+
+	public JTextField getSecondNum() {
+		return secondNum;
+	}
+
+
+	public void setSecondNum(JTextField secondNum) {
+		this.secondNum = secondNum;
+	}
+
+
+	public JButton getSum() {
+		return sum;
+	}
+
+
+	public void setSum(JButton sum) {
+		this.sum = sum;
+	}
+
+
+	public JTextField getCalcution() {
+		return calcution;
+	}
+
+
+	public void setCalcution(JTextField calcution) {
+		this.calcution = calcution;
+	}
+	
+	
+	void addCalculationListener(ActionListener listenForCalcButton) {
+		
+		calcution.addActionListener(listenerForCalcButton);
 	}
 	
 	
