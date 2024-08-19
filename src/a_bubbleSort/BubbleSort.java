7004ab7b21a73eff4180f36e7fac244120d748cb
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-20 02:46:37 +0530

    Bubble sort Correct code

--- src/a_bubbleSort/BubbleSort.java
+++ src/a_bubbleSort/BubbleSort.java
@@ -1,23 +1,33 @@
 package a_bubbleSort;
 
 public class BubbleSort {
-	public static void main(String[] args) {
-		
-		int arr[]  = {7,8,3,1,2};
-		
-//		bubble sort 
-	for (int i = 0; i < arr.length-1; i++) {
-		
-		for (int j = 0; j<arr.length-i-1; j++) {
-			
-			if(arr[j]>arr[j+1]) {
-//				swap
-				int temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1]=temp;
-				
-			}
-		}
-	}
-  }	
+	
+    // Method to print the array
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(); // Move to the next line after printing the array
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 3, 1, 2};
+
+        // Bubble Sort
+        for (int i = 0; i < arr.length - 1; i++) {
+        	
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print the sorted array
+        printArray(arr);
+    }
 }
