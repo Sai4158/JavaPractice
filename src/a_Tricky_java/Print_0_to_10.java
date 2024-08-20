0a1d585a40a0cae98536d9d02bc7381021f6f9c5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-20 22:47:03 +0530

    Printing nums using a loop

---
+++ src/a_Tricky_java/Print_0_to_10.java
@@ -0,0 +1,20 @@
+package a_Tricky_java;
+
+import java.util.Iterator;
+
+public class Print_0_to_10 {
+	public static void main(String[] args) {
+		
+//		use <= to print till 5 or it will print till 4
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+	}
+}
+
+//1
+//2
+//3
+//4
+//5
