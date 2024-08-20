aac92cec421ac1554c81af3a01769d4035649799
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-20 14:44:23 +0530

    Bubble sort practice

---
+++ src/a_bubbleSort/BubbleSort_1.java
@@ -0,0 +1,19 @@
+package a_bubbleSort;
+
+public class BubbleSort_1 {
+	public static void main(String[] args) {
+		
+		int AbcArr[] = {4,5,7,4,3,7,1};
+
+		for (int i = 0; i < AbcArr.length -1; i++) {
+			
+			for (int j = 0; j < AbcArr.length - i -1; j++) {
+				if(AbcArr[j] > AbcArr[j+1]) {
+					int temp = AbcArr[];
+					  arr[j] = arr[j + 1];
+	                    arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+}
