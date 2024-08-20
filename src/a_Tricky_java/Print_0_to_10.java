fff548f5a9e831cc746306116708c26c453f6fbf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-20 22:50:07 +0530

    Added while loop

--- src/a_Tricky_java/Print_0_to_10.java
+++ src/a_Tricky_java/Print_0_to_10.java
@@ -6,13 +6,31 @@ public class Print_0_to_10 {
 	public static void main(String[] args) {
 		
 //		use <= to print till 5 or it will print till 4
-		
+//		using for loop 
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
+		}
+		
+		
+//		new line 
+		System.out.println("Added while loop");
+		
+//		while loop
+		int t =0;
+		while (t<=5) {
+		System.out.println(t);	
+		t++;
 		}
 	}
 }
 
+//1
+//2
+//3
+//4
+//5
+//Added while loop
+//0
 //1
 //2
 //3
