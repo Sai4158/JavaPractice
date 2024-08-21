5a4f0201d4275ba474cbf54e673474905afe58e8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-21 19:00:10 +0530

    printing a solid rectangle

---
+++ src/patterns/SolidRectanglePattern.java
@@ -0,0 +1,19 @@
+package patterns;
+// printing a solid rectangle 
+public class SolidRectanglePattern {
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <=20; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+//********************
+//********************
+//********************
+//********************
