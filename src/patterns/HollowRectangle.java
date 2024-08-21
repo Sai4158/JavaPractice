c9e494787fd55f849ddd026aec3fbde37becf727
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-21 19:26:30 +0530

    Added comments

--- src/patterns/HollowRectangle.java
+++ src/patterns/HollowRectangle.java
@@ -11,18 +11,21 @@ public class HollowRectangle {
 		for (int i = 1; i <=n; i++) {
 
 			for (int j = 1; j <=m; j++) {
-
+				
+//				if its i =1 = start , then i = n end then also print
 				if(i == 1|| j == 1 || i == n ||j == m) {
 					System.out.print("^");
 				}
+//				if it doesn't match then print a empty space 
 				else {
 					System.out.print(" ");
 				}
 			}
 			System.out.println();
 		}
-		
+
 	}
+
 }
 
 //^^^^^
