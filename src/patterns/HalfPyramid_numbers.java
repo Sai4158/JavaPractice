8aef5661ee89a64567fdfcc58997660e5294c171
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-25 00:02:14 -0400

    Added space

--- src/patterns/HalfPyramid_numbers.java
+++ src/patterns/HalfPyramid_numbers.java
@@ -12,15 +12,16 @@ public class HalfPyramid_numbers {
 			for (int j = 1; j < i; j++) {
 //				print j loop for 12345 
 //				print i loop for 1, 22, 333, 444, 55555
-				System.out.print(j);
+				System.out.print(j +" ");
+				
 			}
 			System.out.println("");
 		}
 	}
 }
 
-//1
-//12
-//123
-//1234
-//12345
+//1 
+//1 2 
+//1 2 3 
+//1 2 3 4 
+//1 2 3 4 5 
