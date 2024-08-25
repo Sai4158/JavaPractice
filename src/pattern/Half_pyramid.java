a72ce3fb4ac40ebf93803488c5e65597783f0dd0
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-24 23:22:35 -0400

    half pyramid pratice

---
+++ src/pattern/Half_pyramid.java
@@ -0,0 +1,21 @@
+package pattern;
+// half pyramid
+public class Half_pyramid {
+		public static void main(String[] args) {
+			
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("5");
+					
+					
+				}
+				System.out.println();
+			}
+		}
+}
+
+//
+//5
+//55
+//555
+//5555
