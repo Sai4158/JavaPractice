01b9687ee1854de78ba9e5056a11bee1cb565589
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-27 12:22:10 -0400

    Updated

--- src/ist_prac/Methods.java
+++ src/ist_prac/Methods.java
@@ -2,34 +2,34 @@ package ist_prac;
 
 import java.util.ArrayList;
 
-
-
 public class Methods {
 	
-//	int because I am returning the int
+	// int because I am returning the int
 	public static int multiplyIntegers(ArrayList<Integer> list) {
-		
-		int result =1;
+		int result = 1;
 		
 		for (int i = 0; i < list.size(); i++) {
-			result =  result * list.get(i);	
+			result = result * list.get(i);	
 		}
 		return result;
 	}
 	
 	public static int multiplyIntegers(int first) {
-		return first*1;
-
+		return first * 1;
 	}
 	
 	public static int multiplyIntegers(int first, int second) {
-		return first* second;
-
+		return first * second;
 	}
+
 	public static void main(String[] args) {
 		int ab =  1;
 		int cd = 3;
 		
-		multiplyIntegers(ab, cd);
+		// Store the result of multiplying two integers
+		int result = multiplyIntegers(ab, cd);
+		
+		// Print the result
+		System.out.println(result);
 	}
 }
