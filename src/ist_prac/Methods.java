1d5cb601c52674b30feaafda3dc98f813e894ff6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-27 11:20:05 -0400

    Array list and method recap

---
+++ src/ist_prac/Methods.java
@@ -0,0 +1,35 @@
+package ist_prac;
+
+import java.util.ArrayList;
+
+
+
+public class Methods {
+	
+//	int because I am returning the int
+	public static int multiplyIntegers(ArrayList<Integer> list) {
+		
+		int result =1;
+		
+		for (int i = 0; i < list.size(); i++) {
+			result =  result * list.get(i);	
+		}
+		return result;
+	}
+	
+	public static int multiplyIntegers(int first) {
+		return first*1;
+
+	}
+	
+	public static int multiplyIntegers(int first, int second) {
+		return first* second;
+
+	}
+	public static void main(String[] args) {
+		int ab =  1;
+		int cd = 3;
+		
+		multiplyIntegers(ab, cd);
+	}
+}
