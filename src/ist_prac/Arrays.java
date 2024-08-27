9bcbe6e7faf1d0583078a7f8d4707f7970847343
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-27 15:39:44 -0400

    adding elements to the array

---
+++ src/ist_prac/Arrays.java
@@ -0,0 +1,30 @@
+package ist_prac;
+
+public class Arrays {
+	
+	public static void main(String[] args) {
+		
+		int [] ints;
+		
+		ints = new int [10];
+		
+		
+		// adding the ints 
+		ints[0] = 1;
+		ints[1] = 2;
+		ints[2] = 3;
+		ints[3] = 4;
+		ints[4] = 5;
+		ints[5] = 6;
+		ints[6] = 7;
+		ints[7] = 8;
+		ints[8] = 9;
+		ints[9] = 10;
+		
+//		printing out 
+		
+		for (int i = 0; i < ints.length; i++) {
+			System.out.println(ints[i]);
+		}
+	}
+}
