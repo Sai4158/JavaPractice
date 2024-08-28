f9c711c14073703f53add954034a2fddd2e891a9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-27 20:25:01 -0400

    IllegalArgumentException in java

--- src/ist_prac/Catch_error.java
+++ src/ist_prac/Catch_error.java
@@ -5,7 +5,22 @@ import java.util.Scanner;
 
 public class Catch_error {
 	
+	public static void amethod(int mustBeOne ) throws IllegalArgumentException {
+		if(mustBeOne !=1) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
 	public static void main(String[] args) {
+		
+	
+		
+		try {
+			amethod(1);
+			System.out.println("Good your are user 1");
+		}catch(IllegalArgumentException ex) {
+			System.out.println("Sorry you are not user 1 ");
+		}
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -17,7 +32,5 @@ public class Catch_error {
 		} catch(InputMismatchException ex) {
 			System.out.println("Try again thats not a int.");
 		}
-		
-		
 	}
 }
