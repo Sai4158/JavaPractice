c64f1d4b7d12a3e33872e50567fd0ff352e16951
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-30 16:26:57 -0400

    Printing methods and using debugger

--- src/ist_prac/Debugging.java
+++ src/ist_prac/Debugging.java
@@ -1,5 +1,5 @@
 package ist_prac;
-
+// double click on the left blue side navbar to debug
 public class Debugging {
 	public static void main(String[] args) {
 		
@@ -7,7 +7,19 @@ public class Debugging {
 		String world = String.format("%s World", hello);
 				
 		System.out.println(world);
+		//Hello World
+		
+		
+		
+//		----------
+		System.out.println(concat("Hello", "I am Sai"));
 	}
+	
+	
+	public static String concat(String a, String b) {
+		return String.format("%s,%s", a, b);
+	
+	}
+
 }
 
-//Hello World
