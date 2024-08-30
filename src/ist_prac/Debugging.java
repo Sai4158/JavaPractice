a6679b431fea5f3f41310711bbe0d6be20b0a4f3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-30 16:11:30 -0400

    Printing using String.format

---
+++ src/ist_prac/Debugging.java
@@ -0,0 +1,13 @@
+package ist_prac;
+
+public class Debugging {
+	public static void main(String[] args) {
+		
+		String hello = "Hello";
+		String world = String.format("%s World", hello);
+				
+		System.out.println(world);
+	}
+}
+
+//Hello World
