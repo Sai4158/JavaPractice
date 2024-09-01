97a5bd6271fa553e25408e3d6ff40c7dcd3f2649
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-01 19:59:48 -0400

    IST classes recap using set and get

---
+++ src/ist_prac_classes/Actor.java
@@ -0,0 +1,23 @@
+package ist_prac_classes;
+
+public class Actor {
+
+		private String firstName;
+		private String lastName;
+		
+		
+		
+//		get and set methods to access the instanceVariable on the main class 
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+}
---
+++ src/ist_prac_classes/Main.java
@@ -0,0 +1,14 @@
+package ist_prac_classes;
+
+public class Main {
+	public static void main(String[] args) {
+		Actor abc = new Actor();
+		
+		abc.setFirstName("Sai");
+		abc.setLastName(" Rang");
+		
+		System.out.println(abc.getFirstName() + abc.getLastName());
+	}
+}
+
+//Sai Rang
