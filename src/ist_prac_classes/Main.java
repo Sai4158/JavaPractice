36a8f9541ccd1f96511c5ef840c8c6960afdd5cd
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-01 21:51:47 -0400

    Classes in Classes - takes Actor name and then uses it for the movie class

--- src/ist_prac_classes/Main.java
+++ src/ist_prac_classes/Main.java
@@ -1,11 +1,27 @@
 package ist_prac_classes;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
 		Actor abc = new Actor("Sai", " Rang");
 		
 		
 		System.out.println(abc.getFirstName() + abc.getLastName());
+		
+		System.out.println();
+		
+		
+		// Create an ArrayList to hold Actor objects
+        ArrayList<Actor> actors = new ArrayList<Actor>();
+     // Add the actor to the list from above
+        actors.add(abc);  
+
+        // Create a Movie object
+        Movie aMovie = new Movie("Hello", "PG-13", 120, "Action", actors);
+
+        // Print the movie details
+        System.out.println(aMovie);
 	}
 }
 
