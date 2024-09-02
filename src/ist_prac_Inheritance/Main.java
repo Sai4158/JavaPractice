ba51112369e2a15b4a4e3e2f8ea8f6d8f18c335e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-02 00:01:23 -0400

    added a array list

--- src/ist_prac_Inheritance/Main.java
+++ src/ist_prac_Inheritance/Main.java
@@ -1,13 +1,20 @@
 package ist_prac_Inheritance;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
 		
-        Dog dog = new Dog("Duke", "German Shepherd");
-		
+        Dog dog1 = new Dog("Duke", "German Shepherd");
+        Dog dog2 = new Dog("Aston", "golden retriver");
+
 		System.out.println(dog);
 		System.out.println(dog.doTrick("sit"));
 		System.out.println(dog.makeSound());
+		
+		ArrayList<Pet> pets = new ArrayList<Pet>();
+		pets.add(dog1);
+		pets.add(dog2);
 	}
 }
 
