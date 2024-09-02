458136eb038610b4c487637c82c2bc8f31fd08e6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-01 20:54:05 -0400

    toString in java - classes in classes

--- src/ist_prac_classes/Movie.java
+++ src/ist_prac_classes/Movie.java
@@ -3,29 +3,76 @@ package ist_prac_classes;
 import java.util.ArrayList;
 
 public class Movie {
-	
-	private String  title;
-	private String  mpaaRating;
-	private Integer  lenght;
-	private String  genre;
-//	 Arraylist then put the class inside that you want 
-	private ArrayList<Actor> actor;
-	
-	
-	public Movie(String title , String  mpaaRating, Integer lenght, String genre, ArrayList<Actor> actor) {
-		
-		this.title  = title;
-		this.mpaaRating =  mpaaRating;
-		this.lenght = lenght;
-		
-		
-//		movie length needs to more than 0 otherwise throw illegal error
-		
-		if(lenght >0) {
-			this.lenght = lenght;
-		}else {
-			throw new IllegalArgumentException("The lenght need to more that 0");
-		}
-		
-	}
+    
+    private String title;
+    private String mpaaRating;
+    private Integer length;
+    private String genre;
+    private ArrayList<Actor> actors;
+
+    public Movie(String title, String mpaaRating, Integer length, String genre, ArrayList<Actor> actors) {
+        this.title = title;
+        this.mpaaRating = mpaaRating;
+        
+        // movie length needs to be more than 0, otherwise throw an illegal argument error
+        if (length > 0) {
+            this.length = length;
+        } else {
+            throw new IllegalArgumentException("The length needs to be more than 0");
+        }
+        
+        this.genre = genre;
+        this.actors = actors;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setMpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        if (length > 0) {
+            this.length = length;
+        } else {
+            throw new IllegalArgumentException("The length needs to be more than 0");
+        }
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public ArrayList<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(ArrayList<Actor> actors) {
+        this.actors = actors;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie [title=" + title + ", mpaaRating=" + mpaaRating + ", length=" + length + ", genre=" + genre
+                + ", actors=" + actors + "]";
+    }
 }
