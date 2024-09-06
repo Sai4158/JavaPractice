b9a2adee765e45234cf8ff250caf9f4a60c63dc7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-06 18:17:50 -0400

    Added Student 1 class data

---
+++ src/ist_day2_recap/Student1.java
@@ -0,0 +1,47 @@
+package ist_day2_recap;
+
+public class Student1 {
+	
+//	instance variables 
+	private String name;
+	private	double gpa;
+	
+//	constructors 
+	public Student1(String name, double gpa) {
+		this.name = name;
+		this.gpa = gpa;
+	}
+
+	
+	//	get methods 
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getGpa() {
+		return gpa;
+	}
+
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	
+//	to String 
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+
+
+	
+	
+}
