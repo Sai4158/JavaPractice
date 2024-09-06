56f12a95fc63ca82a3c4f4b9bbb61b623bec6500
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-06 19:30:06 -0400

    Using Comparable in java

--- src/ist_day2_recap/Student1.java
+++ src/ist_day2_recap/Student1.java
@@ -1,18 +1,19 @@
 package ist_day2_recap;
 
-public class Student1 {
-	
-//	instance variables 
+// this is how you can add the comparable tyoe
+public class Student1 implements Comparable<Student1>{
+
+	//	instance variables 
 	private String name;
 	private	double gpa;
-	
-//	constructors 
+
+	//	constructors 
 	public Student1(String name, double gpa) {
 		this.name = name;
 		this.gpa = gpa;
 	}
 
-	
+
 	//	get methods 
 	public String getName() {
 		return name;
@@ -33,15 +34,28 @@ public class Student1 {
 		this.gpa = gpa;
 	}
 
-	
-//	to String 
+
+	//	to String 
 	@Override
 	public String toString() {
 		return "name= " + name + ", gpa= " + gpa;
 	}
 
+	//	add the unimplemented methods 
+	@Override
+	public int compareTo(Student1 o) {
+
+		if (this.gpa < o.gpa) {
+			return -1; // Current student has lower GPA
+		} else if (this.gpa == o.gpa) {
+			return 0;  // GPAs are equal
+		} else {
+			return 1;  // Current student has higher GPA
+		}
+	}
 
 
-	
-	
+
+
+
 }
--- src/ist_day2_recap/TesterClass.java
+++ src/ist_day2_recap/TesterClass.java
@@ -1,14 +1,24 @@
 package ist_day2_recap;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class TesterClass {
 	public static void main(String[] args) {		
 		ArrayList<Student1> students =  new ArrayList<Student1>();
 		students.add(new Student1("Sai",3.97));
+		students.add(new Student1("wqd",4.04));
+		students.add(new Student1("vds",2.65));
 		
 		System.out.println(students);
+		
+		Collections.sort(students);
+		
+		System.out.println(students);
+
 	}
 }
 
-//name= Sai, gpa= 3.93
+//[name= Sai, gpa= 3.97, name= wqd, gpa= 4.04, name= vds, gpa= 2.65]
+//[name= vds, gpa= 2.65, name= Sai, gpa= 3.97, name= wqd, gpa= 4.04]
