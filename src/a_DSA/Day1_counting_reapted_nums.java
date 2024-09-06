4bb0c25b76f6c294e6ebee8e244a43e1118a1edc
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-05 23:17:25 -0400

    Added wording

--- src/a_DSA/Day1_counting_reapted_nums.java
+++ src/a_DSA/Day1_counting_reapted_nums.java
@@ -4,20 +4,21 @@ public class Day1_counting_reapted_nums {
 	
 	public static void main(String[] args) {
 		
+		int number = 3;
+		
 //		counting all the matching elements in the array 
 		
 		int arr[] = {4,3,3,4,2,32,5,12,12,3,2,3};
-		
 		int count = 0;
 		
 		for (int i = 0; i < arr.length; i++) {
 //			if num is 3 then make sure to increase the count 
-			if(arr[i] == 3) {
+			if(arr[i] == number) {
 				count = 1 + count;
 				
 			}
 		}
 //		make sure to print it outside of the loop 
-		System.out.println(count);
+		System.out.println("the count of " +number + "'s is: " + count);
 	}
 }
