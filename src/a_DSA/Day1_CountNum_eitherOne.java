85fb611e1632d6115c434d42e685f532c9b481e4
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-09 15:38:08 -0400

    Count either if 2 nums are divisible using a or DSA day1

---
+++ src/a_DSA/Day1_CountNum_eitherOne.java
@@ -0,0 +1,17 @@
+package a_DSA;
+
+public class Day1_CountNum_eitherOne {
+	public static void main(String[] args) {
+		
+		int arr[] = {55,34,5,345,53,5};
+		int temp = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if(arr[i] % 5 ==0 || arr[i] % 2 ==0) {
+				temp++;
+			}
+		}
+		System.out.println(temp);
+ 	}
+}
