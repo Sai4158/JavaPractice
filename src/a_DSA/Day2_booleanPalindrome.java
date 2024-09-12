702e2e4c0890077b26d41f387b8641e350d76ed5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-12 11:34:26 -0400

    Added comments

--- src/a_DSA/Day2_booleanPalindrome.java
+++ src/a_DSA/Day2_booleanPalindrome.java
@@ -9,12 +9,15 @@ public class Day2_booleanPalindrome {
 //		get the length
 		int n = s.length();
 	
-		
+		// int i =0 and until length/2
 		for (int i = 0; i <n/2; i++) {
+			//not equal to length - i -1
 			if(s.charAt(i)!=s.charAt(n-i-1)) {
 				panlindrome =  false;
 			}
 		}
+		
+		
 		if(panlindrome) {
 			System.out.println("yes");
 		}else {
