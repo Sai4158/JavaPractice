a6281c43d5112bd8db9d24b5d636a2ee8fdc6e99
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-14 21:50:15 -0400

    Day 3 DSA - printing ASCII / typeCasting

---
+++ src/a_DSA/Day3_AsciiReacp.java
@@ -0,0 +1,21 @@
+package a_DSA;
+
+public class Day3_AsciiReacp {
+	public static void main(String[] args) {
+		
+		int a  = 'b' - 'a';
+		System.out.println(a);
+		
+//		Single quotes
+		System.out.println('c'+'d');
+//		199
+		
+//		single and double quotes 
+		System.out.println('c'+"d");
+//		cd
+		
+//		1
+//		199
+		
+	}
+}
