33e92571059d6bbda06cf27a2440045cdd560d4d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-14 21:24:18 -0400

    Day 3 DSA - ASCII key / TypeCasting

---
+++ src/a_DSA/Day3_ASCII.java
@@ -0,0 +1,14 @@
+package a_DSA;
+
+import aa_prac1.Main;
+
+public class Day3_ASCII {
+	public static void main(String[] args) {
+		
+//		ASCII keys in the java / type casting 
+		
+		int n = 's';
+		
+		System.out.println(n);
+	}
+}
