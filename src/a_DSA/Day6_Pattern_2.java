e02743933d46b19831f41caa8af9395fe8b34448
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-20 18:24:15 -0400

    Updated pattern

--- src/a_DSA/Day6_Pattern_2.java
+++ src/a_DSA/Day6_Pattern_2.java
@@ -2,9 +2,9 @@ package a_DSA;
 
 public class Day6_Pattern_2 {
 	public static void main(String[] args) {
-		int n = 5;
+		int n = 3;
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n ; j++) {
+			for (int j = 0; j < 20 ; j++) {
 				System.out.print("*");
 				
 				if(j!= n-1) {
@@ -19,8 +19,6 @@ public class Day6_Pattern_2 {
 }
 
 
-//*-*-*-*-*
-//*-*-*-*-*
-//*-*-*-*-*
-//*-*-*-*-*
-//*-*-*-*-*
+//*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+//*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+//*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
