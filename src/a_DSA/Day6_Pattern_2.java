d4de76798ab2179d66a1ad9a3f86be7ba3818a5c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-20 18:50:55 -0400

    Added c and r pattern

--- src/a_DSA/Day6_Pattern_2.java
+++ src/a_DSA/Day6_Pattern_2.java
@@ -2,12 +2,18 @@ package a_DSA;
 
 public class Day6_Pattern_2 {
 	public static void main(String[] args) {
-		int n = 3;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < 20 ; j++) {
+		
+//		row - r
+//		coloum - c
+		
+		int r= 20;
+		int c= 3;
+		
+		for (int i = 0; i < c; i++) {
+			for (int j = 0; j < r ; j++) {
 				System.out.print("*");
 				
-				if(j!= n-1) {
+				if(j!= c-1) {
 					System.out.print("-");
 				}				
 				
