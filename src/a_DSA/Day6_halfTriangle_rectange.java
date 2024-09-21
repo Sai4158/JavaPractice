534bacf9845b4c69c67c5c923e73843dc942b3b9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-21 00:44:18 -0400

    Right triangle DSA

---
+++ src/a_DSA/Day6_halfTriangle_rectange.java
@@ -0,0 +1,24 @@
+package a_DSA;
+
+public class Day6_halfTriangle_rectange {
+	public static void main(String[] args) {
+		 int n = 5;
+		 
+		 for (int i = 0; i < n; i++) {
+			
+//			 second loop 
+			 for (int j = 0; j <n-i-1 ; j++) {
+				System.out.print(" ");
+			}
+			 
+			 
+//			 third loop : print
+			 for (int j = 0; j < n-1+i; j++) {
+				System.out.print("*");
+			}
+			 
+			 System.out.println();
+			 
+		}
+	}
+}
