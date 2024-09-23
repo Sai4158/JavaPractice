901be4ba30ee66317c007c9b50d78d8e0c3c8c66
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-23 12:48:08 -0400

    Triangle Pattern

---
+++ src/a_DSA/Day7_Trianlge_Pattern.java
@@ -0,0 +1,25 @@
+package a_DSA;
+
+public class Day7_Trianlge_Pattern {
+	
+	public static void main(String[] args) {
+		
+		int n =5;
+		
+		for (int i = 0; i < n; i++) {
+			
+//			for space 
+			for (int j = 0; j < n-i; j++) {
+				System.out.print(" ");
+			}
+			
+			
+//			For printing stars 
+			for (int j = 0; j < 2*i-1; j++) {
+				System.out.print("^");
+			}
+			
+			System.out.println();
+		}
+	}
+}
