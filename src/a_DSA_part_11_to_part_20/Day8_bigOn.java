6e71e4e86216969c33d33e025b5279daf5ba4e82
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-29 20:54:33 -0400

    Day8 DSA - This is big O of n

---
+++ src/a_DSA_part_11_to_part_20/Day8_bigOn.java
@@ -0,0 +1,15 @@
+package a_DSA_part_11_to_part_20;
+
+public class Day8_bigOn {
+	public static void main(String[] args) {
+		
+//		This is big O of n
+		boolean ab = true;
+		
+		if(ab == true) {
+			System.out.println("HI");
+		}else {
+			System.out.println("Hello");
+		}
+	}
+}
