e18e062f69d64bf98c3576c3ea89572646acfe6d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-29 23:36:49 -0400

    Day8 DSA: This is big of O n

---
+++ src/a_DSA_part_11_to_part_20/Day8_big_o_of_n.java
@@ -0,0 +1,14 @@
+package a_DSA_part_11_to_part_20;
+
+public class Day8_big_o_of_n {
+	public static void main(String[] args) {
+		
+//		This is big of O n - only for loop
+		
+		int n = 5;
+		
+		for (int i = 0; i <= n; i++) {
+			System.out.println(i);
+		}
+	}
+}	
