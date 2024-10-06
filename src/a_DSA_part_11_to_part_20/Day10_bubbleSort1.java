3ff6fc6e223c730a4bac1a1fc13b63152d16dc5e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-06 03:49:39 -0400

    Bubble Sort

---
+++ src/a_DSA_part_11_to_part_20/Day10_bubbleSort1.java
@@ -0,0 +1,25 @@
+package a_DSA_part_11_to_part_20;
+
+public class Day10_bubbleSort1 {
+	public static void main(String[] args) {
+		
+int n[] = {5,4,54,34,24,6345,5};
+		
+		for (int i = 0; i < n.length-1; i++) {
+			
+			for (int j = 0; j < n.length-i-1; j++) {
+				if(n[j]>n[j+1]) {
+					int a  = n[j];
+					n[j] = n[j+1];
+					n[j+1] =  a;
+					
+				}
+			}
+		}
+		
+		for (int i = 0; i < n.length; i++) {
+			System.out.print(n[i] + ",");
+		}
+		System.out.println();
+	}
+}
