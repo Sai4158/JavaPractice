e4710f00d783f2a3e43b3682eab74c11d82aa161
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-06 18:22:24 -0400

    Day10 DSA - Array.Sort

---
+++ src/a_DSA_part_11_to_part_20/Day10_ArryaSort.java
@@ -0,0 +1,30 @@
+package a_DSA_part_11_to_part_20;
+
+import java.util.Arrays;
+
+public class Day10_ArryaSort {
+	public static void main(String[] args) {
+		
+		int arr[] = {4,4,3,4,6,23,34,67,72};
+		
+		Arrays.sort(arr);
+	
+		
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			System.out.println(arr[i]);
+		}
+	}
+}
+
+
+//3
+//4
+//4
+//4
+//6
+//23
+//34
+//67
+//72
