c78faa57fcaabd867ee6d197bcfc5154a0669e65
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-10 01:16:08 -0400

    Day11 DSA- intro to hashmap

---
+++ src/a_DSA_part_11_to_part_20/Day11_HashMap.java
@@ -0,0 +1,18 @@
+package a_DSA_part_11_to_part_20;
+
+import java.util.HashMap;
+
+public class Day11_HashMap {
+	public static void main(String[] args) {
+		
+		HashMap<Integer, Integer> has = new HashMap<>();
+		
+//		Key and value
+		has.put(1, 3);
+		has.put(2, 7);
+		has.put(3, 76);
+		
+//	get key and print  - 3
+	System.out.println(has.get(1));
+	}
+}
