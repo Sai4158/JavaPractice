30d06ba876e3a86ac15daff2daa07a6f53558613
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-10 22:49:19 -0400

    Hashmap recap

---
+++ src/a_DSA_part_11_to_part_20/Day11_HashMap_int_string.java
@@ -0,0 +1,24 @@
+package a_DSA_part_11_to_part_20;
+
+import java.util.HashMap;
+
+public class Day11_HashMap_int_string {
+	public static void main(String[] args) {
+		
+		HashMap<String, Integer> nav = new HashMap< >();
+		
+		nav.put("Chicken", 3);
+		nav.put("celsuis", 2);
+		nav.put("fruit", 0);
+		nav.put("oil", 0);
+		
+
+		System.out.println(nav);
+		
+		if(!nav.containsKey("mayo")) {
+		 nav.put("mayo", 100);
+		}
+		System.out.println(nav);
+
+	}
+}
