b94e49885a3668e14bbc2baf1b8688a2a41e3cc2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-13 21:33:26 -0400

    getOrDefault hashmap

---
+++ src/a_DSA_part_11_to_part_20/Day12_getOrDefault_HashMap.java
@@ -0,0 +1,26 @@
+package a_DSA_part_11_to_part_20;
+
+import java.util.HashMap;
+
+public class Day12_getOrDefault_HashMap {
+	public static void main(String[] args) {
+
+		HashMap<String, Integer> abc =  new HashMap<>();
+
+		String arr[] = {"hy","hu","hi","hi","hu", "hi","hi"};
+
+
+		
+		for (int i = 0; i < arr.length; i++) {
+			String key = arr[i];
+			
+			abc.put(key, abc.getOrDefault(key, 0)+1);
+		}
+		
+		for (String s : abc.keySet()) {
+			System.out.println(s + " " + abc.get(s));
+		}
+		
+		
+	}
+}
