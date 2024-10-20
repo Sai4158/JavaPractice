ee4bf66a83b32e21dad6c32c145ec98596914520
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-19 22:52:41 -0400

    Big o(n)

---
+++ src/a_DSA_part_11_to_part_20/Day8_BIG_O.java
@@ -0,0 +1,20 @@
+package a_DSA_part_11_to_part_20;
+
+public class Day8_BIG_O {
+	
+//	This is o(n)
+	public int sumArray(int[] arr) {
+	    int sum = 0;
+	    for (int i = 0; i < arr.length; i++) {
+	        sum += arr[i];
+	    }
+	    return sum;
+	}
+
+
+//	--------------------------
+	
+
+	
+	
+}
