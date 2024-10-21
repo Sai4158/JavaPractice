c805a759fba83d819c949d1292a7daea98b94e6e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-21 03:25:49 -0400

    Leetcode - 26. Remove Duplicates from Sorted Array

--- src/a_DSA_leetcode/_26_Remove_Duplicates_from_Sorted_Array.java
+++ src/a_DSA_leetcode/_26_Remove_Duplicates_from_Sorted_Array.java
@@ -2,20 +2,23 @@ package a_DSA_leetcode;
 
 public class _26_Remove_Duplicates_from_Sorted_Array {
 	public static int removeDuplicates(int[] nums) {
-        
-			int count = 1;
-			
-		      for (int i = 1; i < nums.length; i++) {
-				if(nums[i] != nums[i-1]) {
-					nums[count] = nums[i];
-					count++;
-				}
-			}
-		return count;	
+
+		 int count = 1;
+	    
+	        for (int i = 1; i < nums.length; i++) {
+	            if (nums[i] != nums[i - 1]) {
+	                 nums[count] = nums[i];
+	                count++;
+	            }
+	        }
+	         return count;
     }
+	
+	
+	
+	
 	public static void main(String[] args) {
-		int arr[] = {43,44,2,1,3,45,62};
-		
+		int arr[] = {1,1,2};
 		System.out.println(removeDuplicates(arr));
 	}
 }
