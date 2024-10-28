9bb65039095327b5f9d5282976dce44ee7e8e1e4
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-27 22:12:03 -0400

    While recap

---
+++ src/a_DSA_part_11_to_part_20/Day14_While.java
@@ -0,0 +1,23 @@
+package a_DSA_part_11_to_part_20;
+
+public class Day14_While {
+    public static void main(String[] args) {
+        
+        int i = 0;
+        
+        while(i < 10) {
+            System.out.println("Hello Sai");
+            
+            // Increment `i` in each iteration
+            if(i == 9) {
+                System.out.println(0);
+            }
+            
+            i += 1;
+        }
+        
+        
+        
+        
+    }
+}
