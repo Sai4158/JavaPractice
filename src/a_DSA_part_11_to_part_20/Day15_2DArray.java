fcc5565e9828e2614182c3d85032df7778726d8f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-29 02:23:05 -0400

    Day15 DSA : 2D array and printing it

---
+++ src/a_DSA_part_11_to_part_20/Day15_2DArray.java
@@ -0,0 +1,23 @@
+package a_DSA_part_11_to_part_20;
+
+import java.util.Arrays;
+
+public class Day15_2DArray {
+	public static void main(String[] args) {
+		
+		int arr[][] = {{1,2,3},{4,5,6}};
+		
+		for (int i = 0; i <= arr.length-1; i++) {
+			for (int j = 0; j <= arr.length; j++) {
+				System.out.println(arr[i][j]);
+			}
+		}
+	}
+}
+
+//1
+//2
+//3
+//4
+//5
+//6
