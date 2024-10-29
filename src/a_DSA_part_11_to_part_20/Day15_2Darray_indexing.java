fe64d30be6074d9e3b2d4b371421b48292651d31
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-29 02:43:33 -0400

    Printing rows and columns for 2D array

---
+++ src/a_DSA_part_11_to_part_20/Day15_2Darray_indexing.java
@@ -0,0 +1,13 @@
+package a_DSA_part_11_to_part_20;
+
+public class Day15_2Darray_indexing {
+	public static void main(String[] args) {
+		
+		int array[][] = {{1,2},{3,5},{7,4}};
+		
+		int r =  array.length;
+		int c = array[0].length;
+		
+		System.out.println(r + " " + c);
+	}
+}
