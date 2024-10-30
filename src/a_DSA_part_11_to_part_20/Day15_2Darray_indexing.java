fb4280500b4f144c97ccedc6448a46b5fcd7ecc2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-30 00:51:20 -0400

    2D Array printing

--- src/a_DSA_part_11_to_part_20/Day15_2Darray_indexing.java
+++ src/a_DSA_part_11_to_part_20/Day15_2Darray_indexing.java
@@ -1,5 +1,7 @@
 package a_DSA_part_11_to_part_20;
 
+import java.util.Iterator;
+
 public class Day15_2Darray_indexing {
 	public static void main(String[] args) {
 		
@@ -9,5 +11,13 @@ public class Day15_2Darray_indexing {
 		int c = array[0].length;
 		
 		System.out.println(r + " " + c);
+		System.out.println("");
+		
+		
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				System.out.println(array[i][j] + ",");
+			}
+		}
 	}
 }
