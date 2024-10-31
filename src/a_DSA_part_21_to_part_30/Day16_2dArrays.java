0a2a3f14092409839b86fa471bbce52cbf6f7ec2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-31 02:39:30 -0400

    2d array

---
+++ src/a_DSA_part_21_to_part_30/Day16_2dArrays.java
@@ -0,0 +1,16 @@
+package a_DSA_part_21_to_part_30;
+
+public class Day16_2dArrays {
+	public static void main(String[] args) {
+		
+		int arr[][] = {{34,43},{43,54},{45,56}};
+		
+		int r = arr.length;
+		int c = arr[0].length;
+		
+		
+		System.out.println(r + " " +c);
+		
+		
+	}
+}
