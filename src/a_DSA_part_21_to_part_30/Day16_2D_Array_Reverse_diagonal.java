18cb3ce64f09b47dbc0462307f53b0a1dbf22667
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-31 20:40:09 -0400

    Day 16 DSA : 2D_Array_Reverse_diagonal

---
+++ src/a_DSA_part_21_to_part_30/Day16_2D_Array_Reverse_diagonal.java
@@ -0,0 +1,21 @@
+package a_DSA_part_21_to_part_30;
+
+import java.util.Iterator;
+
+public class Day16_2D_Array_Reverse_diagonal {
+	public static void main(String[] args) {
+		
+		int arr[][] = {{34,43,45},{43,54,65},{45,56,64}};
+		
+		int n = arr.length-1;
+		
+		for (int i = 0; i <= n; i++) {
+			System.out.println(arr[n-i][n-i]);
+		}
+	}
+}
+
+
+//		64
+//	54
+//34
