3fd446347f01f15501c374e495a3bac2ec99b50b
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-04 02:02:57 -0500

    Setting up Keyboard Row LC

---
+++ src/a_DSA_leetcode/_500_Keyboard_Row.java
@@ -0,0 +1,16 @@
+package a_DSA_leetcode;
+
+public class _500_Keyboard_Row {
+	 public static String[] findWords(String[] words ) {
+		 
+	 }
+	 
+	 
+	 
+	 
+//	 Main  Method
+	 public static void main(String[] args) {
+		String[] words = {"Hello","Alaska","Dad","Peace"};
+		System.out.println(findWords(words));
+	}
+}
