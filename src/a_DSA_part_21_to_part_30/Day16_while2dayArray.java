1d64c90ad0ba94a57c7e8e216259d7d11eb0372e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-03 20:42:17 -0500

    This checks print if diagonal 2d array contains same nums or not

--- src/a_DSA_part_21_to_part_30/Day16_while2dayArray.java
+++ src/a_DSA_part_21_to_part_30/Day16_while2dayArray.java
@@ -2,20 +2,28 @@ package a_DSA_part_21_to_part_30;
 
 public class Day16_while2dayArray {
 	public static void main(String[] args) {
-		int arr[][] = {{12,23,34,56},{45,56,67,78},{78,89,97,89}};
+		int arr[][] = {{11,23,34,56},{45,11,67,78},{43,89,11,89}};
 
 		
 		int r = arr.length;
 		int c = arr[0].length;
 		
 		int i = 0;
-		int j = 2;
+		int j = 0;
+		
+		boolean check = true;
 		
 		while(i<r  && i<c) {
 			System.out.println(arr[i][j]);
 			
+			if(arr[i][i]!=11) {
+				check = false;
+			}
+			 
 			i++;
 			j++;
 		}
+		
+		System.out.println(check);
 	}
 }
