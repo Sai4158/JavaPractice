9a7fb7eb478e6ae7be10cd90a2542d758005f3f5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-03 20:06:21 -0500

    Printing in reverse with while loop

--- src/a_DSA_part_21_to_part_30/Day16_2DArray_whileLoop.java
+++ src/a_DSA_part_21_to_part_30/Day16_2DArray_whileLoop.java
@@ -3,14 +3,14 @@ package a_DSA_part_21_to_part_30;
 public class Day16_2DArray_whileLoop {
 	public static void main(String[] args) {
 		
-		int arr[][] = {{34,43,45},{43,54,65},{45,56,64}};
+		int arr[][] = {{12,23,34},{45,56,67},{78,89,97}};
 
 		
 		int r = arr.length;
 		int c = arr[0].length;
 		
 		int i =0;
-		int j =0;
+		int j =c-1;
 		
 		
 		while (i <r && j<c) {
@@ -18,7 +18,7 @@ public class Day16_2DArray_whileLoop {
 			System.out.println(arr[i][j]);
 			
 			i++;
-			j++;
+			j--;
 		}
 	}
 }
