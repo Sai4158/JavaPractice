31edf980098f61d5233ef8fb450a44415d0778eb
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-09 20:16:47 -0500

    Adding substring to arrayList

--- src/a_DSA_part_21_to_part_30/Day18_SubArray.java
+++ src/a_DSA_part_21_to_part_30/Day18_SubArray.java
@@ -1,18 +1,22 @@
 package a_DSA_part_21_to_part_30;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+
 
 public class Day18_SubArray {
 	public static void main(String[] args) {
 		
 		String s =  "I am sai";
-		
+		ArrayList<String> s1 = new ArrayList<String>();
 		int n = s.length();
 		
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				System.out.println(s.substring(i,j+1));
+			for (int j = i; j < n; j++) {
+				String temp =  s.substring(i,j+1);
+				s1.add(temp);
 			}
 		}
+		
+		System.out.println(s1);
 	}
 }
