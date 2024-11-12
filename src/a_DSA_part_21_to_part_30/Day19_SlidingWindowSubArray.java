398f849e0bf7c24ff7f255a0e8ad318b7e5d31e9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-12 14:03:46 -0500

    Highest sum of subarray of length 3

---
+++ src/a_DSA_part_21_to_part_30/Day19_SlidingWindowSubArray.java
@@ -0,0 +1,26 @@
+package a_DSA_part_21_to_part_30;
+
+public class Day19_SlidingWindowSubArray {
+	public static void main(String[] args) {
+		
+		
+		int arr[] = {5,9,1,8,7};
+		
+		int n =  arr.length;
+		
+		int ans = 0;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				
+				int temp =0;
+				for (int k = i; k <= j; k++) {
+					temp +=  arr[k];
+				}
+			ans =	Math.max(ans, temp);
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
