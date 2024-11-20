795a3a446a4456354bc0f2db7d2ccea3f07d5307
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-19 22:38:42 -0500

    Substrings of Size Three with Distinct Characters

--- src/a_DSA_leetcode/_1876_Substrings_of_Size_Three_with_Distinct_Characters_SubString.java
+++ src/a_DSA_leetcode/_1876_Substrings_of_Size_Three_with_Distinct_Characters_SubString.java
@@ -4,34 +4,38 @@ import java.util.HashMap;
 
 public class _1876_Substrings_of_Size_Three_with_Distinct_Characters_SubString {
     public static void main(String[] args) {
-        String s = "abccbabcba";
-        int n = s.length();
-        int k = 3; // Substring size
-        int l = 0; // Left pointer
-        int ans = 0; // Answer variable
-
-        HashMap<Character, Integer> hm = new HashMap<>();
-
-        for (int r = 0; r < n; r++) {
-            char ch = s.charAt(r); // Current character
-            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
-
-            // Shrink window if size exceeds k
-            if (r - l + 1 > k) {
-                char tch = s.charAt(l);
-                hm.put(tch, hm.get(tch) - 1);
-                if (hm.get(tch) == 0) {
-                    hm.remove(tch);
-                }
-                l++; // Move left pointer
-            }
-
-            // Check if all characters are distinct
-            if (hm.size() == k) {
-                ans++;
-            }
-        }
-
-        System.out.println(ans);
+        
+    	String s = "xyzabc";
+        
+//    	SubString  =  sliding window 
+    	
+    	int n  = s.length();
+    	int ans = 0;
+    	int k =3;
+    	int l = 0;
+    	
+    	HashMap<Character, Integer> h1 = new HashMap<>();
+    	
+    	
+    	for (int r = 0; r < n; r++) {
+			char ch = s.charAt(r);
+			h1.put(ch, h1.getOrDefault(ch, 0)+1);
+			
+			if(r-l ==3) {
+				char tch = s.charAt(l);
+				
+				h1.put(tch, h1.get(tch)-1);
+				
+				if(h1.get(tch)==0) {
+					h1.remove(tch);
+				}
+				l++;
+			}
+			
+			if(h1.size() == 3) {
+				ans++;
+			}
+		}
+    	
     }
 }
