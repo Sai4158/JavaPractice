2e599f8ca10fe856163bd1ebc17e3fd04210d7c8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-19 21:22:31 -0500

    Added comments in code

--- src/a_DSA_part_21_to_part_30/Day22_MaxSubarraySum.java
+++ src/a_DSA_part_21_to_part_30/Day22_MaxSubarraySum.java
@@ -7,6 +7,7 @@ public class Day22_MaxSubarraySum {
 		int arr[] = {4,5,6,3,5};
 		int n  = arr.length;
 		
+		// Left pointer of the window
 		int l=0;
 		int temp=0;
 		int ans=0;
@@ -17,13 +18,14 @@ public class Day22_MaxSubarraySum {
 			
 			temp += arr[r];
 			
-//			1-0
-//			2-0
-//			3-0
-//			4-1 =  3
-//			5-2 =  3
-//			6-3 =  3
+			// r = 0, l = 0 -> temp = 4, ans = 0
+			// r = 1, l = 0 -> temp = 9 (4 + 5), ans = 0
+			// r = 2, l = 0 -> temp = 15 (4 + 5 + 6), ans = 0
+			// r = 3, l = 0 -> temp = 18 (4 + 5 + 6 + 3), ans = 18
+			// r = 4, l = 1 -> temp = 19 (5 + 6 + 3 + 5), ans = 19
 
+			
+            // Check if the window size is 4
 			if(r-l == 3) {
 				temp -= arr[r];
 						
