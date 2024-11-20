3a1f03bd34c4d8aba6c8abb0b0c46be9f5cad27a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-20 00:12:16 -0500

    Printing MAX_VALUE

---
+++ src/a_DSA_part_21_to_part_30/Day23_Max_Value.java
@@ -0,0 +1,14 @@
+package a_DSA_part_21_to_part_30;
+
+public class Day23_Max_Value {
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Float.MAX_VALUE);
+	}
+}
+
+//2147483647
+//9223372036854775807
+//3.4028235E38
