052403e4a808aade0c112c9a7a3b0e650f977829
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-23 18:27:29 -0500

    Leetcode-  1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold

--- src/a_DSA_leetcode/_1343_Number_of_Sub_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.java
+++ src/a_DSA_leetcode/_1343_Number_of_Sub_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.java
@@ -4,7 +4,31 @@ public class _1343_Number_of_Sub_of_Size_K_and_Average_Greater_than_or_Equal_to_
 	
 	 public static int numOfSubarrays(int[] arr, int k, int threshold) {
 	        
-	    }
+		int n = arr.length;
+		int l = 0;
+		int temp = 0;
+		int ans = 0;
+		
+		
+		for (int i = 0; i < n; i++) {
+			temp += arr[i];
+			
+			
+			if(i-l ==k) {
+				temp = temp - arr[l];
+				l++;
+			}
+			
+			if(i-l+1==k) {
+				if(temp/k >= threshold) {
+					ans++;
+				}
+				
+			}
+		}
+		
+		return ans;
+	 }
 	 
 	 
 	 
