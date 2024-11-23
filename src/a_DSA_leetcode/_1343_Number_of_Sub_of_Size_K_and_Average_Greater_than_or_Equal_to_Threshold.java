0ea6baad7eb8f5235439d7b0679a12240d43b07a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-23 18:00:07 -0500

    numOfSubarrays

---
+++ src/a_DSA_leetcode/_1343_Number_of_Sub_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.java
@@ -0,0 +1,21 @@
+package a_DSA_leetcode;
+
+public class _1343_Number_of_Sub_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold {
+	
+	 public static int numOfSubarrays(int[] arr, int k, int threshold) {
+	        
+	    }
+	 
+	 
+	 
+	 
+//	 Main method 
+	 
+	 public static void main(String[] args) {
+		int arr[] = {2,2,2,2,5,5,5,8};
+		int k = 3;
+		int threshold = 4;
+		
+		System.out.println(numOfSubarrays(arr, k, threshold));
+	}
+}
