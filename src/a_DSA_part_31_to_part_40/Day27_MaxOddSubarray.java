40de106b852e6d3c9723e6f44ccd44e66e02b872
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-29 13:26:33 -0500

    Max Odd Subarray

---
+++ src/a_DSA_part_31_to_part_40/Day27_MaxOddSubarray.java
@@ -0,0 +1,34 @@
+package a_DSA_part_31_to_part_40;
+
+public class Day27_MaxOddSubarray {
+	public static void main(String[] args) {
+		
+		
+		int arr[] = {1,3,7,8,5,3,7,3,1};
+
+		int l = 0;
+		int k = 3;
+		int temp = 0;
+		int ans = 0;
+		int n = arr.length;
+		
+		
+		for (int r = 0; r < n; r++) {
+			
+			
+			if(arr[r] % 2 != 0) {
+				temp++;
+			}
+			
+			while(arr[l] > k) {
+				temp -= arr[l];
+				
+				l++;
+			}
+			
+			ans = Math.max(ans, r-l+1);
+		}
+		
+		System.out.println(ans);
+	}
+}
