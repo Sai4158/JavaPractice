6900baf550830df3f209c03a7dc8a2e38e36602c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-29 22:19:24 -0500

    Day 27 - Max Even Subarray

---
+++ src/a_DSA_part_31_to_part_40/Day27_MaxEvenSubarray.java
@@ -0,0 +1,36 @@
+package a_DSA_part_31_to_part_40;
+
+public class Day27_MaxEvenSubarray {
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,2,2,1,1,1,1};
+
+		int l = 0;
+		int k = 1;
+		int temp = 0;
+		int ans = 0;
+		int n = arr.length;
+		
+		
+		for (int r = 0; r < n; r++) {
+			if(arr[r] % 2 != 0) {
+				temp++;
+			}
+			
+			while(temp > k) {
+				if(arr[l] % 2 != 0) {
+					temp--;
+				}
+				l++;
+			}
+			
+			ans = Math.max(ans, r-l+1);
+			
+		}
+		
+		System.out.println(ans);
+		
+		
+		
+	}
+}
