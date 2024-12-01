0d2a541582a8f6c770390b64a608e4b887b89e5e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-30 20:23:43 -0500

    Binaray array max k nums

--- src/a_DSA_part_31_to_part_40/Day28_BinaryArrayMaxKSubArray.java
+++ src/a_DSA_part_31_to_part_40/Day28_BinaryArrayMaxKSubArray.java
@@ -1,29 +1,33 @@
 package a_DSA_part_31_to_part_40;
 
-public class Day27_MaxEvenSubarray {
+public class Day28_BinaryArrayMaxKSubArray {
 	public static void main(String[] args) {
 		
-		int arr[] = {1,2,2,2,1,1,1,1};
+		
+		int arr[] = {1,1,0,1,0,0,0};
 
 		int l = 0;
-		int k = 1; // Maximum allowed even numbers
-		int temp = 0; // Count of even numbers in the current window
-		int ans = 0; // Maximum length of the subarray
+		int k = 2; 
+		int temp = 0;
+		int ans = 0; 
 		int n = arr.length;
 		
 		
 		for (int r = 0; r < n; r++) {
-			if(arr[r] % 2 == 0) {
-				temp++;
+			
+//			Add if
+			
+			if(arr[r] == 0 ) {
+				temp ++;
 			}
 			
+			
 			while(temp > k) {
-				if(arr[l] % 2 == 0) {
+				if(arr[l]==0) {
 					temp--;
 				}
 				l++;
 			}
-			
 			ans = Math.max(ans, r-l+1);
 			
 		}
