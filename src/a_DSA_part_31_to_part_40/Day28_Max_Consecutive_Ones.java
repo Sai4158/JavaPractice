132642da6d29d8cfbe7e4ecbb79e89b98ca7512f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-30 20:50:31 -0500

    Max Consecutive Ones

---
+++ src/a_DSA_part_31_to_part_40/Day28_Max_Consecutive_Ones.java
@@ -0,0 +1,41 @@
+package a_DSA_part_31_to_part_40;
+
+public class Day28_Max_Consecutive_Ones {
+ public static int longestOnes(int[] nums, int k) {
+		 
+		 int temp = 0;
+		 int l = 0;
+		 int ans = 0;
+		 int n = nums.length;
+		 
+		 
+		 for (int r = 0; r < n; r++) {
+			
+			 if(nums[r] == 0) {
+				 temp++;
+			 }
+			 
+			 
+			 while(temp > k) {
+				 if(nums[l] == 0) {
+					 temp--;
+				 }
+				 l++;
+			 }
+			 ans = Math.max(ans, r-l+1);
+		 }
+		 return ans;
+	   }
+	
+	
+	
+	 
+	 
+//	 Main method
+	public static void main(String[] args) {
+		
+		int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+		int k  = 2;
+		System.out.println(longestOnes(nums, k));
+	}
+}
