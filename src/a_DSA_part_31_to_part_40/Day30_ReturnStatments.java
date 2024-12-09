121fad5439bf291a16e6d6524d845228115f5fcb
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-09 10:59:35 -0500

    Testing return statments

---
+++ src/a_DSA_part_31_to_part_40/Day30_ReturnStatments.java
@@ -0,0 +1,22 @@
+package a_DSA_part_31_to_part_40;
+
+public class Day30_ReturnStatments {
+	
+	public int test() {
+		
+		System.out.println("Testing method");
+		
+		int a = 10;
+		
+		return a;
+	}
+	
+//	Main method
+	
+	public static void main(String[] args) {
+		 
+		Day30_ReturnStatments ab = new Day30_ReturnStatments();
+		
+		System.out.println(ab.test());
+	}
+}
