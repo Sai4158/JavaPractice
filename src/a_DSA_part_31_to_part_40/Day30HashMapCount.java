1779a1b04fb72b575716b09881a0db59ff160917
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-12 23:55:14 -0500

    Counting the number of times a string was in the hashmap

---
+++ src/a_DSA_part_31_to_part_40/Day30HashMapCount.java
@@ -0,0 +1,25 @@
+package a_DSA_part_31_to_part_40;
+
+import java.util.HashMap;
+
+public class Day30HashMapCount {	
+	public static void main(String[] args) {
+		
+		String a = "heloffdegjkljkrngjnerjn";
+		
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		
+		for (int i = 0; i < a.length(); i++) {
+			
+			char ch = a.charAt(i);
+			
+			hm.put(ch, hm.getOrDefault(ch, 0)+1);
+		}
+		
+		
+		for(Character keys : hm.keySet()) {
+			System.out.println(hm.values() + " " + hm.get(keys));
+		}
+	
+	}
+}
