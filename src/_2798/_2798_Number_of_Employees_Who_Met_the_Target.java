361ea953ce3b8970920b902ae4ff1882e56349d7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-14 02:07:28 -0500

    Leetcode -  2798. Number of Employees Who Met the Target

---
+++ src/_2798/_2798_Number_of_Employees_Who_Met_the_Target.java
@@ -0,0 +1,31 @@
+package _2798;
+
+public class _2798_Number_of_Employees_Who_Met_the_Target {
+
+	public static int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+
+
+		int n = hours.length;
+
+		int count = 0;
+
+		for (int i = 0; i < n; i++) {
+			if(hours[i] >= target) {
+				count++;
+			}
+		}
+
+		return count;
+
+	}
+
+
+	public static void main(String[] args) {
+
+		int hours[] = {0,1,2,3,4};
+
+		int target = 2;
+
+		System.out.println(numberOfEmployeesWhoMetTarget(hours, target));
+	}
+}
