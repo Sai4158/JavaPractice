5052ce0fe308234c763708fed5413c7e44c96ff8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-21 01:50:13 -0500

    Fixed issues

--- src/a_DSA_part_31_to_part_40/Day31_MaxOdd_KsubArrays.java
+++ src/a_DSA_part_31_to_part_40/Day31_MaxOdd_KsubArrays.java
@@ -1,31 +1,30 @@
 package a_DSA_part_31_to_part_40;
 
 public class Day31_MaxOdd_KsubArrays {
-	public static void main(String[] args) {
-		
-//		find the max k odd Sub arrays in the array
-		
-		int arr[] = {1,2,3,4,5,6};
-		int l = 0 ;
-		int temp = 0 ;
-		int ans = 0 ;
-		int k = 0 ;
+    public static void main(String[] args) {
+        
+        // Find the max k odd subarrays in the array
+        int arr[] = {1,4,6,3,9,9,9,9,9};
+        int l = 0;
+        int temp = 0; 
+        int ans = 0; 
+        int k = 1;
 
-	
-		for (int r = 0; r < arr.length; r++) {
-			if(arr[r] %2 ==1) {
-				temp++;
-			}
-			
-			
-			while(temp>k) {
-				if(arr[r]%2==1) {
-					temp--;
-				}
-				l++;
-			}
-			System.out.println("");
-		}
-	
-	}
+        for (int r = 0; r < arr.length; r++) {
+            if (arr[r] % 2 == 1) {
+                temp++;
+            }
+            
+            while (temp > k) {
+                if (arr[l] % 2 == 1) {
+                    temp--;
+                }
+                l++;
+            }
+            
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        System.out.println(ans);
+    }
 }
