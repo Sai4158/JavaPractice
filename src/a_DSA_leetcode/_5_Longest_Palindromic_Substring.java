3d6f39408d9d1c790c9f8cc9dd4b7f761afa5ee5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-04 16:40:41 -0500

    leetcode - 35. Search Insert Position

---
+++ src/a_DSA_leetcode/_5_Longest_Palindromic_Substring.java
@@ -0,0 +1,26 @@
+package a_DSA_leetcode;
+
+public class _5_Longest_Palindromic_Substring {
+	
+    public static String longestPalindrome(String s) {
+        
+    	for (int i = 0; i < s.length()-1; i++) {
+				
+				char a =  s.charAt(i);
+				char b =  s.charAt(i+1);
+				
+				
+				
+			
+		}
+    	
+    }
+	
+	
+	public static void main(String[] args) {
+		
+		String s  = "babad";
+		System.out.println(longestPalindrome(s));
+		
+	}
+}	
---
+++ src/a_DSA_part_41_to_part_50/Day35_CountNumberOfNiceSubArrays.java
@@ -0,0 +1,11 @@
+package a_DSA_part_41_to_part_50;
+
+public class Day35_CountNumberOfNiceSubArrays {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+}
