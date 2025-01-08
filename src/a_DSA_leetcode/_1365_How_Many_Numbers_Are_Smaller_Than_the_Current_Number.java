1c84a33a57185cb03272402ddb7f2fb18053ea29
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-08 18:46:12 -0500

    Leetcode - 1365. How Many Numbers Are Smaller Than the Current Number

---
+++ src/a_DSA_leetcode/_1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,40 @@
+package a_DSA_leetcode;
+
+import java.util.ArrayList;
+
+public class _1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number {
+	
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        
+    	int n = nums.length;
+    	ArrayList<Integer> list  =  new ArrayList<Integer>();
+    	
+    	
+    	for (int i = 0; i < n; i++) {
+			int count = 0;
+			
+    		for (int j = 0; j < nums.length; j++) {
+    			if (nums[j] < nums[i]) {
+                    count++;
+                }
+				
+			}
+			list.add(count);
+
+    	}
+
+    	
+    		int[] result = new int[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = list.get(i);
+            }
+
+            return result;
+    	
+    }
+    
+    public static void main(String[] args) {
+    	int nums[] = {8,1,2,2,3};
+    	System.out.println(smallerNumbersThanCurrent(nums));
+	}
+}
