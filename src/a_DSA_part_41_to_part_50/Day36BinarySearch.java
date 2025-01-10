768e8d19bb7f6b3cdb05cd2cfe80505531545322
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-10 18:30:41 -0500

    binary search

---
+++ src/a_DSA_part_41_to_part_50/Day36BinarySearch.java
@@ -0,0 +1,33 @@
+package a_DSA_part_41_to_part_50;
+
+public class Day36BinarySearch {
+	
+	public static int bs(int arr[], int target) {
+		
+		
+		int l = 0;
+		int r = arr.length;
+		
+		
+		while(l<=r) {
+			int mid =  (l+r)/2;
+			
+			if(arr[mid] == target) {
+				return arr[mid];
+			}else if(arr[mid] > target) {
+				r = mid -1;
+			}else {
+				r = mid +1;
+			}
+		}
+		return -1;
+		
+	}
+	
+	public static void main(String[] args) {
+		int arr[] = {4,5,7,3,2,5,2};
+		int target = 3;
+		
+		System.out.println(bs(arr, target));
+	}
+}
