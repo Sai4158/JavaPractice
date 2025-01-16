cbdb0673a742ea20a0bb8d37bfb57cf325bc7e51
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-15 19:36:05 -0500

    Find the index of left most binary search

---
+++ src/a_DSA_part_41_to_part_50/Day38_Find_the_index_of_left_most.java
@@ -0,0 +1,36 @@
+package a_DSA_part_41_to_part_50;
+
+public class Day38_Find_the_index_of_left_most {
+	
+public static int bspromb(int arr[], int target) {
+		
+		int l = 0;
+		int r =  arr.length - 1;
+		int result = -1;
+		
+		while(l <= r) {
+			int mid =  l + (r-l)/2;
+			
+			if(arr[mid] == target) {
+					
+			result = mid;
+				
+			 r =  mid - 1;
+			}else if(arr[mid]>target) {
+				 r = mid-1;
+			}else {
+				 l = mid +1;
+			}
+		}
+		return result;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int arr[] = {3,3,6,3,3,3,3,6,7};
+		int target  =  3;
+		
+		System.out.println(bspromb(arr, target));
+	}
+}
