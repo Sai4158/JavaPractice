f6fa52944cc065fd0df03ca1d4683809b907720b
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-15 19:34:38 -0500

    Find the index of right most binary search

--- src/a_DSA_part_41_to_part_50/Day38_Find_the_index_of_right_most.java
+++ src/a_DSA_part_41_to_part_50/Day38_Find_the_index_of_right_most.java
@@ -6,19 +6,23 @@ public class Day38_Find_the_index_of_right_most {
 		
 		int l = 0;
 		int r =  arr.length - 1;
+		int result = -1;
 		
 		while(l <= r) {
 			int mid =  l + (r-l)/2;
 			
 			if(arr[mid] == target) {
-				  return mid;
+					
+			result = mid;
+				
+			 l =  mid + 1;
 			}else if(arr[mid]>target) {
 				 r = mid-1;
 			}else {
 				 l = mid +1;
 			}
 		}
-		return -1;
+		return result;
 		
 	}
 	
