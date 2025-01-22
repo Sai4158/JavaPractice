34b6c985b59ba4a1170f2b2bb11ac6d5abd62ff0
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-21 19:51:08 -0500

    Right most binary search recap

---
+++ src/a_DSA_part_41_to_part_50/Day39RightMostSubArray.java
@@ -0,0 +1,36 @@
+package a_DSA_part_41_to_part_50;
+
+public class Day39RightMostSubArray {
+	
+	
+	public static int bspromb(int arr[], int target) {
+		
+		int n = arr.length;
+		int l = 0;
+		int r =  n-1;
+		int result = 0;
+		
+		while(l<=r) {
+			int mid  =  l+(r-l)/2;
+			
+			if(arr[mid] == target) {
+				result = mid;
+				l = mid +1;
+			}else if(arr[mid] > target){
+				r =  mid -1;
+			}else {
+				l =  mid +1;
+			}
+		}
+		
+		return result;
+	}
+	
+		
+	public static void main(String[] args) {
+		int arr[] = {3,3,6,3,3,3,3,6,7};
+		int target  =  6;
+		
+		System.out.println(bspromb(arr, target));
+	}
+}
