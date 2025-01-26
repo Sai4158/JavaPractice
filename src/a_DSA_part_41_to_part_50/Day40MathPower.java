75f228b8781e98fdd4ea99bfb8193a777555a955
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-25 22:47:12 -0500

    Math power

---
+++ src/a_DSA_part_41_to_part_50/Day40MathPower.java
@@ -0,0 +1,19 @@
+package a_DSA_part_41_to_part_50;
+
+public class Day40MathPower {
+	
+	public static void main(String[] args) {
+		
+		int a  = 5;
+		int b = 2;
+		
+		
+//		Should be 125
+		int c = (int)Math.pow(a, b);
+//		or use double like this 
+		double d = Math.pow(a, b);
+
+		
+		System.out.println(c);
+	}
+}
