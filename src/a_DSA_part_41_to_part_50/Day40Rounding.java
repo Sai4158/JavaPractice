67f4bda9dd1081f20f6e295f1e47b3f83108ad07
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-25 20:09:12 -0500

    Rounding without using Math.

---
+++ src/a_DSA_part_41_to_part_50/Day40Rounding.java
@@ -0,0 +1,24 @@
+package a_DSA_part_41_to_part_50;
+
+public class Day40Rounding {
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 2;
+		
+		int c = a/b;
+		
+		System.out.println(c);
+//		2 => 3
+		
+		
+		if(c != 0) {
+			c++;
+		}
+		
+		System.out.println(c);
+	}
+	
+//	2
+//	3
+}
