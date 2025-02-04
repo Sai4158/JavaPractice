3c9b14d67aba05df2fbdf5867cecb50bf7a28914
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-04 02:39:09 -0500

    Extract last Digit

---
+++ src/a_DSA_part_41_to_part_50/Day42ExtractDigits.java
@@ -0,0 +1,24 @@
+package a_DSA_part_41_to_part_50;
+
+public class Day42ExtractDigits {
+	public static void main(String[] args) {
+		
+		int a = 25;
+		
+		int b =  a % 10;
+		
+		System.out.println(b); //5
+		
+		
+		
+		int a1 = 235;
+		
+		int b1 =  a1 % 10;
+		
+		System.out.println(b1); //5
+		
+		
+		
+		
+	}
+}
