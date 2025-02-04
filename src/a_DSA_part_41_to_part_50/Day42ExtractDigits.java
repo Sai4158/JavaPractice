a785f7b3976d1ada8abfcf67efe55b3fabb12106
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-04 02:40:40 -0500

    Division to remove last digit

--- src/a_DSA_part_41_to_part_50/Day42ExtractDigits.java
+++ src/a_DSA_part_41_to_part_50/Day42ExtractDigits.java
@@ -2,23 +2,27 @@ package a_DSA_part_41_to_part_50;
 
 public class Day42ExtractDigits {
 	public static void main(String[] args) {
-		
+
 		int a = 25;
-		
+
 		int b =  a % 10;
-		
+
 		System.out.println(b); //5
-		
-		
-		
+
+
+
 		int a1 = 235;
-		
+
 		int b1 =  a1 % 10;
-		
+
 		System.out.println(b1); //5
-		
-		
-		
-		
+
+
+
+		int a2 = 235;
+
+		int b2 =  a2 / 10;
+
+		System.out.println(b2); //23
 	}
 }
