5598deca9af71d338b98476e76e80e1f3a441ae8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-04 03:02:29 -0500

    415. Add Strings

--- src/a_DSA_leetcode/_415_Add_Strings.java
+++ src/a_DSA_leetcode/_415_Add_Strings.java
@@ -2,22 +2,29 @@ package a_DSA_leetcode;
 
 public class _415_Add_Strings {
 
-	public static String addStrings(String num1, String num2) {
+    public static String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
 
-		int a = Integer.parseInt(num1);
-		int b = Integer.parseInt(num2);
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
 
-		
-		int c =  a + b;
-		
-		String d =  String.valueOf(c);
-		
-		return d;
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+            result.append(sum % 10);
 
-	}
+            i--;
+            j--;
+        }
+
+        return result.reverse().toString();
+    }
 
 	public static void main(String[] args) {
-		String num1 = "11", num2 = "123";
+		String num1 = "11", num2 = "1234554565454654645";
 
 		System.out.println(addStrings(num1, num2));
 	}
