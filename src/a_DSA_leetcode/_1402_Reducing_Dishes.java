f51fac3b8dfdc82d72c25c9127656f6fdf7bdbf2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-07 02:21:59 -0500

    Leetcode

---
+++ src/a_DSA_leetcode/_1402_Reducing_Dishes.java
@@ -0,0 +1,19 @@
+package a_DSA_leetcode;
+
+public class _1402_Reducing_Dishes {
+	
+    public static int maxSatisfaction(int[] satisfaction) {
+        
+    	int n = satisfaction.length;
+    	
+    	for (int i = 0; i < n; i++) {
+			
+		}
+    	
+    }
+	
+	public static void main(String[] args) {
+		int satisfaction[] = {-1,-8,0,5,-9};
+		System.out.println(maxSatisfaction(satisfaction));
+	}
+}
