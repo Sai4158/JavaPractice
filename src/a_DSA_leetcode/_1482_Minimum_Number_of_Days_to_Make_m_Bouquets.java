46e611c7b576c7f0d6b047fbb7331fdd7745f095
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-10 22:03:29 -0500

    Leetcode - 1482. Minimum Number of Days to Make m Bouquets

--- src/a_DSA_leetcode/_1482_Minimum_Number_of_Days_to_Make_m_Bouquets.java
+++ src/a_DSA_leetcode/_1482_Minimum_Number_of_Days_to_Make_m_Bouquets.java
@@ -2,54 +2,50 @@ package a_DSA_leetcode;
 
 public class _1482_Minimum_Number_of_Days_to_Make_m_Bouquets {
 
-    public static int minDays(int[] bloomDay, int m, int k) {
-        
-        int n = bloomDay.length;
-        if ((long) m * k > n) return -1; // Impossible case
+	public static int minDays(int[] bloomDay, int m, int k) {
+        int l = 1;
+        int r = (int) Math.pow(10, 9);
 
-        int l = Integer.MAX_VALUE;
-        int r = Integer.MIN_VALUE;
-
-        // Find min and max bloom days
-        for (int i = 0; i < n; i++) {
-            l = Math.min(l, bloomDay[i]);
-            r = Math.max(r, bloomDay[i]);
+        if (bloomDay.length < m * k) {
+            return -1;
         }
-        
-        while (l < r) {
+
+        while (l <= r) {
             int mid = l + (r - l) / 2;
-            
-            if (IsPossible(bloomDay, m, k, mid)) {
-                r = mid; 
+
+            if (isPossible(bloomDay, m, k, mid)) {
+                r = mid - 1;
             } else {
                 l = mid + 1;
             }
         }
-        
+
+        int exed = (int) Math.pow(10, 9);
+        if (l == exed + 1) {
+            return -1;
+        }
+
         return l;
     }
-    
-    
-    public static boolean IsPossible(int[] bloomDay, int m, int k, int mid) {
-        
+
+    public static boolean isPossible(int[] bloomDay, int m, int k, int mid) {
         int took = 0;
-        
+
         for (int i = 0; i < bloomDay.length; i++) {
-                    
             if (bloomDay[i] <= mid) {
                 took++;
             } else {
                 took = 0;
             }
-            
+
             if (took == k) {
                 m -= 1;
                 took = 0;
             }
-            
+
             if (m == 0) return true;
         }
-        
+
         return false;
     }
 
