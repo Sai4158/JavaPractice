01d2d5dbece8e52299727eb95a74f00c65e00a37
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-12 11:13:13 -0500

    Leetcode - 374. Guess Number Higher or Lower

---
+++ src/a_DSA_leetcode/_374_Guess_Number_Higher_or_Lower.java
@@ -0,0 +1,31 @@
+package a_DSA_leetcode;
+
+public class _374_Guess_Number_Higher_or_Lower {
+	
+    public static int guessNumber(int n) {
+         
+    	int l = 1;
+    	int r = n;
+    	
+    	while(l<=r) {
+    		int mid = l+(r-l)/2;
+    		
+    		int check = guess(mid);
+    		
+    		if(check == 0) {
+    			return mid;
+    		}else if(check == -1){
+    			r = mid -1;
+    		}else {
+    			l =  mid+1;
+    		}
+    	}
+    	return -1;
+    }
+	
+	public static void main(String[] args) {
+		int n = 5;
+		
+		System.out.println(guessNumber(n));
+	}
+}
