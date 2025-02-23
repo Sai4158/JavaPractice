11c5ccccd0911ca4601152cf39665fef67bf421e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-22 20:52:12 -0500

    Leetcode - 198. House Robber

--- src/a_DSA_leetcode/_198_House_Robber.java
+++ src/a_DSA_leetcode/_198_House_Robber.java
@@ -4,19 +4,21 @@ public class _198_House_Robber {
 	
 	 public static int rob(int[] nums) {
 	        
-	    	int n = nums.length;
-	    	int count1 = 0;
-	    	int count2 = 0;
-	    	
-	    	for (int i = 0; i < n; i += 2) { 
-				count1 += nums[i]; 
-	    	}
-	    	
-	    	for (int i = 1; i < n; i += 2) { 
-				count2 += nums[i]; 
-			}
-	    	
-	    	return Math.max(count1, count2); 
+	        int n = nums.length;
+
+	        if (n == 0) return 0;
+	        if (n == 1) return nums[0];
+
+	        int prev2 = 0; 
+	        int prev1 = nums[0]; 
+
+	        for (int i = 1; i < n; i++) {
+	            int newSum = Math.max(prev1, prev2 + nums[i]); 
+	            prev2 = prev1; 
+	            prev1 = newSum;
+	        }
+
+	        return prev1;
 	    }
 		
 		public static void main(String[] args) {
