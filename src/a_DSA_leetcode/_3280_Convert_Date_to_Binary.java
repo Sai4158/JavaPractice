43dde5f2d165f796038ae7c35f8572b1eb21e93f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-24 03:33:57 -0500

    Leetcode - 3280. Convert Date to Binary using String builder

--- src/a_DSA_leetcode/_3280_Convert_Date_to_Binary.java
+++ src/a_DSA_leetcode/_3280_Convert_Date_to_Binary.java
@@ -4,21 +4,33 @@ public class _3280_Convert_Date_to_Binary {
 	
     public static String convertDateToBinary(String date) {
         
-    	String ans = "";
-    	String temp = "";
+//    	String ans = "";
+//    	String temp = "";
+//    	
+//    	for (int i = 0; i < date.length(); i++) {
+//			
+//    		if(date.charAt(i) != '-') {
+//    			temp += date.charAt(i);
+//    		}else {
+//    			ans += Integer.toBinaryString(Integer.parseInt(temp)) + '-';
+//    			temp = "";
+//    		}
+//		}
+//    	ans += Integer.toBinaryString(Integer.parseInt(temp));
+//
+//    	return ans;
     	
-    	for (int i = 0; i < date.length(); i++) {
-			
-    		if(date.charAt(i) != '-') {
-    			temp += date.charAt(i);
-    		}else {
-    			ans += Integer.toBinaryString(Integer.parseInt(temp)) + '-';
-    			temp = "";
-    		}
-		}
-    	ans += Integer.toBinaryString(Integer.parseInt(temp));
-
-    	return ans;
+    	
+    	StringBuilder ans = new StringBuilder();
+    	
+    	for (String part : date.split("-")) {
+    		ans.append(Integer.toBinaryString(Integer.parseInt(part))).append("-");
+    	}
+    	
+//    	Remove the last extra - 
+    	ans.setLength(ans.length() - 1);
+    	
+    	return ans.toString();
     }
 	
 	public static void main(String[] args) {
