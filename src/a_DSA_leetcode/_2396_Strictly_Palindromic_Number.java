509467e6e407e7b74bcb56f45a5d6794121f5e52
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-02 02:51:44 -0500

    2396. Strictly Palindromic Number

---
+++ src/a_DSA_leetcode/_2396_Strictly_Palindromic_Number.java
@@ -0,0 +1,14 @@
+package a_DSA_leetcode;
+
+public class _2396_Strictly_Palindromic_Number {
+    
+	public static boolean isStrictlyPalindromic(int n) {
+        
+        return false;
+    }
+	
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println(isStrictlyPalindromic(n));
+	}
+}
