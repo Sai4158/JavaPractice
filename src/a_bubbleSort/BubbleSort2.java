e05dbcd4fb2cb8159f68435d64eba6bc882ce109
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-01 22:40:08 -0500

    Bubble sort

---
+++ src/a_bubbleSort/BubbleSort2.java
@@ -0,0 +1,27 @@
+package a_bubbleSort;
+
+public class BubbleSort2 {
+
+	public static int[] bubble(int nums[]) {
+	
+		int n =  nums.length;
+		
+		for (int i = 0; i < n-1; i++) {
+			for (int j = 0; j < n-1; j++) {
+				if(nums[i] > nums[j+1]) {
+					int temp = nums[j];
+					nums[j] =  nums[j+1];
+					nums[j+1] =  temp;
+
+				}
+			}
+		}
+		
+		return nums;
+	}
+
+	public static void main(String[] args) {
+        int arr[] = {7, 8, 3, 1, 7,6,2};
+        System.out.println(bubble(arr));
+	}
+}
