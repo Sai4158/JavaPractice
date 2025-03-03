9d3211d73877c19e2f6a42b8ad4addb4f134f5ef
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-03 02:55:15 -0500

    Leetcode - 1816. Truncate Sentence

---
+++ src/a_DSA_leetcode/_1816_Truncate_Sentence.java
@@ -0,0 +1,24 @@
+package a_DSA_leetcode;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class _1816_Truncate_Sentence {
+
+	public static String truncateSentence(String s, int k) {
+		
+		String word[] = s.split(" ");
+		
+		ArrayList<String> al = new ArrayList<String>();
+		
+		for (int i = 0; i < k; i++) {
+			al.add(word[i]);
+		}
+		return al.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(truncateSentence("Hello how are you Contestant", 4)); 
+	}
+
+}
