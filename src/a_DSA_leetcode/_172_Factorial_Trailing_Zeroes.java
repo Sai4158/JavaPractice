8c726753c648bda6391ca64fc9b94b3bff19c1f3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-04 02:54:23 -0500

    Leetcode - _172_Factorial_Trailing_Zeroes

---
+++ src/a_DSA_leetcode/_172_Factorial_Trailing_Zeroes.java
@@ -0,0 +1,20 @@
+package a_DSA_leetcode;
+
+public class _172_Factorial_Trailing_Zeroes {
+
+    public static int trailingZeroes(int n) {
+        
+    	int count = 0;
+
+    	for (int i = 5; i <=n; i*=5) {
+
+    		
+    		count += n/i;	
+		}
+    	return count;
+    }
+	
+	public static void main(String[] args) {
+		 System.out.println(trailingZeroes(5)); 
+	}
+}
