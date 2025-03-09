b793c04b4a75d327a7669ab76f0dc9c2c4d23b20
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-09 04:07:25 -0400

    Leetcode - 190. Reverse Bits

---
+++ src/a_DSA_leetcode/_190_Reverse_Bits.java
@@ -0,0 +1,14 @@
+package a_DSA_leetcode;
+
+public class _190_Reverse_Bits {
+
+    public static int reverseBits(int n) {
+        
+    	return Integer.reverse(n);
+    }
+	
+	public static void main(String[] args) {
+		 int n1 = 43261596;
+		 System.out.println(reverseBits(n1));
+	}
+}
