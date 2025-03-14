f0795bcea29bbce88fa7b73b2cfd838c8a52ca8c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-14 00:47:38 -0400

    Leetcode - 2206. Divide Array Into Equal Pairs

--- src/a_DSA_leetcode/_2206_Divide_Array_Into_Equal_Pairs.java
+++ src/a_DSA_leetcode/_2206_Divide_Array_Into_Equal_Pairs.java
@@ -5,27 +5,28 @@ import java.util.HashSet;
 
 public class _2206_Divide_Array_Into_Equal_Pairs {
 
-    public static boolean divideArray(int[] nums) {
-        
-    	int n = nums.length;
-        HashMap<Integer, Integer> hs = new HashMap<>();
-    	
-    	int half =  n/2;
-    	
-    	for (int i = 0; i < nums.length; i++) {
+	public static boolean divideArray(int[] nums) {
+
+		int n = nums.length;
+		HashMap<Integer, Integer> hs = new HashMap<>();
+
+		int half =  n/2;
+
+		for (int i = 0; i < nums.length; i++) {
 			hs.put(nums[i], hs.getOrDefault(nums[i], 0)+1);
 		}
-    	
-    	
-    	for(int count: hs.values()) {
-    		if( count % 2 == 0) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
-    
-    public static void main(String[] args) {
+
+
+		for (int count : hs.values()) {
+			if (count % 2 != 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
 		int nums[] = {3,2,3,2,2,2};
 		System.out.println(divideArray(nums));
 	}
