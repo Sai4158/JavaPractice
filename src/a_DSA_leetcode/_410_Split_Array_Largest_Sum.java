e6ad4cde7408f9064226f233ce65acb1f64f9018
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-16 22:04:34 -0400

    Leetcode - 410. Split Array Largest Sum

---
+++ src/a_DSA_leetcode/_410_Split_Array_Largest_Sum.java
@@ -0,0 +1,56 @@
+package a_DSA_leetcode;
+
+public class _410_Split_Array_Largest_Sum {
+
+    public static int splitArray(int[] nums, int k) {
+      int l = 0;
+      int n = nums.length;
+      int r = (int) Math.pow(10, 6);
+      
+      
+      while(l<=r) {
+    	  int mid = l + (r-l)/2;
+    	  
+    	  if(ispossible(nums, k, mid)) {
+    		  r = mid -1;
+    	  }else {
+    		  l =  mid + 1;
+    	  }
+      }
+      return l;
+    }
+    
+    public static boolean ispossible(int[] nums, int k, int mid) {
+    
+    	int temp = 0;
+    	k -=1;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+		
+    		
+    		int val =  nums[i];
+    		
+    		if(val>mid) {
+    			return false;
+    		}
+    		
+    		if(val + temp > mid) {
+    			k-=1;
+    			temp = 0;
+    		}
+    		temp += val;
+    		
+    		if(k<0) {
+    			return false;
+    		}
+		}
+    	
+    	return true;
+    }
+
+	
+	public static void main(String[] args) {
+		int nums[] = {1,2,3,4,5}, k = 2;
+		System.out.println(splitArray(nums, k));
+	}
+}
