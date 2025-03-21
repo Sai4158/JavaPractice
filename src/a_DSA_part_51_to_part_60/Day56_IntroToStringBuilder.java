2972b2c7411322f84af711dadc719b1f7714684d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-21 01:19:05 -0400

    Better time complexity

--- src/a_DSA_part_51_to_part_60/Day56_IntroToStringBuilder.java
+++ src/a_DSA_part_51_to_part_60/Day56_IntroToStringBuilder.java
@@ -4,11 +4,15 @@ public class Day56_IntroToStringBuilder {
 	
 	public static void main(String[] args) {
 		
+		
+//		This is o(n) time complexity
 		String name = "Sai";
 		name += "s";
 		System.out.println(name);
 		
 		
+		
+//		This is o(1) time complexity - better
 		StringBuilder nameb = new StringBuilder("hello");
 		
 //		This will add to string
