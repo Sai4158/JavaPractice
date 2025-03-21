207450e839ee3309594252d226d73983eeba4aa2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-21 01:25:46 -0400

    mutable vs immutable

--- src/a_DSA_part_51_to_part_60/Day56_ToString_StringBuilder.java
+++ src/a_DSA_part_51_to_part_60/Day56_ToString_StringBuilder.java
@@ -4,10 +4,13 @@ public class Day56_ToString_StringBuilder {
 	
 	public static void main(String[] args) {
 		
+//		This is mutable 
 		StringBuilder s1 = new StringBuilder("TO STRING");
 		
 		System.out.println(s1.toString());
 		
 		
+//		This is immutable
+		String a = "Not mutable";
 	}
 }
