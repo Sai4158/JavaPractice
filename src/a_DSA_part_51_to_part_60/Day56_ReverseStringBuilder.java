b2ca8d36c65696d822cf1b82a3ddbbbbf850e852
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-21 01:27:14 -0400

    Simple reverse using stringbuilder

---
+++ src/a_DSA_part_51_to_part_60/Day56_ReverseStringBuilder.java
@@ -0,0 +1,15 @@
+package a_DSA_part_51_to_part_60;
+
+public class Day56_ReverseStringBuilder {
+	
+	public static void main(String[] args) {
+		
+		String s = "Sai";
+		
+		StringBuilder ab = new StringBuilder(s);
+		
+		ab.reverse();
+		
+		System.out.println(ab);
+	}
+}
