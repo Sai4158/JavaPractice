661b9c174419dbb2ffb81f68d24b3eaa6c28f93a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-21 01:24:15 -0400

    To String using StringBuilder

---
+++ src/a_DSA_part_51_to_part_60/Day56_ToString_StringBuilder.java
@@ -0,0 +1,13 @@
+package a_DSA_part_51_to_part_60;
+
+public class Day56_ToString_StringBuilder {
+	
+	public static void main(String[] args) {
+		
+		StringBuilder s1 = new StringBuilder("TO STRING");
+		
+		System.out.println(s1.toString());
+		
+		
+	}
+}
