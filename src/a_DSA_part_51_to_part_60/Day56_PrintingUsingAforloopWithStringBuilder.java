910beb6e08f5271f63504686c0ea989761904623
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-21 01:21:59 -0400

    StringBuilder printing at index

---
+++ src/a_DSA_part_51_to_part_60/Day56_PrintingUsingAforloopWithStringBuilder.java
@@ -0,0 +1,15 @@
+package a_DSA_part_51_to_part_60;
+
+public class Day56_PrintingUsingAforloopWithStringBuilder {
+	
+	public static void main(String[] args) {
+		
+		
+		StringBuilder a =  new StringBuilder("sAIIIII");
+		
+		
+		for (int i = 0; i < a.length(); i++) {
+			System.out.println(a.charAt(i));
+		}
+	}
+}
