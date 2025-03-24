cecc817a5933bfa9b34e2ff438ff175089e14df6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-24 03:06:18 -0400

    Leetcode - 434. Number of Segments in a String

---
+++ src/a_DSA_leetcode/_434_Number_of_Segments_in_a_String.java
@@ -0,0 +1,23 @@
+package a_DSA_leetcode;
+
+public class _434_Number_of_Segments_in_a_String {
+
+	public static int countSegments(String s) {
+
+		int count = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+
+			if(s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+				count++;
+			}
+
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(countSegments("Hello, my name is John")); 
+	}
+}
