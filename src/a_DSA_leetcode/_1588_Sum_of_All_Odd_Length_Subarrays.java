ee87bb2366b66f10f42fe4d3e93461fad93b4422
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-26 02:09:55 -0400

    Leetcode - 1588. Sum of All Odd Length Subarrays

--- src/a_DSA_leetcode/_1588_Sum_of_All_Odd_Length_Subarrays.java
+++ src/a_DSA_leetcode/_1588_Sum_of_All_Odd_Length_Subarrays.java
@@ -1,34 +1,26 @@
 package a_DSA_leetcode;
 
 public class _1588_Sum_of_All_Odd_Length_Subarrays {
-	
-    public static int sumOddLengthSubarrays(int[] arr) {
-        
-    	
-    	int n = arr.length;
-    	int l = 0;
-    	int temp =0;
-    	int ans = 0;
-    	int k = 0;
-    	
-    	for (int r = 0; r < n; r++) {
-			
-    		if(arr[r] % 2 == 1) {
-    			ans++;
-    		}
-    		
-    		while(temp > k) {
-    			if(arr[l] % 2 ==1) {
-					temp--;
+
+	public static int sumOddLengthSubarrays(int[] arr) {
+
+		int ans = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			int temp = 0;
+
+			for (int j = i; j < arr.length; j++) {
+				temp +=arr[j];
+
+				if((j-i+1) % 2 == 1) {
+					ans += temp;
 				}
-    			l++;
-    		}
-    		
-    		ans =  Math.max(ans, r-l+1);
+			}
 		}
-    	return ans;
-    }
-	
+		return ans;
+	}
+
 	public static void main(String[] args) {
 		int arr[] = {1,4,2,5,3};
 		System.out.println(sumOddLengthSubarrays(arr));
