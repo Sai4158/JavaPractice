ba723e87f365ecfad6b6abc1d2d0c0dfac38d396
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-26 02:39:38 -0400

    Leetcode - 2380. Time Needed to Rearrange a Binary String

---
+++ src/a_DSA_leetcode/_2380_Rearrange_Binary_String.java
@@ -0,0 +1,33 @@
+package a_DSA_leetcode;
+
+public class _2380_Rearrange_Binary_String {
+
+	public static int secondsToRemoveOccurrences(String s) {
+
+		int n =  s.length();
+		int ans = 0;
+		int zeroCount = 0;
+
+		for (int i = 0; i < n; i++) {
+
+			if(s.charAt(i) == '0') {
+				zeroCount++;
+			}
+
+			else if(zeroCount > 0) {
+
+				ans = Math.max(ans+1, zeroCount);
+			}
+
+		}
+
+		return ans;
+
+	}
+
+	public static void main(String[] args) {
+		String s = "11100";
+
+		System.out.println(secondsToRemoveOccurrences(s));
+	}
+}
