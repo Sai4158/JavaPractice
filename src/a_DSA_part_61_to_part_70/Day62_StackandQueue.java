61005a9920e4be200fffbd7c87594d048e49ab4e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-01 21:02:56 -0400

    Now I am popping these

--- src/a_DSA_part_61_to_part_70/Day62_StackandQueue.java
+++ src/a_DSA_part_61_to_part_70/Day62_StackandQueue.java
@@ -8,29 +8,29 @@ public class Day62_StackandQueue {
 	public static void main(String[] args) {
 		Macha m1 = new Macha(22, "Vignes");
 		Macha m2 = new Macha(22, "Dinesh");
-		
-		
-//		Adding the values to the stack
-		
+
+
+		//		Adding the values to the stack
 		Stack<Macha> st = new Stack<Macha>();
 		
 		st.push(m1);
-		st.push(m2);
+		st.push(m2);	
 		
-		
-		
-		System.out.println(st.size());
+		st.pop();
+		Macha temp = st.pop();
+
+		System.out.println(temp.age);
 	}
 }
 
 class Macha{
 	int age;
 	String name;
-	
+
 	public Macha(int age, String name) {
-		
+
 		age =  this.age;
 		name =  this.name;
-	
+
 	}
 }
