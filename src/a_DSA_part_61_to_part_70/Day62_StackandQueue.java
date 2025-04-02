3f74708686b4f492911f8817e6cac2be04b0f096
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-01 20:01:24 -0400

    Making simple object then adding to stack

---
+++ src/a_DSA_part_61_to_part_70/Day62_StackandQueue.java
@@ -0,0 +1,25 @@
+package a_DSA_part_61_to_part_70;
+
+public class Day62_StackandQueue {
+	public static void main(String[] args) {
+		Macha m1 = new Macha(22, "Vignes");
+		Macha m2 = new Macha(22, "Dinesh");
+		
+		
+//		Adding the values to the stack
+		
+		
+	}
+}
+
+class Macha{
+	int age;
+	String name;
+	
+	public Macha(int age, String name) {
+		
+		age =  this.age;
+		name =  this.name;
+	
+	}
+}
