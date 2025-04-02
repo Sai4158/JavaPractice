079d306e4fa8afbdf17f2f825e574ec7b1b534e2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-01 21:42:08 -0400

    Fixed the errors here

--- src/a_DSA_part_61_to_part_70/Days62_ObjectInQueue.java
+++ src/a_DSA_part_61_to_part_70/Days62_ObjectInQueue.java
@@ -2,35 +2,32 @@ package a_DSA_part_61_to_part_70;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 public class Days62_ObjectInQueue {
-	
-	public static void main(String[] args) {
-		Macha m1 = new Macha(22, "Vignes");
-		Macha m2 = new Macha(22, "Dinesh");
 
+    public static void main(String[] args) {
+    	person m1 = new person(22, "Vignes");
+    	person m2 = new person(22, "Dinesh");
 
-		//		Adding the values to the stack
-		Queue<Macha> st = new LinkedList<>();
-		
-		st.add(m1);
-		st.add(m2);	
-		
-		st.poll();
-		Macha temp = st.poll();
+        // Adding the values to the queue
+        Queue<person> st = new LinkedList<>();
 
-		System.out.println(temp.age);
-	}
+        st.add(m1);
+        st.add(m2);
+
+        st.poll(); 
+        person temp = st.poll(); 
+
+        System.out.println(temp.age);  
+    }
 }
 
-class Macha{
-	int age;
-	String name;
+class person {
+    int age;
+    String name;
 
-	public Macha(int age, String name) {
-
-		age =  this.age;
-		name =  this.name;
-
-	}
+    public person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
