706821349681307e1c4dcdfddedf71af93e3bcd9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-01 20:18:31 -0400

    Added the objects to stack and printing the size 2

--- src/a_DSA_part_61_to_part_70/Day62_StackandQueue.java
+++ src/a_DSA_part_61_to_part_70/Day62_StackandQueue.java
@@ -1,5 +1,9 @@
 package a_DSA_part_61_to_part_70;
 
+import java.util.Stack;
+
+import javax.crypto.Mac;
+
 public class Day62_StackandQueue {
 	public static void main(String[] args) {
 		Macha m1 = new Macha(22, "Vignes");
@@ -8,7 +12,14 @@ public class Day62_StackandQueue {
 		
 //		Adding the values to the stack
 		
+		Stack<Macha> st = new Stack<Macha>();
 		
+		st.push(m1);
+		st.push(m2);
+		
+		
+		
+		System.out.println(st.size());
 	}
 }
 
