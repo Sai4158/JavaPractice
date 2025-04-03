480f3448748cfff8fbde448c133e7ac9365a2b00
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-03 18:07:33 -0400

    Adding first and last in dq

---
+++ src/a_DSA_part_61_to_part_70/Day63_IntroDeque.java
@@ -0,0 +1,19 @@
+package a_DSA_part_61_to_part_70;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Day63_IntroDeque {
+	
+	public static void main(String[] args) {
+//		Deque  is double ended 
+		
+		Deque<Integer> dq = new LinkedList<Integer>();
+		
+		dq.addLast(1);
+		
+		dq.addFirst(2);
+		
+		System.out.println(dq);
+	}
+}
