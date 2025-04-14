9a1a952429e4733e31d58b6b0307ba356b62ca71
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-13 23:26:38 -0400

    Leetcode - 1880. Check if Word Equals Summation of Two Words

---
+++ src/a_DSA_leetcode/_1880_Check_if_Word_Equals_Summation_of_Two_Words.java
@@ -0,0 +1,35 @@
+package a_DSA_leetcode;
+
+public class _1880_Check_if_Word_Equals_Summation_of_Two_Words {
+	
+    public static boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+            	
+    	int sum1 = 0;
+    	int sum2 = 0;
+    	int sum3 = 0;
+    	
+    	for (int i = 0; i < firstWord.length(); i++) {
+    		sum1 = sum1 * 10 + (firstWord.charAt(i) - 'a');
+    	}
+    	
+    	for (int i = 0; i < secondWord.length(); i++) {
+    		sum2 = sum2 * 10 + (secondWord.charAt(i) - 'a');
+    	}
+    	
+    	for (int i = 0; i < targetWord.length(); i++) {
+    		sum3 = sum3 * 10 + (targetWord.charAt(i) - 'a');
+    	}
+    	
+    	return sum1 + sum2 == sum3;
+    }
+	
+	
+    public static void main(String[] args) {
+		String firstWord = "acb";
+		String secondWord = "cba";
+		String targetWord = "cdb";
+		
+		System.out.println(isSumEqual(firstWord, secondWord, targetWord));  
+	}
+	
+}
