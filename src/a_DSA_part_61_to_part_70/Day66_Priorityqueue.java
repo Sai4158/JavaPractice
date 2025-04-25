4f4e0e9acbcf0b95356e0bde0097fbcebdec882c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-25 01:32:30 -0400

    Adding in PriorityQueue

---
+++ src/a_DSA_part_61_to_part_70/Day66_Priorityqueue.java
@@ -0,0 +1,18 @@
+package a_DSA_part_61_to_part_70;
+
+import java.util.PriorityQueue;
+
+public class Day66_Priorityqueue {
+
+	public static void main(String[] args) {
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+	
+		pq.add(5);
+		pq.add(3);
+		pq.add(10);
+		
+		System.out.println(pq.peek());
+		
+	}
+}
