3a223bdb5014e1400ad153d7b805ff581e2f1271
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-29 18:34:22 -0400

    Leetcode - 3232. Find if Digit Game Can Be Won

---
+++ src/a_DSA_leetcode/_3232_Find_if_Digit_Game_Can_Be_Won.java
@@ -0,0 +1,42 @@
+package a_DSA_leetcode;
+
+public class _3232_Find_if_Digit_Game_Can_Be_Won {
+
+    public static boolean canAliceWin(int[] nums) {
+        
+    	int n = nums.length;
+    	
+    	int singleCount = 0;
+    	int doubleCount = 0;
+    	
+    	
+    	for (int i = 0; i < n; i++) {
+			if(nums[i] <= 9) {
+				singleCount += nums[i];
+			}else {
+				doubleCount += nums[i];
+			}
+		}
+
+    	int total = singleCount + doubleCount;
+    	
+    	
+    	if(singleCount > total - singleCount) {
+    		return true;
+    	}
+    	if(doubleCount > total - doubleCount) {
+    		return true;
+    	}
+    	
+    	
+    	return false;
+    }
+	
+	public static void main(String[] args) {
+		
+		int nums[] = {1,2,3,4,10};
+		
+		System.out.println(canAliceWin(nums));
+		
+	}
+}
