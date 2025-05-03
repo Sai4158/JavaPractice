18f578148b4e055ac46d1138f3e494f7659f8ecc
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-03 11:34:41 -0400

    Added comments

--- src/a_DSA_leetcode/_476_Number_Complement.java
+++ src/a_DSA_leetcode/_476_Number_Complement.java
@@ -4,12 +4,12 @@ public class _476_Number_Complement {
 
 	public static int findComplement(int num) {
 
+//		Convert to binary and using to stringbuilder to add 
 		String toBinary = Integer.toBinaryString(num);
-
 		StringBuilder sb = new StringBuilder();
 
 
-
+// for loops to check if 0 then add 1 or else
 		for (int i = 0; i < toBinary.length(); i++) {
 
 			if(toBinary.charAt(i) == '0') {
@@ -20,6 +20,8 @@ public class _476_Number_Complement {
 
 		}
 
+		
+//		convert int to string
 		return Integer.parseInt(sb.toString(),2);
 
 	}
