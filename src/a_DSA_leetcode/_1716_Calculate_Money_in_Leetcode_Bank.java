b29b5efe108cc9486cbc48828a3442ff627ee668
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-09 02:54:41 -0400

    Leetcode - 1716. Calculate Money in Leetcode Bank

--- src/a_DSA_leetcode/_1716_Calculate_Money_in_Leetcode_Bank.java
+++ src/a_DSA_leetcode/_1716_Calculate_Money_in_Leetcode_Bank.java
@@ -5,11 +5,11 @@ public class _1716_Calculate_Money_in_Leetcode_Bank {
 	public static int totalMoney(int n) {
 
 		int count = 0;
-		int week = 0;
+		int week = 1;
 
 		for (int i = 1; i <= n; i++) {
 
-			count += week +(i%7 == 0 ? 7 : i % 7);
+			count += week  + (i - 1) % 7;
 
 			if(i % 7 == 0) {
 				week++;
