f9f6716514e13fd27d32e3c80a4df0a4cb982559
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-12 16:23:21 -0400

    Added comments

--- src/a_DSA_leetcode/_2283_Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value.java
+++ src/a_DSA_leetcode/_2283_Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value.java
@@ -6,8 +6,10 @@ public class _2283_Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value {
 	
     public static boolean digitCount(String num) {
         
+//    	Using hashmap
     	HashMap<Integer, Integer> hm =  new HashMap<Integer, Integer>();
-    	
+
+//Forloop to add the numbers and the count
     	for (int i = 0; i < num.length(); i++) {
 			
     		int digit = num.charAt(i)- '0';
@@ -15,6 +17,7 @@ public class _2283_Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value {
     		hm.put(digit, hm.getOrDefault(digit, 0)+1);
 		}
     	
+//    	Forloop to check the if its not the same then return false 
     	for (int i = 0; i < num.length(); i++) {
 			
     		int excepted  = num.charAt(i) - '0';
