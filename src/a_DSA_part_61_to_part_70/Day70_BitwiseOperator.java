79ba79f1a2bc5b547213fdad4c0745defd2abd3a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-14 17:35:26 -0400

    Day 70 Bitwise Operators

--- src/a_DSA_part_61_to_part_70/Day70_BitwiseOperator.java
+++ src/a_DSA_part_61_to_part_70/Day70_BitwiseOperator.java
@@ -5,8 +5,16 @@ public class Day70_BitwiseOperator {
 	public static void main(String[] args) {
 		
 		int n = 9;
+		int m = 10;
 		String binary  = Integer.toBinaryString(n);
 	
-		System.out.println();
+		System.out.println(binary);
+		
+		System.out.println(n | m);
+		System.out.println(n & m);
+		System.out.println(n ^ m);
+		System.out.println(n >> m);
+		System.out.println(n << m);
+		
 	}
 }
