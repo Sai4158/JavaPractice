c220269622f9d746ae96b43231dd5e832075894d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-14 17:40:33 -0400

    Simple Ternary perator

---
+++ src/a_DSA_part_61_to_part_70/Day70_TernaryOperator.java
@@ -0,0 +1,14 @@
+package a_DSA_part_61_to_part_70;
+
+public class Day70_TernaryOperator {
+
+	public static void main(String[] args) {
+
+		int ans =  2;
+		String ans1 ="";
+
+		ans1 =  (ans % 2 == 0) ? "Even" : "Odd";
+		
+		System.out.println(ans1);
+	}
+}
