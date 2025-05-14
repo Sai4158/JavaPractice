ea5b9ef7c115ca07811e840298f5276d561d380b
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-14 17:30:36 -0400

    Converting a int to binary

---
+++ src/a_DSA_part_61_to_part_70/Day70_BitwiseOperator.java
@@ -0,0 +1,12 @@
+package a_DSA_part_61_to_part_70;
+
+public class Day70_BitwiseOperator {
+	
+	public static void main(String[] args) {
+		
+		int n = 9;
+		String binary  = Integer.toBinaryString(n);
+	
+		System.out.println();
+	}
+}
