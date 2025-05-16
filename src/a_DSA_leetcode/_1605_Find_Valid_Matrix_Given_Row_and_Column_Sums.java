d76b9df406ae1495e2bb5a59c8a08bf8d4438e9f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-16 03:11:53 -0400

    Added comments

--- src/a_DSA_leetcode/_1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.java
+++ src/a_DSA_leetcode/_1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.java
@@ -8,22 +8,30 @@ public class _1605_Find_Valid_Matrix_Given_Row_and_Column_Sums {
 
 	public static List<List<Integer>> findMatrix(int[] nums) {
 
+//		Len of the array
 		int n = nums.length;
+		
+//		HashMap
 		HashMap<Integer, Integer> freq = new HashMap<>();
+		
+//		List 
 		List<List<Integer>> ans =  new 	ArrayList<List<Integer>>();
 		
+//		forLoop
 		for (int i = 0; i < n; i++) {
+//			Count the freq of the numbers
 			int count = freq.getOrDefault(nums[i], 0);
 
+//			if the list size does not match the size of the count  - make a new arrayList to seprate
 			if (ans.size() <= count) {
 				ans.add(new ArrayList<>());
 			}
 
 			ans.get(count).add(nums[i]);
-
 			freq.put(nums[i], count + 1);
 		}
 
+//		Return the ans
 		return ans;
 	}
 	
