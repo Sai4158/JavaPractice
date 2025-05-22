6e5928dc52403eb8eb30be15af4f62b3898a9302
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-22 01:49:10 -0400

    Added comments

--- src/a_DSA_leetcode/_1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.java
+++ src/a_DSA_leetcode/_1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.java
@@ -7,22 +7,28 @@ public class _1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence 
 	
     public static int isPrefixOfWord(String sentence, String searchWord) {
         
+    	
     	int n =  searchWord.length();
     	
+//    	split the words by space
     	String[] words  =  sentence.trim().split(" ");
+    	
+//    	Array list to store each words
     	ArrayList<String> al = new ArrayList<String>();
     	
+//    	Add the words to the arrayList
     	for (int i = 0; i < words.length; i++) {
     		al.add(words[i]);
 		}
     	
+//    	in the ArrayList, search the word using startsWith and if so return index + 1
     	for (int i = 0; i < al.size(); i++) {
-			
     		if(al.get(i).startsWith(searchWord)) {
     			return i + 1;
     		}
 		}
     	
+//    	if not return -1
     	return -1;
     }
 	
