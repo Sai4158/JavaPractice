4ad78566481c390d7e144ed061a4e9a3de06a660
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-26 17:03:04 -0400

    Leetcode - 2023. Number of Pairs of Strings With Concatenation Equal to Target

---
+++ src/a_DSA_leetcode/_2023_Number_of_Pairs_of_Strings_With_Concatenation_Equal_to_Target.java
@@ -0,0 +1,34 @@
+package a_DSA_leetcode;
+
+public class _2023_Number_of_Pairs_of_Strings_With_Concatenation_Equal_to_Target {
+	
+    public static int numOfPairs(String[] nums, String target) {
+        
+    	int n =  nums.length;
+    	
+    	String combined  = "";
+    	
+    	int count = 0;
+    	
+    	for (int i = 0; i < n; i++) {
+			
+    		for (int j = i; j < n; j++) {
+				
+    			combined = nums[i] + nums[j];
+    			
+    			if(combined.equals(target)) {
+    				
+    				count ++;
+    			}
+    			
+			}
+    		
+		}
+    	return count;
+    }
+	
+	public static void main(String[] args) {
+		String nums[] = {"777","7","77","77"}, target = "7777";
+		System.out.println(numOfPairs(nums, target));	
+	}
+}
