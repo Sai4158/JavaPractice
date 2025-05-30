986464e469ce0cd6d0deb2db4dd1d364405cc7fe
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-30 02:44:07 -0400

    Added comments

--- src/a_DSA_leetcode/_2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.java
+++ src/a_DSA_leetcode/_2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.java
@@ -4,18 +4,23 @@ public class _2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K {
 
     public static int minOperations(int[] nums, int k) {
         
+//    	set to 0
 		int xor = 0;
 
+		
     	for (int i = 0; i < nums.length; i++) {
-			    		
+		
+//    		XOR all the array values 
     		xor ^= nums[i];
   
 		}
+    
     	
-    	
+//    	find the diffrence between the XOR and k
     	int diff =  xor ^ k;
     	
     	
+//    	Count all the ones after converting using bitcount
     	return Integer.bitCount(diff);
     	
     }
