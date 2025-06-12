da2f945bebe94bd6d9d9b7333620613c5862d466
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-06-12 17:51:13 -0400

    2057. Smallest Index With Equal Value

---
+++ src/a_DSA_leetcode/_2057_Smallest_Index_With_Equal_Value.java
@@ -0,0 +1,22 @@
+package a_DSA_leetcode;
+
+
+public class _2057_Smallest_Index_With_Equal_Value {
+    public static int smallestEqual(int[] nums) {
+        
+    	
+    	for (int i = 0; i < nums.length; i++) {
+			
+    		if(i % 10 == nums[i]) {
+    			return nums[i];
+    		}
+    		
+		}
+    	return -1;
+    }
+    
+    public static void main(String[] args) {
+		int nums[] = {0,1,2};
+		System.out.println(smallestEqual(nums));
+	}
+}
