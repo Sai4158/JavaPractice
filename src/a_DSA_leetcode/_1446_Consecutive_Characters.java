4e1d8c92b2e72f7530fe40dadee7f98b9b725313
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-07-05 16:36:59 -0400

    Consecutive Characters

--- src/a_DSA_leetcode/_1446_Consecutive_Characters.java
+++ src/a_DSA_leetcode/_1446_Consecutive_Characters.java
@@ -8,17 +8,23 @@ public class _1446_Consecutive_Characters {
         int count = 1;
     	int n = s.length();
     	
+//    	Start a forloop till the s.lenght
     	for (int i = 1; i < n; i++) {
 			
+    		//if 0 or 1 index char matches then count++;
     		if(s.charAt(i) == s.charAt(i-1)) {
     			count++;
     			max =  Math.max(count, max);
     			
+    			
+//    			Otherwise make it 0
     		}else {
     			count = 1;
     		}
     		
 		}
+    	
+//    	Return the max amount of count found it
     	return max;
     }
 	
