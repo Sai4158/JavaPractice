80453e65d2a586d46f13e4d7124792931d46f1ae
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-07-06 15:25:26 -0400

    2073. Time Needed to Buy Tickets

---
+++ src/a_DSA_leetcode/_2073_Time_Needed_to_Buy_Tickets.java
@@ -0,0 +1,27 @@
+package a_DSA_leetcode;
+
+public class _2073_Time_Needed_to_Buy_Tickets {
+	
+    public static int timeRequiredToBuy(int[] tickets, int k) {
+        
+    	int n = tickets.length;
+    	
+    	int count = 0;
+    	
+    	for (int i = 0; i < n; i++) {
+			
+    		if(i <= k) {
+    			
+    			count += Math.min(tickets[i], tickets[k]);
+    		}else {
+    			count += Math.min(tickets[i], tickets[k]-1);
+    		}
+    		
+		}
+    	return count;
+    }
+	
+	public static void main(String[] args) {
+		 System.out.println(timeRequiredToBuy(new int[]{2, 3, 2}, 2));
+	}
+}
