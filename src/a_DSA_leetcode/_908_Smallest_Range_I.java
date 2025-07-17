1822b06296f26acf2f5bd0ab52c1c9dd309549b7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-07-17 15:27:42 -0400

    908. Smallest Range I

--- src/a_DSA_leetcode/_908_Smallest_Range_I.java
+++ src/a_DSA_leetcode/_908_Smallest_Range_I.java
@@ -2,42 +2,22 @@ package a_DSA_leetcode;
 
 public class _908_Smallest_Range_I {
 
-	public static int smallestRangeI(int[] nums, int k) {
-		// Step 1: Find the minimum and maximum values in the array
-		int n = nums.length;
-		int min =  nums[0];
-		int max =  nums[0];
-
-		for (int i = 0; i < n; i++) {
-
-			if( nums[i] < k) {
-
-				min = nums[i];
-
-			}
-
-			if(nums[i] > k) {
-				max =  nums[i];
-			}
-
-		}
-		// Step 2: Try to reduce the difference between max and min by 2*k
-
-		int diff =  max - min;
-
-		int reduced  = diff - 2 *k;
-
-		// Step 3: If reduced is negative, return 0, else return reduced
-
-		if(reduced < 0) {
-			return 0;
-		}
-
-		else {
-			return reduced;
-		}
-
-	}
+    public static int smallestRangeI(int[] nums, int k) {
+        int min = nums[0];
+        int max = nums[0];
+        
+        // Find the actual min and max in the array
+        for (int i = 1; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        
+        int diff = max - min;
+        int reduced = diff - 2 * k;
+        
+        // If reduced is less than 0, return 0
+        return Math.max(0, reduced);
+    }
 
 
 	public static void main(String[] args) {
