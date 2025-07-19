dc198b3d134a63144b9179ecced020b0942e973f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-07-19 00:57:20 -0400

    Added comments

--- src/a_DSA_leetcode/_693_Binary_Number_with_Alternating_Bits.java
+++ src/a_DSA_leetcode/_693_Binary_Number_with_Alternating_Bits.java
@@ -4,13 +4,18 @@ public class _693_Binary_Number_with_Alternating_Bits {
 
 	public static boolean hasAlternatingBits(int n) {
 
+		
+//		Convert int to binary string
 		String binary = Integer.toBinaryString(n);
 		for (int i = 1; i < binary.length(); i++) {
+			
+//			Return false
 			if (binary.charAt(i) == binary.charAt(i - 1)) {
 				return false;
 			}
 		}
 
+//		Return true
 		return true;
 	}
 
