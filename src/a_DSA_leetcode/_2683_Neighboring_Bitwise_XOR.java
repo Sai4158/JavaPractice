5f316bf5f49469eb591e32451b87a589a015bfb2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-01 17:49:34 -0400

    Added comments

--- src/a_DSA_leetcode/_2683_Neighboring_Bitwise_XOR.java
+++ src/a_DSA_leetcode/_2683_Neighboring_Bitwise_XOR.java
@@ -4,17 +4,21 @@ public class _2683_Neighboring_Bitwise_XOR {
 
 	public static boolean doesValidArrayExist(int[] derived) {
 
+//		Length of the nums
 		int n =  derived.length;
-
-
+		
+// 		Set ans to  0
 		int ans = 0;
 
+//		Run for loop from i to n
 		for (int i = 0; i < n; i++) {
 
+//			Add all the XOR into ans
 			ans ^= derived[i];
 
 		}
 
+//		if it the ans is 0 then true or false
 		return ans == 0;
 
 
