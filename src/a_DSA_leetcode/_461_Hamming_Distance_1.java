9dfcb52ec2eee76443cdb0dfaf1e3e156b76c2a8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-02 23:33:47 -0400

    461. Hamming Distance

---
+++ src/a_DSA_leetcode/_461_Hamming_Distance_1.java
@@ -0,0 +1,18 @@
+package a_DSA_leetcode;
+
+public class _461_Hamming_Distance_1 {
+
+
+	public static int hammingDistance(int x, int y) {
+		
+		return Integer.bitCount(x ^ y);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		int x = 1, y = 4;
+		System.out.println(hammingDistance(x, y));
+		
+	}
+}
