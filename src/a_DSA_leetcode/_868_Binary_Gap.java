b4ecb591ceb842232cafafb72cfae15ba9867cf0
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-13 23:20:37 -0400

    Add solution for Leetcode 868 Binary Gap
    Implements the binaryGap method to find the maximum distance between consecutive 1's in the binary representation of a number. Includes a main method for testing with sample input.

---
+++ src/a_DSA_leetcode/_868_Binary_Gap.java
@@ -0,0 +1,37 @@
+package a_DSA_leetcode;
+
+public class _868_Binary_Gap {
+
+	public static int binaryGap(int n) {
+				
+		String  bit =  Integer.toBinaryString(n);
+		
+		
+		int last = -1;
+		int max = 0;
+		
+		for (int i = 0; i < bit.length(); i++) {
+			
+			char c =  bit.charAt(i);
+			
+			if(c == '1') {
+				if(last != -1) {
+					
+					int gap = i - last;
+					
+					if(gap > last) {
+						max =  gap;
+					}
+					
+				}
+				last = i;
+			}
+			
+		}
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(binaryGap(22));
+	}
+}
