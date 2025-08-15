b25635cf779eb3372d040d2210995846157d68c7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-15 15:08:52 -0400

    1752. Check if Array Is Sorted and Rotated

---
+++ src/a_DSA_leetcode/_1752_Check_if_Array_Is_Sorted_and_Rotated.java
@@ -0,0 +1,30 @@
+package a_DSA_leetcode;
+
+public class _1752_Check_if_Array_Is_Sorted_and_Rotated {
+
+    public static boolean check(int[] nums) {
+        
+    	int n = nums.length;
+    	int count = 0;
+    	
+    	for (int i = 0; i < n-1; i++) {
+			
+    		if(nums[i] >  nums[i+1]) {
+    			count++;
+    		}
+    		
+		}
+    	
+    	if(nums[n-1] > nums[0]) {
+    		count++;
+    	}
+    	
+    	return count <= 1;
+    	
+    }
+    
+    public static void main(String[] args) {
+		int nums[] = {3,4,5,1,2};
+		System.out.println(check(nums));
+	}
+}
