6ddfff2a6b98bbb6943ee0165465d81a636be674
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-17 17:32:22 -0400

    Comments

--- src/a_DSA_leetcode/_1752_Check_if_Array_Is_Sorted_and_Rotated.java
+++ src/a_DSA_leetcode/_1752_Check_if_Array_Is_Sorted_and_Rotated.java
@@ -7,18 +7,22 @@ public class _1752_Check_if_Array_Is_Sorted_and_Rotated {
     	int n = nums.length;
     	int count = 0;
     	
+//    	Forloop
     	for (int i = 0; i < n-1; i++) {
 			
+//    		Count them
     		if(nums[i] >  nums[i+1]) {
     			count++;
     		}
     		
 		}
     	
+//    	Checking if
     	if(nums[n-1] > nums[0]) {
     		count++;
     	}
     	
+//    	Return
     	return count <= 1;
     	
     }
