7223a09c55853b000555097c4f791b4e0f0a7e82
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-20 16:50:14 -0400

    888. Fair Candy Swap

---
+++ src/a_DSA_leetcode/_888_Fair_Candy_Swap.java
@@ -0,0 +1,11 @@
+package a_DSA_leetcode;
+
+public class _888_Fair_Candy_Swap {
+
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        
+    }
+    public static void main(String[] args) {
+		
+	}
+}
