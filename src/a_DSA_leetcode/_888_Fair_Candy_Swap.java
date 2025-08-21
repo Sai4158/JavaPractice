312d240851566737abb1d4dcdce46e1a9f062cd6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-21 01:27:06 -0400

    888. Fair Candy Swap

--- src/a_DSA_leetcode/_888_Fair_Candy_Swap.java
+++ src/a_DSA_leetcode/_888_Fair_Candy_Swap.java
@@ -1,11 +1,48 @@
 package a_DSA_leetcode;
 
+import java.util.HashSet;
+
+
 public class _888_Fair_Candy_Swap {
 
-    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+    public static int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
         
+    	int suma = 0;
+    	int sumb = 0;
+    	
+    	for (int i = 0; i < aliceSizes.length; i++) {
+			suma += aliceSizes[i];
+		}
+    	
+    	for (int i = 0; i < bobSizes.length; i++) {
+			sumb += bobSizes[i];
+		}
+    	
+    	int diff = (suma - sumb) / 2;
+    	
+    	HashSet<Integer> setb = new HashSet<Integer>();
+    	for (int i = 0; i < bobSizes.length; i++) {	
+    		setb.add(bobSizes[i]);
+		}
+    	
+    	for (int i = 0; i < aliceSizes.length; i++) {
+			
+            int x = aliceSizes[i];
+    		int y = x - diff;
+			
+			if(setb.contains(y)) {
+				return new int[] {x,y};
+			}
+		}
+    	
+    	
+    	return new int[0];
+
+    	
+    	
     }
     public static void main(String[] args) {
-		
+		int liceSizes[] = {1,1}, bobSizes[] = {2,2};
+		System.out.println(fairCandySwap(liceSizes, bobSizes));
 	}
 }
