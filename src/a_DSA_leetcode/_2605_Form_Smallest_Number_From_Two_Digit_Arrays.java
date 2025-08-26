cf22563ed03306f2f364420ba077b5f69de622be
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-08-25 20:20:27 -0400

    2605. Form Smallest Number From Two Digit Arrays

--- src/a_DSA_leetcode/_2605_Form_Smallest_Number_From_Two_Digit_Arrays.java
+++ src/a_DSA_leetcode/_2605_Form_Smallest_Number_From_Two_Digit_Arrays.java
@@ -10,25 +10,17 @@ public class _2605_Form_Smallest_Number_From_Two_Digit_Arrays {
 		Arrays.sort(nums2);
 
 
-		for (int i = 0; i < nums2.length; i++) {
-
-
-			for (int j = 0; j < nums2.length; j++) {
-
-
-				if(nums1[i] ==  nums2[j]) {
-
-					return nums1[i];
-				}
-
-			}
-
-		}
+		for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    return nums1[i];
+                }
+            }
+        }
 
 		int a  =  nums1[0];
 		int b  =  nums2[0];
 
-		String ans3 = "";
 
 		if(a < b) {
 			return Integer.parseInt(Integer.toString(a) + Integer.toString(b));
