908149885487b90625e601a03188113658ef33f5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-02 22:46:30 -0400

    2769. Find the Maximum Achievable Number

---
+++ src/a_DSA_leetcode/_2769_Find_the_Maximum_Achievable_Number1.java
@@ -0,0 +1,9 @@
+package a_DSA_leetcode;
+
+public class _2769_Find_the_Maximum_Achievable_Number1 {
+    public int theMaximumAchievableX(int num, int t) {
+        
+        return num + 2 *t;
+
+    }
+}
