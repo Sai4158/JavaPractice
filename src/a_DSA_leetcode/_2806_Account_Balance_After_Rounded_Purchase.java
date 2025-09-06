c254cf275b9cd71f38b3118aedfd5994d0be5ee9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-05 22:43:44 -0400

    Added comments

--- src/a_DSA_leetcode/_2806_Account_Balance_After_Rounded_Purchase.java
+++ src/a_DSA_leetcode/_2806_Account_Balance_After_Rounded_Purchase.java
@@ -4,12 +4,15 @@ public class _2806_Account_Balance_After_Rounded_Purchase {
 
     public static int accountBalanceAfterPurchase(int purchaseAmount) {
             	
+//    	f =  float
     	int cut =  Math.round(purchaseAmount/10.0f)*10;
     	
+//    	return the count -  the original amount 100
     	return 100 - cut;
     	
     }
 	
+//    main
 	public static void main(String[] args) {
 		int purchaseAmount = 10;
 		System.out.println(accountBalanceAfterPurchase(purchaseAmount));
