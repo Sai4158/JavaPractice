d162f47c6b834454bb4e71aaa9b9895d47a97e07
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-06 20:03:59 -0400

    2578. Split With Minimum Sum

---
+++ src/a_DSA_leetcode/_2578_Split_With_Minimum_Sum.java
@@ -0,0 +1,41 @@
+package a_DSA_leetcode;
+
+import java.util.Arrays;
+
+public class _2578_Split_With_Minimum_Sum {
+	
+    public static int splitNum(int num) {
+        
+    	char[] digits = String.valueOf(num).toCharArray();
+    	
+    	Arrays.sort(digits);
+    	
+    	int num1 = 0;
+    	int num2 = 0;
+    	
+    	
+    	for (int i = 0; i < digits.length; i++) {
+			
+    		int d =  digits[i] - '0';
+    		
+    		if(digits[i] % 2 == 0) {
+    			num1 =  num1 * 10 + d;
+    		}
+    		else{
+    			num2 =  num2 * 10 + d;
+
+    		}
+		}
+    	
+    	return num1 + num2;
+    	
+    	
+    }
+	
+	public static void main(String[] args) {
+		
+		int num = 4325;
+		System.out.println(splitNum(num));
+		
+	}
+}
