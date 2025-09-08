33bd9bb93a8aa29b7184e3472afd49e25a6956c7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-07 22:48:43 -0400

    2255. Count Prefixes of a Given String

---
+++ src/a_DSA_leetcode/_2255_Count_Prefixes_of_a_Given_String.java
@@ -0,0 +1,12 @@
+package a_DSA_leetcode;
+
+public class _2255_Count_Prefixes_of_a_Given_String {
+
+    public int countPrefixes(String[] words, String s) {
+        
+    }
+    
+    public static void main(String[] args) {
+		
+	}
+}
