a64111a6ede0f080b6666238149c481ef752bdf9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-10 03:45:20 -0400

    1047. Remove All Adjacent Duplicates In String

--- src/a_DSA_leetcode/_1047_Remove_All_Adjacent_Duplicates_In_String.java
+++ src/a_DSA_leetcode/_1047_Remove_All_Adjacent_Duplicates_In_String.java
@@ -3,20 +3,27 @@ package a_DSA_leetcode;
 public class _1047_Remove_All_Adjacent_Duplicates_In_String {
 	
     public static String removeDuplicates(String s) {
-        
-    	int n = s.length();
     	
-    	StringBuilder sb = new StringBuilder();
-    	
-    	for (int i = 0; i < n-1; i++) {
-			
-    		if(s.charAt(i) ==  s.charAt(i+1))
-    		
-		}
-    	
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+        	
+            char c = s.charAt(i);
+
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
+                sb.deleteCharAt(sb.length() - 1);
+            } 
+            
+            else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 	
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("abbaca")); 
+        System.out.println(removeDuplicates("azxxzy")); 
+    }
 }
