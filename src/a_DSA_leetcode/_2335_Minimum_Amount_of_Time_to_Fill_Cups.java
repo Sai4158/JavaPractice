43b5a986c8eaff44795dfec71a6f350ccfa33804
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-15 22:22:03 -0400

    Added comments

--- src/a_DSA_leetcode/_2335_Minimum_Amount_of_Time_to_Fill_Cups.java
+++ src/a_DSA_leetcode/_2335_Minimum_Amount_of_Time_to_Fill_Cups.java
@@ -4,18 +4,24 @@ import java.util.Arrays;
 
 public class _2335_Minimum_Amount_of_Time_to_Fill_Cups {
 
+
     public static int fillCups(int[] amount) {
         
+//    	Sort all the amount in the array
     	Arrays.sort(amount);
     	
+    	
+//    	Add all the 3 nums in the array to find the sum
     	int sum =  amount[0] + amount[1] + amount[2];
     	
     	
+//    	Once you find the sum, return which is max
     	return Math.max((sum+1)/2, amount[2]);
     	
     	
     }
     
+//    Main method
     public static void main(String[] args) {
 		
     	int amount[] = {1,4,2};
