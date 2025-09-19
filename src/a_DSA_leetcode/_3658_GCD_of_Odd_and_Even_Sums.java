ae51030b29082a57a1ebfc0825791a0d5d14aecb
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-19 17:58:28 -0400

    3658. GCD of Odd and Even Sums

---
+++ src/a_DSA_leetcode/_3658_GCD_of_Odd_and_Even_Sums.java
@@ -0,0 +1,16 @@
+package a_DSA_leetcode;
+
+public class _3658_GCD_of_Odd_and_Even_Sums {
+    
+	public static int gcdOfOddEvenSums(int n) {
+        
+        return n;
+
+		
+    }
+    
+    public static void main(String[] args) {
+		int n = 4;
+		System.out.println(gcdOfOddEvenSums(n));
+	}
+}
