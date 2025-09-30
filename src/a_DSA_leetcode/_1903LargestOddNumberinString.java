2533799a4ea6d57208ab73e6de97cff3f0523e43
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-09-30 02:46:22 -0400

    1903. Largest Odd Number in String

--- src/a_DSA_leetcode/_1903LargestOddNumberinString.java
+++ src/a_DSA_leetcode/_1903LargestOddNumberinString.java
@@ -3,31 +3,22 @@ package a_DSA_leetcode;
 
 public class _1903LargestOddNumberinString {
 
-	public static String largestOddNumber(String num) {
+    public static String largestOddNumber(String num) {
 
-		int n =  num.length();
-		String arr[] =  new String[n+1];
+        int n = num.length();
 
-		for (int i = 0; i < n; i++) {
-            arr[i] = num.substring(0, i + 1);  
-		}
+        for (int i = n - 1; i >= 0; i--) {
+        	
+            char c = num.charAt(i);
+            int digit = c - '0';
 
-        String maxOdd = "";
+            if (digit % 2 == 1) {
+                return num.substring(0, i + 1);
+            }
+        }
 
-
-		for (int i = 0; i < n; i++) {
-
-			String curr = arr[i];
-			int lastDigit =  curr.charAt(curr.length() - 1) - '0';
-				
-			if(Integer.parseInt(arr[i]) % 2 == 1) {
-				maxOdd =  curr;
-			}
-		}
-
-		return maxOdd;  
-
-	}
+        return "";
+    }
 
 	public static void main(String[] args) {
 
