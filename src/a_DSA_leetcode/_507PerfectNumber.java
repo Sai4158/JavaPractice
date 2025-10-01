43a6a1021da251a5b6021f3b1c0f989d742eddbf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-10-01 16:33:32 -0400

    507. Perfect Number

---
+++ src/a_DSA_leetcode/_507PerfectNumber.java
@@ -0,0 +1,32 @@
+package a_DSA_leetcode;
+
+public class _507PerfectNumber {
+	
+    public static boolean checkPerfectNumber(int num) {
+        
+    	if(num == 1) {
+    		return false;
+    	}
+    	
+    	int sum = 1;
+    	
+    	for (int i = 2; i * i <= num; i++) {
+			
+    		if(num % i ==0) {
+    			sum += i;
+    			
+        		
+        		if(i != num/i) {
+        			sum += num/i;
+        		}
+    			
+    		}
+    		
+		}
+    	
+    	return sum == num;
+    	
+    }
+	
+
+}
