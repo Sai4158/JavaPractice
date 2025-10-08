b93bd588f73bb978ff810fd6b7f1bf9b5fc5d017
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-10-07 21:45:09 -0400

    594. Longest Harmonious Subsequence

---
+++ src/a_DSA_leetcode/_594LongestHarmoniousSubsequence.java
@@ -0,0 +1,39 @@
+package a_DSA_leetcode;
+
+import java.util.HashMap;
+
+public class _594LongestHarmoniousSubsequence {
+
+	
+    public static int findLHS(int[] nums) {
+     
+    	int n = nums.length;
+    	
+    	HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+    	
+    	
+    	for (int i = 0; i < n; i++) {
+			hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+		}
+    	
+    	int ans =0;
+    	
+    	 for (int x : hm.keySet()) {
+             if (hm.containsKey(x + 1)) {
+                 ans = Math.max(ans, hm.get(x) + hm.get(x + 1));
+             }
+         }
+    	 
+    	 
+    	 return ans;
+    }
+	
+    
+    public static void main(String[] args) {
+		
+    	int  nums[] = {1,3,2,2,5,2,3,7};
+    	System.out.println(findLHS(nums));
+    	
+	}
+    
+}
