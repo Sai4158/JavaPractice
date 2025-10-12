7d62b544430b46f3f9e2cc9be2352f05063cc94d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-10-12 15:21:47 -0400

    Added comments

--- src/a_DSA_leetcode/_492ConstructtheRectangle.java
+++ src/a_DSA_leetcode/_492ConstructtheRectangle.java
@@ -2,16 +2,22 @@ package a_DSA_leetcode;
 
 public class _492ConstructtheRectangle {
 	
+
     public static int[] constructRectangle(int area) {
         
+//    	width is the sqrt of the area
     	int wid = (int) Math.sqrt(area);
     	
+//    	if the area is not equal 0 then decrease the width
     	while(area % wid != 0) {
     		wid--;
     	}
     	
+//    	and the length is the = area/width
     	int len = area / wid;
     	
+    	
+    	// return the length and the width
     	return new int[] {len, wid};
     	
     }
