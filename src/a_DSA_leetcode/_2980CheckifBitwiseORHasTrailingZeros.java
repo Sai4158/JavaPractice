072b1276eed7e00dc12014d133c398b030312175
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-10-29 13:42:14 -0400

    2980. Check if Bitwise OR Has Trailing Zeros

--- src/a_DSA_leetcode/_2980CheckifBitwiseORHasTrailingZeros.java
+++ src/a_DSA_leetcode/_2980CheckifBitwiseORHasTrailingZeros.java
@@ -21,7 +21,8 @@ public class _2980CheckifBitwiseORHasTrailingZeros {
     }
     
     public static void main(String[] args) {
-		
+		int nums[] = {1,2,3,4,5};
+		System.out.println(hasTrailingZeros(nums));
 	}
 	
 }
