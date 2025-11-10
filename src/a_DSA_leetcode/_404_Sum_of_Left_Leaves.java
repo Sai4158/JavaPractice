0136d3ca06166c33b6160230276bb1b8873a41c8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-11-09 22:21:46 -0500

    404. Sum of Left Leaves

---
+++ src/a_DSA_leetcode/_404_Sum_of_Left_Leaves.java
@@ -0,0 +1,34 @@
+package a_DSA_leetcode;
+
+public class _404_Sum_of_Left_Leaves {
+
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static int sumOfLeftLeaves(TreeNode root) {
+
+		int sum = 0;
+		
+		if(root.left != null && root.left.left == null && root.left.right == null) {
+			sum += root.left.val;
+		}
+		
+		
+		sum+= sumOfLeftLeaves(root.left);
+		sum+= sumOfLeftLeaves(root.right);
+		
+		return sum;
+	}
+
+}
