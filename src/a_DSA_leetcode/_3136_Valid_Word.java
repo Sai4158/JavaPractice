173a2586218db23d8a5882209953a91406f73d96
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-11-15 03:04:30 -0500

    3136. Valid Word

---
+++ src/a_DSA_leetcode/_3136_Valid_Word.java
@@ -0,0 +1,43 @@
+package a_DSA_leetcode;
+
+public class _3136_Valid_Word {
+
+    public static boolean isValid(String word) {
+        
+    	int n  = word.length();
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        
+    	for (char c : word.toCharArray()) {
+			
+    		if(!Character.isLetterOrDigit(c)) {
+    			return false;
+    		}
+    			
+    		
+    		if(Character.isLetter(c)) {
+    			
+    			char lower = Character.toLowerCase(c);
+    			
+    			 if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+                     hasVowel = true;
+                 } 
+    			 
+    			 else {
+    				 hasConsonant =  true;
+    			 }
+    		}
+		}
+    	
+    	return hasConsonant && hasVowel;
+    	
+    }
+	
+	public static void main(String[] args) {
+		
+		
+		String word = "234Adas";
+		System.out.println(isValid(word));
+	}
+}
