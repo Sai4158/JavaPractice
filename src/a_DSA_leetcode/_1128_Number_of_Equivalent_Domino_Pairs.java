f3426c70bbf2bf9f13363f77d93eac610b41ec54
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-11-17 22:20:22 -0500

    1128. Number of Equivalent Domino Pairs

--- src/a_DSA_leetcode/_1128_Number_of_Equivalent_Domino_Pairs.java
+++ src/a_DSA_leetcode/_1128_Number_of_Equivalent_Domino_Pairs.java
@@ -22,5 +22,9 @@ public class _1128_Number_of_Equivalent_Domino_Pairs {
 	public static void main(String[] args) {
 		int[][] dominoes = {{1,2},{2,1},{3,4},{5,6}};
 		System.out.println(numEquivDominoPairs(dominoes));
+		
+		int[][] dominoes1 = {{1,4},{4,1},{3,4},{1,4}};
+		System.out.println(numEquivDominoPairs(dominoes1));
+		
 	}
 }
