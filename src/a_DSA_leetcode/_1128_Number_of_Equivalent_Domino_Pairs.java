7cb224eb026591ac2f8e1654defff0a8d8d43350
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-11-18 22:40:53 -0500

    HashMap way -  1128. Number of Equivalent Domino Pairs

--- src/a_DSA_leetcode/_1128_Number_of_Equivalent_Domino_Pairs.java
+++ src/a_DSA_leetcode/_1128_Number_of_Equivalent_Domino_Pairs.java
@@ -1,5 +1,6 @@
 package a_DSA_leetcode;
 
+import java.util.HashMap;
 
 public class _1128_Number_of_Equivalent_Domino_Pairs {
 
@@ -18,13 +19,32 @@ public class _1128_Number_of_Equivalent_Domino_Pairs {
 
         return ans;
     }
+    
+    public static int numEquivDominoPairs1(int[][] dominoes) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+
+        for (int[] d : dominoes) {
+            int a = d[0];
+            int b = d[1];
+            int key = Math.min(a, b) * 10 + Math.max(a, b); 
+
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        for (int count : map.values()) {
+            ans += (count * (count - 1)) / 2;
+        }
+
+        return ans;
+    }
 
 	public static void main(String[] args) {
 		int[][] dominoes = {{1,2},{2,1},{3,4},{5,6}};
 		System.out.println(numEquivDominoPairs(dominoes));
 		
 		int[][] dominoes1 = {{1,4},{4,1},{3,4},{1,4}};
-		System.out.println(numEquivDominoPairs(dominoes1));
+		System.out.println(numEquivDominoPairs1(dominoes1));
 		
 	}
 }
