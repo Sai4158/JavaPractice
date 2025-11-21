1b238478dfead233089a68dc97a0746a82cc7463
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-11-21 16:10:29 -0500

    867_Transpose_Matrix

--- src/a_DSA_leetcode/_867_Transpose_Matrix.java
+++ src/a_DSA_leetcode/_867_Transpose_Matrix.java
@@ -29,5 +29,14 @@ public class _867_Transpose_Matrix {
 		int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
 		System.out.println(transpose(matrix));
 		
+		int matrix2[][] = {{1,3,43},{4,5,46},{467,6,9}};
+		System.out.println(transpose(matrix2));
+		
+		int matri3[][] = {{1,2,3},{4,5,6},{4,5,6}};
+		System.out.println(transpose(matri3));
+		
+		int matri4[][] = {{4,5,6},{4,5,6},{3,1,3}};
+		System.out.println(transpose(matri4));
+		
 	}
 }
