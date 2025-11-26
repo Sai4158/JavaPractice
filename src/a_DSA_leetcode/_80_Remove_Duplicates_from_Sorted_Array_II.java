25a4f5d519feae3e400d07491263bcd5b56e45e4
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-11-25 21:42:05 -0500

    80. Remove Duplicates from Sorted Array II

--- src/a_DSA_leetcode/_80_Remove_Duplicates_from_Sorted_Array_II.java
+++ src/a_DSA_leetcode/_80_Remove_Duplicates_from_Sorted_Array_II.java
@@ -1,8 +1,37 @@
 package a_DSA_leetcode;
 
+import java.util.HashMap;
+
 public class _80_Remove_Duplicates_from_Sorted_Array_II {
 
     public static int removeDuplicates(int[] nums) {
+        
+    	int n =  nums.length;
+    	
+    	if(n <= 2)return n;
+    	
+    HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+    	
+    	for (int i = 0; i < n; i++) {
+			hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);	
+		}
+    	
+    	int count = 0;
+    	
+    	for(int c : hm.keySet()) {
+    		    		
+    		if(hm.get(c) > 2) {
+    			count += 2;
+    		}else {
+    			count += hm.get(c);
+    		}	
+    	}
+    	
+    	
+    	return count;
+    }
+    
+    public static int removeDuplicates1(int[] nums) {
         
     	int n =  nums.length;
     	
@@ -21,10 +50,14 @@ public class _80_Remove_Duplicates_from_Sorted_Array_II {
 		}
     	return first;
     }
+    
 	
 	public static void main(String[] args) {
 		
-		int nums[] = {1,1,1,2,2,3};
+		int nums[] = {0,0,1,1,1,1,2,3,3};
 		System.out.println(removeDuplicates(nums));
+		
+		int nums1[] = {0,0,1,1,1,1,2,3,3};
+		System.out.println(removeDuplicates1(nums1));
 	}
 }
