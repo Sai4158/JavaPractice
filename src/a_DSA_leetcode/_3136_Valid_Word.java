53b405b77d887c600c7648deae3526db2bcc3f00
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-11-27 22:21:16 -0500

    isValidReverse

--- src/a_DSA_leetcode/_3136_Valid_Word.java
+++ src/a_DSA_leetcode/_3136_Valid_Word.java
@@ -34,11 +34,47 @@ public class _3136_Valid_Word {
     	return hasConsonant && hasVowel;
     	
     }
+    
+    public static boolean isValidReverse(String word) {
+        
+        int n = word.length();
+        if (n < 3) return false; 
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        
+    	for (char c : word.toCharArray()) {
+			
+    		if(!Character.isLetterOrDigit(c)) {
+    			return true;
+    		}
+    			
+    		
+    		if(Character.isLetter(c)) {
+    			
+    			char lower = Character.toLowerCase(c);
+    			
+    			 if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+                     hasVowel = false;
+                 } 
+    			 
+    			 else {
+    				 hasConsonant =  false;
+    			 }
+    		}
+		}
+    	
+    	return hasConsonant && hasVowel;
+    	
+    }
 	
 	public static void main(String[] args) {
 		
 		
 		String word = "234Adas";
 		System.out.println(isValid(word));
+		
+		String word1 = "4158Tesla";
+		System.out.println(isValidReverse(word1));
 	}
 }
