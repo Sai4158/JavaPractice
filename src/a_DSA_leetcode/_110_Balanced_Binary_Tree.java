edb14e4e8d4e3932f75117a71045a75f3c28cc09
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-12-12 20:06:06 -0500

    110. Balanced Binary Tree

--- src/a_DSA_leetcode/_110_Balanced_Binary_Tree.java
+++ src/a_DSA_leetcode/_110_Balanced_Binary_Tree.java
@@ -2,4 +2,35 @@ package a_DSA_leetcode;
 
 public class _110_Balanced_Binary_Tree {
 
+	
+    public static boolean isBalanced(TreeNode root) {
+        
+    	return checkHeight(root) != -1;
+    	
+    }
+    
+
+     private static int checkHeight(TreeNode root) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	 public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+        }
+      }
+    
+    
+    public static void main(String[] args) {
+		
+	}
 }
