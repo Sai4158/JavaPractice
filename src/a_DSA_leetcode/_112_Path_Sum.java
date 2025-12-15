41d14e83c3f46dfbf715cf91a1d1518639b01390
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-12-14 21:50:39 -0500

    112. Path Sum

---
+++ src/a_DSA_leetcode/_112_Path_Sum.java
@@ -0,0 +1,42 @@
+package a_DSA_leetcode;
+
+import a_DSA_leetcode._111_Minimum_Depth_of_Binary_Tree_1.TreeNode;
+
+public class _112_Path_Sum {
+
+	
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	     TreeNode right;
+	     TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	  }
+	
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        
+    	if(root ==  null) {
+    		return false;
+    	}
+    	
+    	if(root.left == null && root.right == null) {
+    		return targetSum == root.val;
+    	}
+    	
+    	int newTarget = targetSum -  root.val;
+    	
+    	
+    	return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    	
+    }
+    
+    public static void main(String[] args) {
+		
+	}
+	
+}
