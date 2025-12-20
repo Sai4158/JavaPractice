17d16af03e63d5ba27a6645addc706f46330f1cf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-12-19 20:41:04 -0500

    94. Binary Tree Inorder Traversal

---
+++ src/a_DSA_leetcode/_94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,52 @@
+package a_DSA_leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _94_Binary_Tree_Inorder_Traversal {
+
+	
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+    	List<Integer> ans =  new ArrayList<Integer>();
+    	
+    	inorder(root, ans);
+    	
+    	
+    	return ans;
+    }
+    
+    
+    
+     private void inorder(TreeNode root, List<Integer> ans) {
+
+    	 if(root == null) {
+    		 return;
+    	 }
+    	 
+    	 inorder(root.left, ans);
+    	 
+    	 ans.add(root.val);
+    	 
+    	 inorder(root.right, ans);
+	}
+
+
+
+	 public class TreeNode {
+    	      int val;
+    	      TreeNode left;
+    	      TreeNode right;
+    	      TreeNode() {}
+    	      TreeNode(int val) { this.val = val; }
+    	      TreeNode(int val, TreeNode left, TreeNode right) {
+    	          this.val = val;
+    	          this.left = left;
+    	          this.right = right;
+    	      }
+    	  }
+    
+    public static void main(String[] args) {
+		
+	}
+}
