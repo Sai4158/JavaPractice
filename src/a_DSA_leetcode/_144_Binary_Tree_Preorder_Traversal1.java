52030ce8dc6e850a5075c932073f782c5ec96e3f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-12-21 15:48:57 -0500

    144. Binary Tree Preorder Traversal

---
+++ src/a_DSA_leetcode/_144_Binary_Tree_Preorder_Traversal1.java
@@ -0,0 +1,43 @@
+package a_DSA_leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import a_DSA_leetcode._144_Binary_Tree_Preorder_Traversal.TreeNode;
+
+public class _144_Binary_Tree_Preorder_Traversal1 {
+
+	 public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	  }
+	 
+	    public List<Integer> preorderTraversal(TreeNode root) {
+	    
+	    List<Integer> ans  =  new ArrayList<Integer>();
+	    preorder(root, ans);
+	    
+	    return ans;
+	    }
+
+		private void preorder(TreeNode root, List<Integer> ans) {
+
+			if(root ==  null) {
+				return;
+			}
+			
+			ans.add(root.val);
+			
+			preorder(root.left, ans);
+			
+			preorder(root.right, ans);
+		}
+}
