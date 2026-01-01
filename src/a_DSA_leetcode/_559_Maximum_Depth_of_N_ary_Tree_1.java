10f219f41459d58fdb6c9e50c91d3da20d7e7428
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-01-01 03:42:52 -0500

    Updated

--- src/a_DSA_leetcode/_559_Maximum_Depth_of_N_ary_Tree_1.java
+++ src/a_DSA_leetcode/_559_Maximum_Depth_of_N_ary_Tree_1.java
@@ -30,23 +30,23 @@ public class _559_Maximum_Depth_of_N_ary_Tree_1 {
     	}
     	
     	
-    	int max = 0;
+    	int maxDepth = 0;
     	
         // Check the depth of each child
 
     	for (Node child: root.children) {
 			int childdepth = maxDepth(child);
 			
-			if(childdepth >  max) {
-				max =  childdepth;
+			if(childdepth >  maxDepth) {
+				maxDepth =  childdepth;
 			}
 			
-			return 1 + max;
+			return 1 + maxDepth;
 		}
     	
         // Depth of this node = 1 (itself) + max depth among children
 
-    	return  1 + max;
+    	return  1 + maxDepth;
     }
 	
 }
