72abc699671c8256390eee171bd43646b2d8408f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-01-06 20:16:07 -0500

    Added comments

--- src/a_DSA_leetcode/_226_Invert_Binary_Tree.java
+++ src/a_DSA_leetcode/_226_Invert_Binary_Tree.java
@@ -18,14 +18,19 @@ public class _226_Invert_Binary_Tree {
 	
     public static TreeNode invertTree(TreeNode root) {
         
-    	
+        // If the tree (or subtree) is empty, nothing to invert
     	if(root ==  null) {
     		return null;
     	}
     	
+        // Invert left and right subtrees
+
     	TreeNode LeftInverted =  invertTree(root.left);
     	TreeNode RightyInverted =  invertTree(root.right);
     	
+    	
+        // Swap them
+
     	root.left =  RightyInverted;
     	root.right =  LeftInverted;
     	
