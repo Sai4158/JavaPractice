1c82cf835b982b05e5375ee8aed9de8e3a1a37a1
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-01-12 20:28:40 -0500

    Basic classes

---
+++ src/_B_OOPS/Part1_OOPS.java
@@ -0,0 +1,28 @@
+package _B_OOPS;
+
+public class Part1_OOPS {
+
+	public static void main(String[] args) {
+
+		String name1 = "cat";
+		int price1  = 5;
+
+        ToyStore branch1 = new ToyStore(name1, price1);
+
+		System.out.println(branch1);
+
+	}
+}
+
+	class  ToyStore{
+		String name;
+		int price;
+
+		public ToyStore(String name, int price) {
+
+			name =  this.name;
+			price  =  this.price;
+		}
+
+	}
+
