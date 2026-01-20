ab0e99cfc7b1508519117d287851045aabf4eb9f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-01-19 19:40:00 -0500

    Added comments

--- src/a_DSA_leetcode/_2331_Evaluate_Boolean_Binary_Tree.java
+++ src/a_DSA_leetcode/_2331_Evaluate_Boolean_Binary_Tree.java
@@ -18,18 +18,25 @@ public class _2331_Evaluate_Boolean_Binary_Tree {
 
 	public boolean evaluateTree(TreeNode root) {
 		
+		
+        // If it's a leaf node, value is either 0 (false) or 1 (true)
 		if(root.left == null && root.right == null) {
 			return root.val == 1;
 		}
 		
+        // Recursively evaluate left and right subtrees
 		boolean leftVal = evaluateTree(root.left);
 		boolean rigtVal = evaluateTree(root.right);
 		
+		
+        // Apply the operation based on root.vaL
 		if(root.val == 2) {
 			return leftVal || rigtVal;
 		}
 		
+		
 		else {
+            // val == 3, AND
 			return leftVal && rigtVal;
 		}
 		
