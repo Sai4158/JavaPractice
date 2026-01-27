b4e7332375d44a2bef84f9de009d186075f3c471
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-01-27 01:11:36 -0500

    getMaxDeletions

--- src/_B_LC/_Character_Reprogramming.java
+++ src/_B_LC/_Character_Reprogramming.java
@@ -2,7 +2,32 @@ package _B_LC;
 
 public class _Character_Reprogramming {
 	
+    public static int getMaxDeletions(String s) {
+
+
+        int u = 0, d = 0, r = 0, l = 0;
+    	
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == 'U') u++;
+            else if (c == 'D') d++;
+            else if (c == 'R') r++;
+            else if (c == 'L') l++;
+        }
+    	
+    	
+        int netX = r - l;
+        int netY = u - d;
+    	
+
+        int mustKeep = Math.abs(netX) + Math.abs(netY);
+        return s.length() - mustKeep;
+    	
+    }
+	
 	public static void main(String[] args) {
-		
+		String s  = "URDR";
+		System.out.println(getMaxDeletions(s));
 	}
 }
