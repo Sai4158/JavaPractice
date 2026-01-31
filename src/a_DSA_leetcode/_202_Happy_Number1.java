b288c5e3b324f71da19607426b04dce70bc4b61f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-01-30 19:22:04 -0500

    202_Happy_Number1

--- src/a_DSA_leetcode/_202_Happy_Number1.java
+++ src/a_DSA_leetcode/_202_Happy_Number1.java
@@ -45,6 +45,46 @@ public class _202_Happy_Number1 {
         // if somehow not found 1 and not hit 4 within 1000 steps
         return false;
     }
+    
+    public static boolean isHappy1(int n) {
+        
+    	if(n < 0) {
+    		return false;
+    	}
+    	
+    	
+    	    	
+    	for (int i = 0; i <1000; i++) {
+			
+    		if(n == 1) {
+    			return true;
+    		}
+    		
+    		if(n ==  4) {
+    			return false;
+    		}
+    		
+    		int ans = 0;
+        	String s =  Integer.toString(n);
+
+    		
+    		 for (int j = 0; j < s.length(); j++) {
+				
+    			 int digit =  s.charAt(j) - '0';
+    			
+    			 ans += digit*digit;
+    			 
+			}
+    		
+    		 
+    		 n = ans;
+    		 
+		}
+    	
+    	
+    	return false;
+    	
+    }
 
     public static void main(String[] args) {
         int n = 19;
