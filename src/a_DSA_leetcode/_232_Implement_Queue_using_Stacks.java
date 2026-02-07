0c54e5f20d1391dab0a8f6957692a4a3fa931b75
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-02-07 16:20:06 -0500

    Added comments

--- src/a_DSA_leetcode/_232_Implement_Queue_using_Stacks.java
+++ src/a_DSA_leetcode/_232_Implement_Queue_using_Stacks.java
@@ -4,51 +4,50 @@ import java.util.Stack;
 
 public class _232_Implement_Queue_using_Stacks {
 
-	class MyQueue {
+    class MyQueue {
+    	// stack used for pushing (back of queue)
+        private Stack<Integer> in;  
+        
+        // stack used for popping/peeking (front of queue)
+        private Stack<Integer> out;  
+        
+        public MyQueue() {
+            in = new Stack<>();      
+            out = new Stack<>();     
+        }
 
-		private Stack<Integer> in;
-		private Stack<Integer> out;
+        public void push(int x) {
+            // always push new elements into 'in'
+            in.push(x);
+        }
 
-		public MyQueue() {
-			in =  new Stack<>();
-			out =  new Stack<>();
-		}
+        public int pop() {
+            // make sure 'out' has the front element on top
+            moveIfneeded();
+            // remove and return the front of the queue
+            return out.pop();
+        }
 
-		public void push(int x) {
+        public int peek() {
+            // make sure 'out' has the front element on top
+            moveIfneeded();
+            // return the front of the queue (do not remove)
+            return out.peek();
+        }
 
-			in.push(x);
+        public boolean empty() {
+            // queue is empty only if both stacks are empty
+            return in.isEmpty() && out.isEmpty();
+        }
 
-		}
-
-		public int pop() {
-
-			moveIfneeded();
-			return out.pop();
-
-		}
-
-
-		public int peek() {
-
-			moveIfneeded();
-			return out.peek();
-
-
-		}
-
-		public boolean empty() {
-			return in.empty() && out.isEmpty();
-		}
-
-
-		private void moveIfneeded() {
-
-			if (out.isEmpty()) {
-				while (!in.isEmpty()) {
-					out.push(in.pop());
-				}
-			}
-
-		}
-	}
+        private void moveIfneeded() {
+            // only move when 'out' is empty
+            // moving reverses order so oldest element becomes on top of 'out'
+            if (out.isEmpty()) {
+                while (!in.isEmpty()) {
+                    out.push(in.pop());
+                }
+            }
+        }
+    }
 }
