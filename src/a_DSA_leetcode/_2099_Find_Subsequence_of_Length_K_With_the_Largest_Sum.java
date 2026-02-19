e2073d7e6fc2f8e792f6a5d119170f7e0372ae33
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-02-18 21:50:41 -0500

    2099. Find Subsequence of Length K With the Largest Sum

--- src/a_DSA_leetcode/_2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum.java
+++ src/a_DSA_leetcode/_2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum.java
@@ -1,38 +1,33 @@
-package a_DSA_leetcode;
-
 import java.util.Arrays;
 
-public class _2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum {
-	
-    public static int[] maxSubsequence(int[] nums, int k) {
-        
-//    	Nums length
-    	int  n = nums.length;
-    	
-//    	Array copy
-    	Integer[] idx = new Integer[n];
-    	
+class Solution {
+    public int[] maxSubsequence(int[] nums, int k) {
 
-    	for (int i = 0; i < n; i++) {
-    		idx[i] = i;
-		}
-    	
-//    	Sorting 
+        int n = nums.length;
+
+        // store indices
+        Integer[] idx = new Integer[n];
+        
+        for (int i = 0; i < n; i++) {
+        	idx[i] = i;
+        }
+
+        // sort indices by nums value (small -> big)
         Arrays.sort(idx, (a, b) -> Integer.compare(nums[a], nums[b]));
 
+        // take last k indices (right side = biggest values)
         Integer[] pick = Arrays.copyOfRange(idx, n - k, n);
-        
+
+        // sort picked indices to keep original order (subsequence rule)
         Arrays.sort(pick);
 
-        
+        // build answer
         int[] ans = new int[k];
-        for(int i = 0; i<k; i++) {
-        	ans[i] =  nums[pick[i]];
-        }
         
-        return ans;
-    
-    }
-    
-}
+        for (int i = 0; i < k; i++) {
+            ans[i] = nums[pick[i]];
+        }
 
+        return ans;
+    }
+}
