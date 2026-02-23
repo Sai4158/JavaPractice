8617fb770ec5791256f959496ac2dc54fe41d7fe
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-02-22 22:54:00 -0500

    1929. Concatenation of Array

--- src/Q1/_Concatenation_of_Array.java
+++ src/Q1/_Concatenation_of_Array.java
@@ -19,9 +19,26 @@ public class _Concatenation_of_Array {
 		return ans;
 	}
 
+	public static int[] getConcatenation1(int[] nums) {
+		
+		int  n =  nums.length;
+		int ans[] = new int[n*2];
+		
+		for (int i = 0; i < n; i++) {
+			
+			ans[i] = nums[i];
+			ans[n+i] =  nums[i];
+			
+		}
+		
+		return ans;
+		
+	}
 
 	public static void main(String[] args) {
 		int nums[] = {1,2,1};
 		System.out.println(getConcatenation(nums));
+		
+		System.out.println(getConcatenation1(nums));
 	}
 }
