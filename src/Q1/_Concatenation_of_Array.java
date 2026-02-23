e409941ae85a82be38a913e12dfa34d8c97dfb51
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-02-22 19:35:27 -0500

    Q1. Concatenation of Array

---
+++ src/Q1/_Concatenation_of_Array.java
@@ -0,0 +1,27 @@
+package Q1;
+
+public class _Concatenation_of_Array {
+
+	public static int[] getConcatenation(int[] nums) {
+
+		int  n =  nums.length;
+		int[] ans = new int[2 * n];
+
+
+		for (int i = 0; i < n; i++) {
+
+			ans[i] = nums[i]; 
+			
+			ans[i + n] =  nums[i];
+		}
+
+		
+		return ans;
+	}
+
+
+	public static void main(String[] args) {
+		int nums[] = {1,2,1};
+		System.out.println(getConcatenation(nums));
+	}
+}
