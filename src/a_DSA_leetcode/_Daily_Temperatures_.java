8d0c19c33066ba82289a86c0517d74b26e3e7ac5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-03-02 22:50:19 -0500

    Added comments

--- src/a_DSA_leetcode/_Daily_Temperatures_.java
+++ src/a_DSA_leetcode/_Daily_Temperatures_.java
@@ -3,31 +3,38 @@ package a_DSA_leetcode;
 public class _Daily_Temperatures_ {
 
     public static int[] dailyTemperatures(int[] temperatures) {
-        
-    	int n =  temperatures.length;
-    	int ans[] = new int[n];
-    	
-    	
-    	for (int i = 0; i < n; i++) {
-			
-    		ans[i] =  0;
-    		
-    		for (int j = i + 1; j < n; j++) {
 
+        // get size of array
+        int n = temperatures.length;
+
+        // answer array to store how many days to wait
+        int ans[] = new int[n];
+
+        // check each day one by one
+        for (int i = 0; i < n; i++) {
+
+            // default is 0 (no warmer day found yet)
+            ans[i] = 0;
+
+            // look for the next warmer day on the right side
+            for (int j = i + 1; j < n; j++) {
+
+                // if we find a warmer temperature
                 if (temperatures[j] > temperatures[i]) {
-                    ans[i] = j - i;
-                    break;
+                    ans[i] = j - i; // store number of days waited
+                    break;          // stop at the first warmer day
                 }
             }
-    		
-		}
-    	return ans;
+        }
+
+        // return final answer array
+        return ans;
     }
-    
+
     public static void main(String[] args) {
-		
-    	int temp[] = {73,74,75,71,69,72,76,73};
-    	System.out.println(dailyTemperatures(temp));
-	}
-	
+
+        int temp[] = {73, 74, 75, 71, 69, 72, 76, 73};
+
+        System.out.println(java.util.Arrays.toString(dailyTemperatures(temp)));
+    }
 }
