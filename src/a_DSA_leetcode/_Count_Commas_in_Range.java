1907d4e36aae95a052719a6c44774672b71e2f08
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-03-15 02:31:15 -0400

    Q1. Count Commas in Range

---
+++ src/a_DSA_leetcode/_Count_Commas_in_Range.java
@@ -0,0 +1,28 @@
+package a_DSA_leetcode;
+
+public class _Count_Commas_in_Range {
+
+	
+    public static int countCommas(int n) {
+        
+    	int ans = 0;
+    	
+    	if(n >= 1000) {
+    		
+    		ans =  n - 999;
+    		
+    	}else {
+    		return 0;
+    	}
+    	
+    	return ans;
+    }
+    
+    public static void main(String[] args) {
+		
+    	int n = 998;
+    	
+    	System.out.println(countCommas(n));
+    	
+	}
+}
